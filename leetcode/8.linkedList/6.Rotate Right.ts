f091fc8caacf0eb08dc339ed5e0caa31697e459b
meixiang474 <1436098850@qq.com>
2021-10-08 11:46:02 +0800

    feat: leetcode 61

---
+++ leetcode/8.linkedList/6.Rotate Right.ts
@@ -0,0 +1,33 @@
+// leetcode 61
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val: number) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+export default function rotateRight(
+  head: ListNode | null,
+  k: number
+): ListNode | null {
+  if (k === 0 || !head || !head.next) return head;
+  let count = 1;
+  let current = head;
+  while (current.next) {
+    count++;
+    current = current.next;
+  }
+  current.next = head;
+  k = count - (k % count);
+  let index = 0;
+  let prev = head;
+  for (let i = 0; i < k - 1; i++) {
+    prev = prev.next!;
+  }
+  const res = prev.next;
+  prev.next = null;
+  return res;
+}
