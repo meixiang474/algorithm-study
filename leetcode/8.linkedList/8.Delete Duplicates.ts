2b9afd0567a4f2f423d434b1d125b2b9b18662c9
meixiang474 <1436098850@qq.com>
2021-10-10 10:11:48 +0800

    feat: leetcode 83

---
+++ leetcode/8.linkedList/8.Delete Duplicates.ts
@@ -0,0 +1,24 @@
+// leetcode 83
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val: number) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+export default function deleteDuplicates(head: ListNode | null) {
+  const dummyHead = new ListNode(-1);
+  dummyHead.next = head;
+  let prev = dummyHead;
+  while (prev.next && prev.next.next) {
+    if (prev.next.val === prev.next.next.val) {
+      prev.next = prev.next.next;
+    } else {
+      prev = prev.next;
+    }
+  }
+  return dummyHead.next;
+}
