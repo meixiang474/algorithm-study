3ba445fbdbd6d5851b042b9befb6b4045390822e
zhengbowen01 <zhengbowen01@MacBook-Pro.local>
2021-10-14 13:15:46 +0800

    feat: leetcode 2

---
+++ leetcode/9.math/1.Add Two.ts
@@ -0,0 +1,32 @@
+// leetcode 2
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val: number) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+export default function addTwo(l1: ListNode | null, l2: ListNode | null) {
+  const l3 = new ListNode(-1);
+  let p1 = l1;
+  let p2 = l2;
+  let p3 = l3;
+  let carry = 0;
+  while (p1 || p2) {
+    const n1 = p1 ? p1.val : 0;
+    const n2 = p2 ? p2.val : 0;
+    const sum = n1 + n2 + carry;
+    carry = Math.floor(sum / 10);
+    p3.next = new ListNode(sum % 10);
+    p3 = p3.next;
+    if (p1) p1 = p1.next;
+    if (p2) p2 = p2.next;
+  }
+  if (carry) {
+    p3.next = new ListNode(carry);
+  }
+  return l3.next;
+}
