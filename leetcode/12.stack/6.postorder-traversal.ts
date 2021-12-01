142706bee3ab6d3f7cd023724692c6b4b8deb9d9
meixiang474 <1436098850@qq.com>
2021-12-01 10:22:31 +0800

    feat: leetcode 145

---
+++ leetcode/12.stack/6.postorder-traversal.ts
@@ -0,0 +1,35 @@
+// leetcode 145
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val: number) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export default function postOrderTraversal(root: TreeNode | null) {
+  if (!root) return [];
+  const res: number[] = [];
+  const stack: TreeNode[] = [];
+  let p: TreeNode | null = root;
+  let prevRight: TreeNode | null = null
+  while (stack.length > 0 || p) {
+    while (p) {
+      stack.push(p);
+      p = p.left;
+    }
+    const current = stack.pop()!;
+    if (!current.right || current.right === prevRight) {
+      prevRight = current
+      res.push(current.val);
+    } else {
+      stack.push(current)
+      p = current.right;
+    }
+  }
+  return res
+}
