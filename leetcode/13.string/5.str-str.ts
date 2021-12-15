56d1a5f52edd5cd0d624ecd67b557c6f3be6612c
meixiang474 <1436098850@qq.com>
2021-12-15 20:21:08 +0800

    feat: leetcode 28

---
+++ leetcode/13.string/5.str-str.ts
@@ -0,0 +1,27 @@
+// leetcode 28
+
+export default function strStr(haystack: string, needle: string) {
+  if (!needle) return 0;
+  if (haystack.length < needle.length) return -1;
+  let res = -1;
+  let index = 0;
+  while (index < haystack.length) {
+    if (haystack.length - index < needle.length) break;
+    const current = haystack[index];
+    if (current === needle[index]) {
+      let flag = true;
+      for (let i = index + 1; i < index + needle.length; i++) {
+        if (haystack[i] !== needle[i - index]) {
+          flag = false;
+          break;
+        }
+      }
+      if (flag) {
+        res = index;
+        break;
+      }
+    }
+    index++;
+  }
+  return res;
+}
