eca5e7bba43908a54ea34a7d74b818935b299407
meixiang474 <1436098850@qq.com>
2021-12-20 10:52:46 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -1,147 +1,106 @@
-// offer 60
+// offer 61
 
-export function dicesProbability(n: number) {
-  if (n === 0) return [];
-  let dp = new Array(6).fill(1 / 6);
-  for (let i = 2; i <= n; i++) {
-    const temp: number[] = [];
-    for (let j = 1; j <= 6; j++) {
-      for (let k = 0; k < dp.length; k++) {
-        const sum = k + j - 1;
-        temp[sum] = (temp[sum] || 0) + dp[k] * (1 / 6);
-      }
-      dp = temp;
+export function isStraight(nums: number[]) {
+  const set = new Set<number>();
+  let min = 14;
+  let max = 0;
+  for (let item of nums) {
+    if (item === 0) continue;
+    if (set.has(item)) return false;
+    max = Math.max(item, max);
+    min = Math.min(item, min);
+    set.add(item);
+  }
+  return max - min < 5;
+}
+
+// BST
+
+interface Visitor {
+  visit: (val: number) => void;
+}
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val: number) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export class BST {
+  root: TreeNode | null;
+  size: number;
+  constructor() {
+    this.root = null;
+    this.size = 0;
+  }
+  getSize() {
+    return this.size;
+  }
+  isEmpty() {
+    return this.size === 0;
+  }
+  add(e: number) {
+    this.root = this.addNode(e, this.root);
+  }
+  addNode(e: number, node: TreeNode | null): TreeNode {
+    if (!node) {
+      this.size++;
+      return new TreeNode(e);
+    }
+    if (node.val < e) {
+      node.right = this.addNode(e, node.right);
+    }
+    if (node.val > e) {
+      node.right = this.addNode(e, node.left);
+    }
+    return node;
+  }
+  contains(e: number) {
+    return this.containsNode(this.root, e);
+  }
+  containsNode(node: TreeNode | null, e: number): boolean {
+    if (!node) return false;
+    if (e > node.val) {
+      return this.containsNode(node.right, e);
+    }
+    if (e < node.val) {
+      return this.containsNode(node.left, e);
+    }
+    return true;
+  }
+  preOrder(visitor: Visitor) {
+    this.preOrderNode(this.root, visitor);
+  }
+  preOrderNode(node: TreeNode | null, visitor: Visitor) {
+    if (node) {
+      visitor.visit(node.val);
+      this.preOrderNode(node.left, visitor);
+      this.preOrderNode(node.right, visitor);
     }
   }
-  return dp;
-}
-
-// binary search
-
-export function binarySearch(arr: number[], target: number) {
-  const searchData = (
-    arr: number[],
-    l: number,
-    r: number,
-    target: number
-  ): number => {
-    if (l > r) return -1;
-    const mid = Math.floor(l + (r - l) / 2);
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] > target) {
-      return searchData(arr, l, mid - 1, target);
-    } else {
-      return searchData(arr, mid + 1, r, target);
-    }
-  };
-  return searchData(arr, 0, arr.length - 1, target);
-}
-
-export function binarySearch1(arr: number[], target: number) {
-  let l = 0,
-    r = arr.length;
-  while (l < r) {
-    const mid = Math.floor(l + (r - l) / 2);
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] > target) {
-      r = mid;
-    } else {
-      l = mid + 1;
+  inOrder(visitor: Visitor) {
+    this.inOrderNode(this.root, visitor);
+  }
+  inOrderNode(node: TreeNode | null, visitor: Visitor) {
+    if (node) {
+      this.inOrderNode(node.left, visitor);
+      visitor.visit(node.val);
+      this.inOrderNode(node.right, visitor);
     }
   }
-  return -1;
-}
-
-// > target的第一个
-export function upper(arr: number[], target: number) {
-  let l = 0,
-    r = arr.length;
-  while (l < r) {
-    const mid = Math.floor(l + (r - l) / 2);
-    if (arr[mid] > target) {
-      r = mid;
-    } else {
-      l = mid + 1;
+  postOrder(visitor: Visitor) {
+    this.postOrderNode(this.root, visitor);
+  }
+  postOrderNode(node: TreeNode | null, visitor: Visitor) {
+    if (node) {
+      this.postOrderNode(node.left, visitor);
+      this.postOrderNode(node.right, visitor);
+      visitor.visit(node.val);
     }
   }
-  return l;
-}
-
-// = target的最后一个或者 > target的第一个
-export function ceil(arr: number[], target: number) {
-  const index = upper(arr, target);
-  if (index - 1 >= 0 && arr[index - 1] === target) {
-    return index - 1;
-  }
-  return index;
-}
-
-// = target第一个或者 > target第一个
-export function lowerCeil(arr: number[], target: number) {
-  let l = 0,
-    r = arr.length;
-  while (l < r) {
-    const mid = Math.floor(l + (r - l) / 2);
-    if (arr[mid] >= target) {
-      r = mid;
-    } else {
-      l = mid + 1;
-    }
-  }
-  return l;
-}
-
-// < target得第一个
-export function lower(arr: number[], target: number) {
-  let l = -1,
-    r = arr.length - 1;
-  while (l < r) {
-    const mid = Math.floor(l + (r - l + 1) / 2);
-    if (arr[mid] < target) {
-      l = mid;
-    } else {
-      r = mid - 1;
-    }
-  }
-  return l;
-}
-
-// = target的最后一个，< target的第一个
-export function upperFloor(arr: number[], target: number) {
-  let l = -1,
-    r = arr.length - 1;
-  while (l < r) {
-    const mid = Math.floor(l + (r - l + 1) / 2);
-    if (arr[mid] <= target) {
-      l = mid;
-    } else {
-      r = mid - 1;
-    }
-  }
-  return l;
-}
-
-// < target得第一个，= target得第一个
-export function lowerFloor(arr: number[], target: number) {
-  const index = lower(arr, target);
-  if (index + 1 < arr.length && arr[index + 1] === target) {
-    return index + 1;
-  }
-  return index;
-}
-
-export function fn(x: number) {
-  let l = 0,
-    r = x;
-  while (l < r) {
-    const mid = Math.floor(l + (r - l + 1) / 2);
-    if (mid ** 2 <= x) {
-      l = mid;
-    } else {
-      r = mid - 1;
-    }
-  }
-  return l;
 }
