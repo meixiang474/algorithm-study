82cf73bc88f5bd716fa38c5cafcbd3602c202b1f
meixiang474 <1436098850@qq.com>
2021-12-28 10:39:52 +0800

    feat: leetcode 96

---
+++ leetcode/14.tree/2.num-trees.ts
@@ -0,0 +1,14 @@
+// leetcode 96
+
+export default function numTrees(n: number) {
+  const dp = [1, 1]
+  for(let i = 2; i <= n; i++) {
+    if(dp[i] == null) {
+      dp[i] = 0
+    }
+    for(let j = 1; j <= i; j++) {
+      dp[i] = dp[i] + dp[j - 1] * dp[i - j]
+    }
+  }
+  return dp[n]
+}
