6eb575a6f15064ab15554bdafb8530f83550b4cd
meixiang474 <1436098850@qq.com>
2021-12-31 10:25:37 +0800

    feat: leetcode 101

---
+++ leetcode/14.tree/5.is-symmetric.ts
@@ -0,0 +1,29 @@
+// leetcode 101
+
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val: number) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export default function isSymmetric(root: TreeNode | null) {
+  if (!root) return true;
+  const compare = (p: TreeNode | null, q: TreeNode | null) => {
+    if (!p && !q) return true;
+    if (
+      p &&
+      q &&
+      p.val === q.val &&
+      compare(p.left, q.right) &&
+      compare(p.right, q.left)
+    )
+      return true;
+    return false;
+  };
+  return compare(root.left, root.right);
+}
