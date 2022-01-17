f0fa713b651564f1e18bda156ca71324d990e823
meixiang474 <1436098850@qq.com>
2022-01-17 10:38:53 +0800

    feat: leetcode 18

---
+++ leetcode/15.two-pointers/5.four-sum.ts
@@ -0,0 +1,58 @@
+// leetcode 18
+
+export default function forSum(nums: number[], target: number) {
+  nums.sort((a, b) => a - b);
+  const res: number[][] = [];
+  for (let i = 0; i < nums.length - 3; i++) {
+    const current = nums[i];
+    if (i > 0 && current === nums[i - 1]) continue;
+    if (current + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+    if (
+      current +
+        nums[nums.length - 1] +
+        nums[nums.length - 2] +
+        nums[nums.length - 3] <
+      target
+    )
+      continue;
+    for (let j = i + 1; j < nums.length - 2; j++) {
+      const second = nums[j];
+      if (j > i + 1 && nums[j - 1] === second) continue;
+      if (current + second + nums[j + 1] + nums[j + 2] > target) break;
+      if (
+        current + second + nums[nums.length - 1] + nums[nums.length - 2] <
+        target
+      )
+        continue;
+      let l = j + 1,
+        r = nums.length - 1;
+      while (l < r) {
+        const left = nums[l];
+        const right = nums[r];
+        const sum = current + second + left + right;
+        if (sum === target) {
+          res.push([current, second, left, right]);
+          while (l < r) {
+            l++;
+            if (nums[l] !== left) break;
+          }
+          while (l < r) {
+            r--;
+            if (nums[r] !== right) break;
+          }
+        } else if (sum < target) {
+          while (l < r) {
+            l++;
+            if (nums[l] !== left) break;
+          }
+        } else {
+          while (l < r) {
+            r--;
+            if (nums[r] !== right) break;
+          }
+        }
+      }
+    }
+  }
+  return res;
+}
