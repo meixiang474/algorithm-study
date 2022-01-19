0dc9f554d6f8333798e6422bc394239a21a1758e
meixiang474 <1436098850@qq.com>
2022-01-19 09:59:30 +0800

    feat: leetcode 26

---
+++ leetcode/15.two-pointers/7.remove-duplicates.ts
@@ -0,0 +1,17 @@
+// leetcode 26
+
+export default function removeDuplicates(nums: number[]) {
+  let res = nums.length;
+  let i = 0;
+  while (i < res) {
+    if (i > 0 && nums[i - 1] === nums[i]) {
+      res--;
+      for (let j = i; j < res; j++) {
+        nums[j] = nums[j + 1];
+      }
+    } else {
+      i++;
+    }
+  }
+  return res;
+}
