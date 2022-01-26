46cf747f33eb4983d21b1cb0a39a0f9251600054
meixiang474 <1436098850@qq.com>
2022-01-26 20:04:00 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -43,13 +43,184 @@ export class LinkedList<T = number> {
   dummyHead: ListNode<T>;
   size: number;
   constructor(dummyHead: T) {
-    this.dummyHead = new ListNode(dummyHead)
-    this.size = 0
+    this.dummyHead = new ListNode(dummyHead);
+    this.size = 0;
   }
   getSize() {
-    return this.size
+    return this.size;
   }
   isEmpty() {
-    
+    return this.size === 0;
   }
+  add(index: number, val: T) {
+    if (index < 0 || index > this.size) throw new Error("error");
+    let prev = this.dummyHead;
+    for (let i = 0; i < index; i++) {
+      prev = prev.next!;
+    }
+    prev.next = new ListNode(val, prev.next);
+    this.size++;
+  }
+  addFirst(val: T) {
+    this.add(0, val);
+  }
+  addLast(val: T) {
+    this.add(this.size, val);
+  }
+  get(index: number) {
+    if (index < 0 || index >= this.size) throw new Error("error");
+    let current = this.dummyHead.next;
+    for (let i = 0; i < index; i++) {
+      current = current!.next;
+    }
+    return current!.val;
+  }
+  getFirst() {
+    return this.get(0);
+  }
+  getLast() {
+    return this.get(this.size - 1);
+  }
+  set(index: number, val: T) {
+    if (index < 0 || index >= this.size) throw new Error("error");
+    let current = this.dummyHead.next;
+    for (let i = 0; i < index; i++) {
+      current = current!.next;
+    }
+    current!.val = val;
+  }
+  contains(val: T) {
+    let current = this.dummyHead.next;
+    while (current) {
+      if (current.val === val) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+  remove(index: number) {
+    if (index < 0 || index >= this.size) throw new Error("error");
+    let prev = this.dummyHead;
+    for (let i = 0; i < index; i++) {
+      prev = prev.next!;
+    }
+    const res = prev.next!;
+    prev.next = prev.next!.next;
+    this.size--;
+    return res.val;
+  }
+  removeFirst() {
+    return this.remove(0);
+  }
+  removeLast() {
+    return this.remove(this.size - 1);
+  }
+  removeElement(val: T) {
+    let prev = this.dummyHead;
+    while (prev.next) {
+      if (prev.next.val === val) break;
+      prev = prev.next;
+    }
+    if (prev.next) {
+      this.size--;
+      prev.next = prev.next;
+    }
+  }
+  toString() {
+    let res = "";
+    let current = this.dummyHead.next;
+    while (current) {
+      res += JSON.stringify(current.val) + "->";
+      current = current.next;
+    }
+    res += "NULL";
+    return res;
+  }
+}
+
+export function removeElements(head: ListNode | null, val: number) {
+  while (head && head.val === val) {
+    head = head.next;
+  }
+  if (!head) return head;
+  let prev = head;
+  while (prev.next) {
+    if (prev.next.val === val) {
+      prev.next = prev.next.next;
+    } else {
+      prev = prev.next;
+    }
+  }
+  return head;
+}
+
+export function removeElements1(head: ListNode | null, val: number) {
+  const dummyHead = new ListNode(-1);
+  dummyHead.next = head;
+  let prev = dummyHead;
+  while (prev.next) {
+    if (prev.next.val === val) {
+      prev.next = prev.next.next;
+    } else {
+      prev = prev.next;
+    }
+  }
+  return dummyHead.next;
+}
+
+export function removeElements2(
+  head: ListNode | null,
+  val: number
+): ListNode | null {
+  if (!head) return head;
+  const res = removeElements2(head.next, val);
+  if (head.val === val) {
+    return res;
+  } else {
+    head.next = res;
+    return head;
+  }
+}
+
+export function reverse(head: ListNode | null) {
+  let prev = null;
+  let current = head;
+  while (current) {
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+}
+
+export function reverse1(head: ListNode | null): ListNode | null {
+  if (!head || !head.next) return head;
+  const res = reverse1(head.next);
+  head.next.next = head;
+  head.next = null;
+  return res;
+}
+
+export function addTwoNumbers(l1: ListNode | null, l2: ListNode | null) {
+  const l3 = new ListNode(-1);
+  let p1 = l1;
+  let p2 = l2;
+  let p3 = l3;
+  let carry = 0;
+  while (p1 || p2) {
+    const v1 = p1 ? p1.val : 0;
+    const v2 = p2 ? p2.val : 0;
+    const sum = v1 + v2 + carry;
+    carry = Math.floor(sum / 10);
+    p3.next = new ListNode(sum % 10);
+    p3 = p3.next;
+    if (p1) p1 = p1.next;
+    if (p2) p2 = p2.next;
+  }
+  if (carry) {
+    p3.next = new ListNode(carry);
+  }
+  return l3.next;
 }
