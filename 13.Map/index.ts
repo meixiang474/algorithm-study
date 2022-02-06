94f0b91cf89773f486b28f72287c4735431b2b19
meixiang474 <1436098850@qq.com>
2022-02-06 10:10:11 +0800

    feat: linkedlistmap

--- 13.Map/index.ts
+++ 13.Map/index.ts
@@ -2,13 +2,97 @@ export {};
 const m = new Map();
 
 // 增
-m.set('a', 'aa');
-m.set('b', 'bb');
+m.set("a", "aa");
+m.set("b", "bb");
 
 // 删
-m.delete('b');
+m.delete("b");
 // 删除所有项
 // m.clear();
 
 // 改
-m.set('a', 'aaa');
+m.set("a", "aaa");
+
+class LinkedListMapNode<K = string, V = any> {
+  key: K | null;
+  value: V | null;
+  next: LinkedListMapNode<K, V> | null;
+  constructor(
+    key: K | null = null,
+    value: V | null = null,
+    next: LinkedListMapNode<K, V> | null = null
+  ) {
+    this.key = key;
+    this.value = value;
+    this.next = next;
+  }
+  toString() {
+    return `${JSON.stringify(this.key)} : ${JSON.stringify(this.value)}`;
+  }
+}
+
+export class LinkedListMap<K = string, V = any> {
+  dummyHead: LinkedListMapNode<K, V>;
+  size: number;
+  constructor() {
+    this.dummyHead = new LinkedListMapNode();
+    this.size = 0;
+  }
+  getSize() {
+    return this.size;
+  }
+  isEmpty() {
+    return this.size === 0;
+  }
+  getNode(key: K) {
+    let current = this.dummyHead.next;
+    while (current) {
+      if (current.key === key) {
+        return current;
+      }
+      current = current.next;
+    }
+    return null;
+  }
+  contains(key: K) {
+    return this.getNode(key) != null;
+  }
+  get(key: K) {
+    const node = this.getNode(key);
+    return node == null ? null : node.value;
+  }
+  add(key: K, value: V) {
+    const node = this.getNode(key);
+    if (node == null) {
+      this.dummyHead.next = new LinkedListMapNode(
+        key,
+        value,
+        this.dummyHead.next
+      );
+      this.size++;
+    } else {
+      node.value = value;
+    }
+  }
+  set(key: K, value: V) {
+    const node = this.getNode(key);
+    if (node == null) throw new Error("error");
+    node.value = value;
+  }
+  remove(key: K) {
+    let prev = this.dummyHead;
+    while (prev.next) {
+      if (prev.next.key === key) {
+        break;
+      }
+      prev = prev.next;
+    }
+    if (prev.next) {
+      const node = prev.next;
+      prev.next = node.next;
+      this.size--;
+      return node.value;
+    }
+    return null;
+  }
+}
