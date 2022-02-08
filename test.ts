6807313b26885b4312a016a22634f5cf9859b9ca
meixiang474 <1436098850@qq.com>
2022-02-08 11:28:44 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -105,114 +105,127 @@ export function sortColors(colors: (1 | 2 | 0)[]) {
   return colors;
 }
 
-// backtracking 1 - 5
-export function letterCombinations(digits: string) {
-  if (digits.length === 0) return [];
-  const res: string[] = [];
-  const arr = [
-    "",
-    "",
-    "abc",
-    "def",
-    "ghi",
-    "jkl",
-    "mno",
-    "pqrs",
-    "tuv",
-    "wxyz",
-  ];
-  const dfs = (path: string, index: number) => {
-    if (index === digits.length) {
-      res.push(path);
-      return;
-    }
-    const current = arr[parseInt(digits[index])];
-    for (let i = 0; i < current.length; i++) {
-      const char = current[i];
-      dfs(path + char, index + 1);
+export function findKMax(nums: number[], k: number) {
+  k = nums.length - k;
+  const sortArr = (arr: number[], l: number, r: number): number => {
+    if (l >= r) return arr[l];
+    const p = partition(arr, l, r);
+    if (p === k) {
+      return arr[k];
+    } else if (p > k) {
+      return sortArr(arr, l, p - 1);
+    } else {
+      return sortArr(arr, p + 1, r);
     }
   };
-  dfs("", 0);
-  return res;
-}
-
-export function generateParenthesis(n: number) {
-  if (n === 0) return [];
-  const res: string[] = [];
-  const dfs = (path: string, open: number, close: number) => {
-    if (path.length === n * 2) {
-      res.push(path);
-      return;
-    }
-    if (open < n) {
-      dfs(path + "(", open + 1, close);
-    }
-    if (close < open) {
-      dfs(path + ")", open, close + 1);
-    }
-  };
-  dfs("", 0, 0);
-  return res;
-}
-
-export function permuteUnique(nums: number[]) {
-  if (nums.length === 0) return [];
-  nums.sort((a, b) => a - b);
-  const res: number[][] = [];
-  const set = new Set<number>();
-  const dfs = (path: number[]) => {
-    if (path.length === nums.length) {
-      res.push(path);
-      return;
-    }
-    for (let i = 0; i < nums.length; i++) {
-      if (i > 0 && nums[i - 1] === nums[i] && set.has(i - 1)) continue;
-      if (!set.has(i)) {
-        set.add(i);
-        dfs(path.concat(nums[i]));
-        set.delete(i);
+  const getRandom = (l: number, r: number) =>
+    Math.floor(Math.random() * (r - l + 1) + l);
+  const swap = (arr: number[], i: number, j: number) =>
+    ([arr[i], arr[j]] = [arr[j], arr[i]]);
+  const partition = (arr: number[], l: number, r: number) => {
+    const p = getRandom(l, r);
+    swap(arr, l, p);
+    let i = l + 1,
+      j = r;
+    while (true) {
+      while (i <= j && arr[i] < arr[l]) {
+        i++;
       }
+      while (i <= j && arr[j] > arr[l]) {
+        j--;
+      }
+      if (i >= j) break;
+      swap(arr, i, j);
+      i++;
+      j--;
     }
+    swap(arr, l, j);
+    return j;
   };
-  dfs([]);
-  return res;
+  return sortArr([...nums], 0, nums.length - 1);
 }
 
-export function getPermutation(n: number, k: number) {
-  let groupNum = 1;
-  for (let i = 1; i <= n; i++) {
-    groupNum *= i;
+export function findKMin(nums: number[], k: number) {
+  if (k >= nums.length) return [...nums];
+  const sortArr = (arr: number[], l: number, r: number): number[] => {
+    if (l >= r) return arr.slice(0, k);
+    const p = partition(arr, l, r);
+    if (p === k) {
+      return arr.slice(0, k);
+    } else if (p > k) {
+      return sortArr(arr, l, p - 1);
+    } else {
+      return sortArr(arr, p + 1, r);
+    }
+  };
+  const getRandom = (l: number, r: number) =>
+    Math.floor(Math.random() * (r - l + 1) + l);
+  const swap = (arr: number[], i: number, j: number) =>
+    ([arr[i], arr[j]] = [arr[j], arr[i]]);
+  const partition = (arr: number[], l: number, r: number) => {
+    const p = getRandom(l, r);
+    swap(arr, l, p);
+    let i = l + 1,
+      j = r;
+    while (true) {
+      while (i <= j && arr[i] < arr[l]) {
+        i++;
+      }
+      while (i <= j && arr[j] > arr[l]) {
+        j--;
+      }
+      if (i <= j) break;
+      swap(arr, i, j);
+      i++;
+      j--;
+    }
+    swap(arr, l, j);
+    return j;
+  };
+  return sortArr([...nums], 0, nums.length - 1);
+}
+
+// binary search 1 - 5
+export function myPow(x: number, n: number) {
+  const isNegative = n < 0;
+  n = Math.abs(n);
+  const absPow = (x: number, n: number): number => {
+    if (n === 0) return 1;
+    if (n === 1) return x;
+    const res = absPow(x, Math.floor(n / 2));
+    return n % 2 === 0 ? res * res : res * res * x;
+  };
+  const res = absPow(x, n);
+  return isNegative ? 1 / res : res;
+}
+
+export function mySqrt(x: number) {
+  let l = 0,
+    r = x;
+  while (l < r) {
+    const mid = Math.floor(l + (r - l + 1) / 2);
+    if (mid ** 2 <= x) {
+      l = mid;
+    } else {
+      r = mid - 1;
+    }
   }
-  const dfs = (path: number[]): string => {
-    if (path.length === n) return path.join("");
-    groupNum = groupNum / n - path.length;
-    for (let i = 1; i <= n; i++) {
-      if (path.includes(i)) continue;
-      if (k > groupNum) {
-        k -= groupNum;
-        continue;
-      }
-      return dfs(path.concat(i));
-    }
-    return "";
-  };
-  return dfs([]);
+  return l;
 }
 
-export function combine(n: number, k: number) {
-  const res: number[][] = [];
-  const dfs = (path: number[], start: number) => {
-    if (path.length === k) {
-      res.push(path);
-      return;
+export function twoSum(nums: number[], target: number) {
+  let l = 0,
+    r = nums.length - 1;
+  while (l < r) {
+    const left = nums[l];
+    const right = nums[r];
+    const sum = left + right;
+    if (sum === target) {
+      return [l + 1, r + 1];
+    } else if (sum > target) {
+      r--;
+    } else {
+      l++;
     }
-    if (path.length + n - start + 1 < k) {
-      return;
-    }
-    for (let i = start; i <= n; i++) {
-      dfs(path.concat(i), i + 1);
-    }
-  };
-  dfs([], 1);
-  return res;
+  }
 }
