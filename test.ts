cfa895a72b7bb5a27d723edbfad041521ddb8fac
meixiang474 <1436098850@qq.com>
2022-02-17 13:07:50 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -126,6 +126,114 @@ export class LinkedListMap<K = string, V = any> {
   }
 }
 
+export class BSTMapNode<K = number, V = any> {
+  key: K;
+  value: V;
+  left: BSTMapNode<K, V> | null;
+  right: BSTMapNode<K, V> | null;
+  constructor(key: K, value: V) {
+    this.key = key;
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export class BSTMap<K = number, V = any> {
+  root: BSTMapNode<K, V> | null;
+  size: number;
+  constructor(compare?: (a: K, b: K) => boolean) {
+    this.root = null;
+    this.size = 0;
+    this.compare = compare || this.compare;
+  }
+  compare(a: K, b: K) {
+    return a < b;
+  }
+  add(key: K, value: V) {
+    this.root = this.addNode(key, value, this.root);
+  }
+  addNode(key: K, value: V, node: BSTMapNode<K, V> | null) {
+    if (!node) {
+      this.size++;
+      return new BSTMapNode(key, value);
+    }
+    if (this.compare(key, node.key)) {
+      node.left = this.addNode(key, value, node.left);
+    } else if (this.compare(node.key, key)) {
+      node.right = this.addNode(key, value, node.right);
+    } else {
+      node.value = value;
+    }
+    return node;
+  }
+  getNode(node: BSTMapNode<K, V> | null, key: K): BSTMapNode<K, V> | null {
+    if (!node) return null;
+    if (this.compare(node.key, key)) {
+      return this.getNode(node.right, key);
+    } else if (this.compare(key, node.key)) {
+      return this.getNode(node.left, key);
+    } else {
+      return node;
+    }
+  }
+  contains(key: K) {
+    return this.getNode(this.root, key) != null;
+  }
+  get(key: K) {
+    const node = this.getNode(this.root, key);
+    return node == null ? null : node.value;
+  }
+  set(key: K, value: V) {
+    const node = this.getNode(this.root, key);
+    if (node == null) {
+      throw new Error("error");
+    }
+    node.value = value;
+  }
+  minimumNode(node: BSTMapNode<K, V>): BSTMapNode<K, V> {
+    if (!node.left) return node;
+    return this.minimumNode(node.left);
+  }
+  removeMinNode(node: BSTMapNode<K, V>) {
+    if (!node.left) {
+      this.size--;
+      return node.right;
+    }
+    node.left = this.removeMinNode(node.left);
+    return node;
+  }
+  remove(key: K) {
+    const node = this.getNode(this.root, key);
+    if (node == null) return null;
+    this.root = this.removeNode(this.root, key);
+    return node.value;
+  }
+  removeNode(node: BSTMapNode<K, V> | null, key: K) {
+    if (!node) return null;
+    if (this.compare(key, node.key)) {
+      node.left = this.removeNode(node.left, key);
+      return node;
+    } else if (this.compare(node.key, key)) {
+      node.right = this.removeNode(node.right, key);
+      return node;
+    } else {
+      if (!node.left) {
+        this.size--;
+        return node.right;
+      }
+      if (!node.right) {
+        this.size--;
+        return node.left;
+      }
+      const successor = this.minimumNode(node.right);
+      successor.left = node.left;
+      successor.right = this.removeMinNode(node.right);
+      return successor;
+    }
+  }
+}
+
 // dp 1 - 5
 export function maxSubArr(arr: number[]) {
   const dp = [arr[0]];
