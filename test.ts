5f0612e385591b83f2002d1e63e4282ffc671be8
meixiang474 <1436098850@qq.com>
2022-03-01 10:09:57 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -1,155 +1,40 @@
-// offer 15
-export function harmmingWeight(n: number) {
-  return n
-    .toString(2)
-    .split("")
-    .reduce((memo, current) => {
-      if (current === "1") memo++;
-      return memo;
-    }, 0);
-}
+import { Heap } from "./practice/week5/1.heap";
 
-// fenzhi donggui tanxin huisu
-export class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-  constructor(val: number) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-export function reverseTree(root: TreeNode | null) {
-  if (!root) return null;
-  const dfs = (node: TreeNode) => {
-    const temp = node.left;
-    node.left = node.right;
-    node.right = temp;
-    if (node.left) dfs(node.left);
-    if (node.right) dfs(node.right);
+// offer 16
+export function myPow(x: number, n: number) {
+  const isNegative = n < 0;
+  n = isNegative ? -n : n;
+  const absPow = (x: number, n: number): number => {
+    if (n === 0) return 1;
+    if (n === 1) return x;
+    const res = absPow(x, Math.floor(n / 2));
+    return n % 2 === 0 ? res * res : res * res * x;
   };
-  dfs(root);
-  return root;
+  return isNegative ? 1 / absPow(x, n) : absPow(x, n);
 }
 
-export function isSameTree(p: TreeNode | null, q: TreeNode | null) {
-  if (!p && !q) return true;
-  if (
-    p &&
-    q &&
-    p.val === q.val &&
-    isSameTree(p.left, q.left) &&
-    isSameTree(p.right, q.right)
-  )
-    return true;
-  return false;
-}
-
-export function fn(root: TreeNode | null) {
-  if (!root) return true;
-  const isMirror = (p: TreeNode | null, q: TreeNode | null) => {
-    if (!p && !q) return true;
-    if (
-      p &&
-      q &&
-      p.val === q.val &&
-      isMirror(p.left, q.right) &&
-      isMirror(p.right, q.left)
-    )
-      return true;
-    return false;
-  };
-  return isMirror(root.left, root.right);
-}
-
-export function climbStairs(n: number) {
-  const dp = [1, 1];
-  for (let i = 2; i <= n; i++) {
-    dp[i] = dp[i - 2] + dp[i - 1];
+// priority tree shell sort
+export class PriorityQueue<T = number> {
+  maxHeap: Heap<T>;
+  constructor(compare?: (a: T, b: T) => boolean) {
+    this.maxHeap = new Heap('max', compare)
   }
-  return dp[n];
-}
-
-export function rob(nums: number[]) {
-  if (nums.length === 0) return 0;
-  const dp = [0, nums[0]];
-  for (let i = 2; i <= nums.length; i++) {
-    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+  getSize() {
+    return this.maxHeap.size()
   }
-  return dp[nums.length];
-}
-
-export function rob1(nums: number[]) {
-  if (nums.length === 1) return nums[0];
-  const compute = (nums: number[]) => {
-    const dp = [0, nums[0]];
-    for (let i = 2; i <= nums.length; i++) {
-      dp[i] = Math.max(dp[i - 1] + dp[i - 2] + nums[i - 1]);
-    }
-    return dp[nums.length];
-  };
-  return Math.max(compute(nums.slice(1)), compute(nums.slice(0, -1)));
-}
-
-export function findContentChildren(g: number[], s: number[]) {
-  s.sort((a, b) => a - b);
-  g.sort((a, b) => a - b);
-  let res = 0;
-  s.forEach((item) => {
-    if (item >= g[res]) {
-      res++;
-    }
-  });
-  return res;
-}
-
-export function maxProfit(profits: number[]) {
-  let res = 0;
-  for (let i = 0; i < profits.length; i++) {
-    if (profits[i] < profits[i + 1]) {
-      res += profits[i + 1] - profits[i];
-    }
+  isEmpty() {
+    return this.getSize() === 0
   }
-  return res;
-}
-
-export function permute(nums: number[]) {
-  const res: number[][] = [];
-  const backtrack = (path: number[]) => {
-    if (path.length === nums.length) {
-      res.push(path);
-      return;
-    }
-    nums.forEach((item) => {
-      if (!path.includes(item)) {
-        backtrack(path.concat(item));
-      }
-    });
-  };
-  backtrack([]);
-  return res;
-}
-
-export function subsets(nums: number[]) {
-  const res: number[][] = [];
-  const backtrack = (path: number[], start: number, length: number) => {
-    if (path.length === length) {
-      res.push(path);
-      return;
-    }
-    if (path.length + nums.length - start < length) {
-      return;
-    }
-    for (let i = start; i < nums.length; i++) {
-      backtrack(path.concat(nums[i]), i + 1, length);
-    }
-  };
-  for (let i = 0; i <= nums.length; i++) {
-    backtrack([], 0, i);
+  getFront() {
+    return this.maxHeap.peek()
   }
-  return res;
+  enqueue(item: T) {
+    this.maxHeap.insert(item)
+  }
+  dequeue() {
+    if(this.isEmpty()) throw new Error('error')
+    return this.maxHeap.pop()
+  }
 }
 
 // sliding window 1 - 5
@@ -181,6 +66,71 @@ export function charactorReplacement(s: string, k: number) {
     max = Math.max(max, arr[s[r].charCodeAt(0) - "A".charCodeAt(0)]);
     if (r - l + 1 - max > k) {
       arr[s[l].charCodeAt(0) - "A".charCodeAt(0)]--;
+      l++;
+    }
+    r++;
+  }
+  return r - l;
+}
+
+export function checkInclusion(s1: string, s2: string) {
+  if (s1.length > s2.length) return false;
+  const arr1: number[] = new Array(26).fill(0);
+  const arr2: number[] = new Array(26).fill(0);
+  for (let i = 0; i < s1.length; i++) {
+    arr1[s1[i].charCodeAt(0) - "a".charCodeAt(0)]++;
+    arr2[s2[i].charCodeAt(0) - "a".charCodeAt(0)]++;
+  }
+  if (arr1.toString() === arr2.toString()) return true;
+  for (let i = s1.length; i < s2.length; i++) {
+    arr2[s2[i].charCodeAt(0) - "a".charCodeAt(0)]++;
+    arr2[s2[i - s1.length].charCodeAt(0) - "a".charCodeAt(0)]--;
+    if (arr1.toString() === arr2.toString()) return true;
+  }
+  return false;
+}
+
+export function maxTurbulence(nums: number[]) {
+  let res = 1;
+  let l = 0,
+    r = 0;
+  while (r < nums.length - 1) {
+    const left = nums[l];
+    const right = nums[r];
+    if (l === r) {
+      if (left === nums[l + 1]) {
+        l++;
+      }
+      r++;
+    } else {
+      if (
+        (right > nums[r - 1] && right > nums[r + 1]) ||
+        (right < nums[r - 1] && right < nums[r + 1])
+      ) {
+        r++;
+      } else {
+        l = r;
+      }
+    }
+    res = Math.max(res, r - l + 1);
+  }
+  return res;
+}
+
+export function longestOnes(nums: number[], k: number) {
+  let max = 0,
+    l = 0,
+    r = 0,
+    onesInWindow = 0;
+  while (r < nums.length) {
+    const current = nums[r];
+    if (current === 1) {
+      onesInWindow++;
+    }
+    max = Math.max(max, onesInWindow);
+    if (r - l + 1 - max > k) {
+      const currentl = nums[l];
+      if (currentl === 1) onesInWindow--;
       l++;
     }
     r++;
