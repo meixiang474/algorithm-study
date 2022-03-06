c795ccb1e1abaef00ee75607c46bc9dc06cbdaec
meixiang474 <1436098850@qq.com>
2022-03-06 23:13:21 +0800

    feat: union-find4

--- 25.union-find/index.ts
+++ 25.union-find/index.ts
@@ -88,3 +88,39 @@ export class UnionFind3 {
     }
   }
 }
+
+export class UnionFind4 {
+  parent: number[];
+  // 每课树的高度
+  rank: number[];
+  constructor(size: number) {
+    this.parent = new Array(size).fill(0).map((item, index) => index);
+    this.rank = new Array(size).fill(1);
+  }
+  getSize() {
+    return this.parent.length;
+  }
+  find(p: number) {
+    if (p < 0 || p >= this.parent.length) throw new Error("error");
+    while (p !== this.parent[p]) {
+      p = this.parent[p];
+    }
+    return p;
+  }
+  isConnected(p: number, q: number) {
+    return this.find(p) === this.find(q);
+  }
+  unionElements(p: number, q: number) {
+    const pRoot = this.find(p);
+    const qRoot = this.find(q);
+    if (pRoot === qRoot) return;
+    if (this.rank[pRoot] < this.rank[qRoot]) {
+      this.parent[pRoot] = qRoot;
+    } else if (this.rank[qRoot] < this.rank[pRoot]) {
+      this.parent[qRoot] = pRoot;
+    } else {
+      this.parent[qRoot] = pRoot;
+      this.rank[pRoot] += 1;
+    }
+  }
+}
