5142e15b274dc4cfb57cc9f50467c2657eb25a76
zhengbowen01 <zhengbowen01@MacBook-Pro.local>
2022-03-10 11:02:00 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -298,133 +298,97 @@ export class NumArray2 {
   }
 }
 
-// sort 1 - 5
-
-export function mergeField(fields: number[][]) {
-  fields.sort((a, b) => a[0] - b[0]);
-  const res: number[][] = [];
-  let prevEnd = -Infinity;
-  for (let i = 0; i < fields.length; i++) {
-    const [start, end] = fields[i];
-    if (i > 0 && prevEnd >= start) {
-      res.splice(res.length - 1, 1, [
-        res[res.length - 1][0],
-        Math.max(prevEnd, end),
-      ]);
+// stack 1-5
+export function isValid(s: string) {
+  if (s.length % 2 !== 0) return false;
+  const map = new Map<string, string>();
+  map.set("(", ")");
+  map.set("[", "]");
+  map.set("{", "}");
+  const stack: string[] = [];
+  for (let i = 0; i < s.length; i++) {
+    const current = s[i];
+    if (map.has(current)) {
+      stack.push(current);
     } else {
-      res.push([start, end]);
-    }
-    prevEnd = Math.max(prevEnd, end);
-  }
-  return res;
-}
-
-export function insertField(intervals: number[][], newInterval: number[]) {
-  intervals.push(newInterval);
-  intervals.sort((a, b) => a[0] - b[0]);
-  const res: number[][] = [];
-  let prevEnd = -Infinity;
-  for (let i = 0; i < intervals.length; i++) {
-    const [start, end] = intervals[i];
-    if (prevEnd >= start) {
-      res.splice(res.length - 1, 1, [
-        res[res.length - 1][0],
-        Math.max(prevEnd, end),
-      ]);
-    } else {
-      res.push([start, end]);
-    }
-    prevEnd = Math.max(prevEnd, end);
-  }
-  return res;
-}
-
-export function sortColors(colors: number[]) {
-  let l = -1,
-    i = 0,
-    r = colors.length;
-  const swap = (arr: number[], i: number, j: number) =>
-    ([arr[i], arr[j]] = [arr[j], arr[i]]);
-  while (i < r) {
-    if (colors[i] === 0) {
-      l++;
-      swap(colors, l, i);
-      i++;
-    } else if (colors[i] === 2) {
-      r--;
-      swap(colors, r, i);
-    } else {
-      i++;
+      const prev = stack.pop();
+      if (!prev || map.get(prev) !== current) {
+        return false;
+      }
     }
   }
+  return stack.length === 0;
 }
 
-export class ListNode {
+export function simplifyPath(path: string) {
+  const dirs = path.split("/");
+  const stack: string[] = [];
+  for (let item of dirs) {
+    if (item === "" || item === ".") continue;
+    if (item === "..") {
+      stack.pop();
+      continue;
+    }
+    stack.push(item);
+  }
+  return "/" + stack.join("/");
+}
+
+export class TreeNode {
   val: number;
-  next: ListNode | null;
+  left: TreeNode | null;
+  right: TreeNode | null;
   constructor(val: number) {
     this.val = val;
-    this.next = null;
+    this.left = null;
+    this.right = null;
   }
 }
 
-export function insertionSortList(head: ListNode | null) {
-  if (!head || !head.next) return head;
-  const dummyHead = new ListNode(-1);
-  dummyHead.next = head;
-  let lastSorted = head;
-  let current: ListNode | null = head.next;
-  while (current) {
-    if (lastSorted.val <= current.val) {
-      lastSorted = current;
-      current = current.next;
-    } else {
-      let prev = dummyHead;
-      while (prev.next) {
-        if (prev.next.val > current.val) break;
-        prev = prev.next;
-      }
-      const insertNext = prev.next;
-      const next: ListNode | null = current.next;
-      prev.next = current;
-      current.next = insertNext;
-      current = next;
-      lastSorted.next = next;
+export function inorderTraversal(root: TreeNode | null) {
+  if (!root) return null;
+  const res: number[] = [];
+  const stack: TreeNode[] = [];
+  let p: TreeNode | null = root;
+  while (p || stack.length > 0) {
+    while (p) {
+      stack.push(p);
+      p = p.left;
     }
+    const current = stack.pop()!;
+    res.push(current.val);
+    p = current;
   }
-  return dummyHead.next;
+  return res;
 }
 
-export function sortList(head: ListNode | null) {
-  if (!head || !head.next) return head;
-  let slow = head,
-    fast = head;
-  while (slow && fast && fast.next && fast.next.next) {
-    slow = slow.next!;
-    fast = fast.next.next;
-  }
-  const next = slow.next;
-  const l1 = sortList(head);
-  const l2 = sortList(next);
-  const l3 = new ListNode(-1);
-  let p1 = l1;
-  let p2 = l2;
-  let p3 = l3;
-  while (p1 && p2) {
-    if (p1.val < p2.val) {
-      p3.next = p1;
-      p1 = p1.next;
+export function zigzagLevelTraversal(root: TreeNode | null) {
+  if (!root) return [];
+  const res: number[][] = [];
+  const queue: [TreeNode, number][] = [[root, 0]];
+  while (queue.length > 0) {
+    const [current, level] = queue.shift()!;
+    const arr = res[level] || (res[level] = []);
+    if (level % 2 === 0) {
+      arr.push(current.val);
     } else {
-      p3.next = p2;
-      p2 = p2.next;
+      arr.unshift(current.val);
     }
-    p3 = p3.next;
+    if (current.left) queue.push([current.left, level + 1]);
+    if (current.right) queue.push([current.right, level + 1]);
   }
-  if (p1) {
-    p3.next = p1;
+  return res;
+}
+
+export function preorderTraversal(root: TreeNode | null) {
+  if (!root) return [];
+  const res: number[] = [];
+  const stack: TreeNode[] = [root];
+  while (stack.length > 0) {
+    const current = stack.pop()!;
+    res.push(current.val);
+    if (current.right) stack.push(current.right);
+    if (current.left) stack.push(current.left);
   }
-  if (p2) {
-    p3.next = p2;
-  }
-  return l3.next;
+  return res;
 }
