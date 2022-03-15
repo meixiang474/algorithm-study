b20363ed94bf185cc2f2dde97ccf0c19ec0d2b19
meixiang474 <1436098850@qq.com>
2022-03-15 14:26:00 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -12,7 +12,127 @@ export function exchange(nums: number[]) {
 }
 
 // union-find
+export class UnionFind1 {
+  id: number[];
+  constructor(size: number) {
+    this.id = new Array(size).fill(0).map((item, index) => index);
+  }
+  getSize() {
+    return this.id.length;
+  }
+  find(p: number) {
+    if (p < 0 || p >= this.id.length) throw new Error("error");
+    return this.id[p];
+  }
+  isConnected(p: number, q: number) {
+    return this.find(p) === this.find(q);
+  }
+  unionElements(p: number, q: number) {
+    const pID = this.find(p);
+    const qID = this.find(q);
+    if (pID === qID) return;
+    for (let i = 0; i < this.id.length; i++) {
+      if (this.id[i] === pID) {
+        this.id[i] = qID;
+      }
+    }
+  }
+}
 
+export class UnionFind2 {
+  parent: number[];
+  constructor(size: number) {
+    this.parent = new Array(size).fill(0).map((item, index) => index);
+  }
+  getSize() {
+    return this.parent.length;
+  }
+  find(p: number) {
+    if (p < 0 || p >= this.parent.length) throw new Error("error");
+    while (p !== this.parent[p]) {
+      p = this.parent[p];
+    }
+    return p;
+  }
+  isConnected(p: number, q: number) {
+    return this.find(p) === this.find(q);
+  }
+  unionElements(p: number, q: number) {
+    const pRoot = this.find(p);
+    const qRoot = this.find(q);
+    if (pRoot === qRoot) return;
+    this.parent[pRoot] = qRoot;
+  }
+}
+
+export class UnionFind3 {
+  parent: number[];
+  sz: number[];
+  constructor(size: number) {
+    this.parent = new Array(size).fill(0).map((item, index) => index);
+    this.sz = new Array(size).fill(1);
+  }
+  getSize() {
+    return this.parent.length;
+  }
+  find(p: number) {
+    if (p < 0 || p >= this.parent.length) throw new Error("error");
+    while (p !== this.parent[p]) {
+      p = this.parent[p];
+    }
+    return p;
+  }
+  isConnected(p: number, q: number) {
+    return this.find(p) === this.find(q);
+  }
+  unionElements(p: number, q: number) {
+    const pRoot = this.find(p);
+    const qRoot = this.find(q);
+    if (pRoot === qRoot) return;
+    if (this.sz[pRoot] < this.sz[qRoot]) {
+      this.parent[pRoot] = qRoot;
+      this.sz[qRoot] += this.sz[pRoot];
+    } else {
+      this.parent[qRoot] = pRoot;
+      this.sz[pRoot] += this.sz[qRoot];
+    }
+  }
+}
+
+export class UnionFind4 {
+  parent: number[];
+  rank: number[];
+  constructor(size: number) {
+    this.parent = new Array(size).fill(0).map((item, index) => index);
+    this.rank = new Array(size).fill(1);
+  }
+  getSize() {
+    return this.parent.length;
+  }
+  find(p: number) {
+    if (p < 0 || p >= this.parent.length) throw new Error("error");
+    while (p !== this.parent[p]) {
+      p = this.parent[p];
+    }
+    return p;
+  }
+  isConnected(p: number, q: number) {
+    return this.find(p) === this.find(q);
+  }
+  unionElements(p: number, q: number) {
+    const pRoot = this.find(p);
+    const qRoot = this.find(q);
+    if (pRoot === qRoot) return;
+    if (this.rank[pRoot] < this.rank[qRoot]) {
+      this.parent[pRoot] = qRoot;
+    } else if (this.rank[qRoot] < this.rank[pRoot]) {
+      this.parent[qRoot] = pRoot;
+    } else {
+      this.parent[qRoot] = pRoot;
+      this.rank[pRoot] += 1;
+    }
+  }
+}
 
 // string 1-5
 export function longestSubstring(s: string) {
