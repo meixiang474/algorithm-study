11f13abd4848da10642bb08e4bc76eb87be7b9aa
meixiang474 <1436098850@qq.com>
2022-04-20 18:57:01 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -227,3 +227,20 @@ export function numDecoding(s: string) {
   }
   return dp[s.length];
 }
+
+export function numTrees(n: number) {
+  const dp = [1, 1];
+  for (let i = 2; i <= n; i++) {
+    if (dp[i] == null) {
+      dp[i] = 0;
+    }
+    for (let j = 1; j <= i; j++) {
+      dp[i] += dp[j - 1] * dp[i - j];
+    }
+  }
+  return dp[n];
+}
+
+export function minimumTotal(triangle: number[]) {
+  
+}
