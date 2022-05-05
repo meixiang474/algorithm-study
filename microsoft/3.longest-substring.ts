85e84261caf40cea5b58a178c3bec0679dd62460
meixiang474 <1436098850@qq.com>
2022-05-05 14:00:42 +0800

    leetcode microsoft 3

---
+++ microsoft/3.longest-substring.ts
@@ -0,0 +1,18 @@
+// leetcode 3
+
+export default function lengthOfLongestSubstring(s: string) {
+  const map = new Map<string, number>();
+  let l = 0,
+    r = 0;
+  let res = 0;
+  while (r < s.length) {
+    const current = s[r];
+    if (map.has(current) && map.get(current)! >= l) {
+      l = map.get(current)! + 1;
+    }
+    res = Math.max(res, r - l + 1);
+    map.set(current, r);
+    r++;
+  }
+  return res;
+}
