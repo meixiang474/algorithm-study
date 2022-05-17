bee1d03f1b56f69f74c3d680e9687872250c639b
meixiang474 <1436098850@qq.com>
2022-05-17 18:29:50 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -1,4 +1,8 @@
+import { BST } from "./11.BST";
+import { LinkedList } from "./7.LinkedList";
+
 // offer 32-III
+
 export function levelOrder(root: TreeNode | null) {
   if (!root) return [];
   const queue: [TreeNode, number][] = [[root, 0]];
@@ -17,362 +21,100 @@ export function levelOrder(root: TreeNode | null) {
   return res;
 }
 
-// BST
+// set
 
-interface Visitor<T = number> {
-  visit: (val: T) => void;
+export function fn(nums1: number[], nums2: number[]) {
+  return [...new Set(nums1)].filter((item) => nums2.includes(item));
 }
 
-export class TreeNode<T = number> {
-  val: T;
-  left: TreeNode<T> | null;
-  right: TreeNode<T> | null;
-  constructor(val: T) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
+export function fn1(nums1: number[], nums2: number[]) {
+  return [...new Set(nums1)].filter((item) => !nums2.includes(item));
 }
 
-export class BST<T = number> {
-  root: TreeNode<T> | null;
-  size: number;
+export class BSTSet<T = number> {
+  bst: BST<T>;
   constructor() {
-    this.root = null;
-    this.size = 0;
+    this.bst = new BST();
   }
   getSize() {
-    return this.size;
+    return this.bst.getSize();
   }
   isEmpty() {
-    return this.size === 0;
+    return this.bst.isEmpty();
   }
-  add(val: T) {
-    this.root = this.addNode(this.root, val);
+  add(e: T) {
+    this.bst.add(e);
   }
-  addNode(node: TreeNode<T> | null, val: T) {
-    if (!node) {
-      this.size++;
-      return new TreeNode(val);
-    }
-    if (val === node.val) return node;
-    if (val < node.val) {
-      node.left = this.addNode(node.left, val);
-    } else {
-      node.right = this.addNode(node.right, val);
-    }
-    return node;
+  contains(e: T) {
+    return this.bst.contains(e);
   }
-  contains(val: T) {
-    return this.containsNode(this.root, val);
-  }
-  containsNode(node: TreeNode<T> | null, val: T): boolean {
-    if (!node) return false;
-    if (node.val === val) return true;
-    if (node.val > val) {
-      return this.containsNode(node.left, val);
-    } else {
-      return this.containsNode(node.right, val);
-    }
-  }
-  preOrder(visitor: Visitor<T>) {
-    this.preOrderNode(this.root, visitor);
-  }
-  preOrderNode(node: TreeNode<T> | null, visitor: Visitor<T>) {
-    if (!node) return;
-    visitor.visit(node.val);
-    this.preOrderNode(node.left, visitor);
-    this.preOrderNode(node.right, visitor);
-  }
-  inOrder(visitor: Visitor<T>) {
-    this.inOrderNode(this.root, visitor);
-  }
-  inOrderNode(node: TreeNode<T> | null, visitor: Visitor<T>) {
-    if (!node) return;
-    this.inOrderNode(node.left, visitor);
-    visitor.visit(node.val);
-    this.inOrderNode(node.right, visitor);
-  }
-  postOrder(visitor: Visitor<T>) {
-    this.postOrderNode(this.root, visitor);
-  }
-  postOrderNode(node: TreeNode<T> | null, visitor: Visitor<T>) {
-    if (!node) return;
-    this.postOrderNode(node.left, visitor);
-    this.postOrderNode(node.right, visitor);
-    visitor.visit(node.val);
-  }
-  preOrderNR(visitor: Visitor<T>) {
-    if (!this.root) return;
-    const stack: TreeNode<T>[] = [this.root];
-    while (stack.length) {
-      const current = stack.pop()!;
-      visitor.visit(current.val);
-      if (current.right) stack.push(current.right);
-      if (current.left) stack.push(current.left);
-    }
-  }
-  levelOrder(visitor: Visitor<T>) {
-    if (!this.root) return;
-    const queue: TreeNode<T>[] = [this.root];
-    while (queue.length) {
-      const current = queue.shift()!;
-      visitor.visit(current.val);
-      if (current.left) queue.push(current.left);
-      if (current.right) queue.push(current.right);
-    }
-  }
-  minimum() {
-    if (!this.root) throw new Error("error");
-    return this.minimumNode(this.root).val;
-  }
-  minimumNode(node: TreeNode<T>): TreeNode<T> {
-    if (!node.left) {
-      return node;
-    }
-    return this.minimumNode(node.left);
-  }
-  maximum() {
-    if (!this.root) throw new Error("error");
-    return this.maximumNode(this.root).val;
-  }
-  maximumNode(node: TreeNode<T>): TreeNode<T> {
-    if (!node.right) return node;
-    return this.maximumNode(node.right);
-  }
-  removeMin() {
-    if (!this.root) throw new Error("error");
-    const { res, next } = this.removeMinNode(this.root);
-    this.root = next;
-    return res;
-  }
-  removeMinNode(node: TreeNode<T>): { res: T; next: TreeNode<T> | null } {
-    if (!node.left) {
-      this.size--;
-      return {
-        next: node.right,
-        res: node.val,
-      };
-    }
-    const { res, next } = this.removeMinNode(node.left);
-    node.left = next;
-    return {
-      res,
-      next: node,
-    };
-  }
-  removeMax() {
-    if (!this.root) throw new Error("error");
-    const { res, next } = this.removeMaxNode(this.root);
-    this.root = next;
-    return res;
-  }
-  removeMaxNode(node: TreeNode<T>): { res: T; next: TreeNode<T> | null } {
-    if (!node.right) {
-      this.size--;
-      return {
-        res: node.val,
-        next: node.left,
-      };
-    }
-    const { res, next } = this.removeMaxNode(node.right);
-    node.right = next;
-    return {
-      res,
-      next: node,
-    };
-  }
-  remove(val: T) {
-    this.root = this.removeNode(this.root, val);
-  }
-  removeNode(node: TreeNode<T> | null, val: T): TreeNode<T> | null {
-    if (!node) return null;
-    if (node.val > val) {
-      node.left = this.removeNode(node.left, val);
-      return node;
-    } else if (node.val < val) {
-      node.right = this.removeNode(node.right, val);
-      return node;
-    } else {
-      if (!node.left) {
-        this.size--;
-        return node.right;
-      }
-      if (!node.right) {
-        this.size--;
-        return node.left;
-      }
-      const successor = this.minimumNode(node.right);
-      successor.left = node.left;
-      successor.right = this.removeMinNode(node.right).next;
-      return successor;
-    }
+  remove(e: T) {
+    this.bst.remove(e);
   }
 }
 
-// todo 11.bst
-export function maxDepth(root: TreeNode | null) {
-  if (!root) return 0;
-  let res = 0;
-  const dfs = (node: TreeNode, level: number) => {
-    if (!node.left && !node.right) {
-      res = Math.max(res, level);
-      return;
+export class LinkedListSet<T = number> {
+  list: LinkedList<T>;
+  constructor() {
+    this.list = new LinkedList(-1 as any);
+  }
+  getSize() {
+    return this.list.getSize();
+  }
+  isEmpty() {
+    return this.list.isEmpty();
+  }
+  contains(e: T) {
+    return this.list.contains(e);
+  }
+  add(e: T) {
+    if (!this.list.contains(e)) {
+      this.list.addFirst(e);
     }
-    if (node.left) {
-      dfs(node.left, level + 1);
-    }
-    if (node.right) {
-      dfs(node.right, level + 1);
-    }
-  };
-  dfs(root, 1);
-  return res;
-}
-
-export function minDepth(root: TreeNode | null) {
-  if (!root) return 0;
-  const queue: [TreeNode, number][] = [[root, 1]];
-  while (queue.length) {
-    const [current, level] = queue.shift()!;
-    if (!current.left && !current.right) return level;
-    if (current.left) queue.push([current.left, level + 1]);
-    if (current.right) queue.push([current.right, level + 1]);
+  }
+  remove(e: T) {
+    this.list.removeElement(e);
   }
 }
 
-export function levelOrder1(root: TreeNode | null) {
-  if (!root) return [];
-  const res: number[][] = [];
-  const queue: [TreeNode, number][] = [];
-  while (queue.length) {
-    const [current, level] = queue.shift()!;
-    const arr = res[level] || (res[level] = []);
-    arr.push(current.val);
-    if (current.left) queue.push([current.left, level + 1]);
-    if (current.right) queue.push([current.right, level + 1]);
+export function uniqueMorse(words: string[]) {
+  const arr = [
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--..",
+  ];
+  const set = new Set<string>();
+  for (let item of words) {
+    set.add(
+      item.split("").reduce((memo, current) => {
+        return memo + arr[current.charCodeAt(0) - "a".charCodeAt(0)];
+      }, "")
+    );
   }
-  return res;
+  return set.size;
 }
 
-export function inOrderTraversal(root: TreeNode | null) {
-  if (!root) return [];
-  const res: number[] = [];
-  const dfs = (node: TreeNode) => {
-    if (node.left) dfs(node.left);
-    res.push(node.val);
-    if (node.right) dfs(node.right);
-  };
-  dfs(root);
-  return res;
-}
-
-export function inOrderTraversal1(root: TreeNode | null) {
-  if (!root) return [];
-  const res: number[] = [];
-  const stack: TreeNode[] = [];
-  let p: TreeNode | null = root;
-  while (stack.length || p) {
-    while (p) {
-      stack.push(p);
-      p = p.left;
-    }
-    const current = stack.pop()!;
-    res.push(current.val);
-    p = current.right;
-  }
-  return res;
-}
-
-export function hasPathSum(root: TreeNode | null, target: number) {
-  if (!root) return false;
-  const dfs = (node: TreeNode, sum: number): boolean => {
-    if (!node.left && !node.right && sum === target) return true;
-    if (!node.left && !node.right) return false;
-    let res = false;
-    if (node.left) {
-      res = dfs(node.left, sum + node.left.val);
-    }
-    if (res) return res;
-    if (node.right) {
-      res = dfs(node.right, sum + node.right.val);
-    }
-    return res;
-  };
-  return dfs(root, root.val);
-}
-
-export function postOrderTraversal(root: TreeNode | null) {
-  if (!root) return [];
-  const res: number[] = [];
-  const stack: TreeNode[] = [];
-  let p: TreeNode | null = root;
-  let prevRight: TreeNode | null = null;
-  while (stack.length || p) {
-    while (p) {
-      stack.push(p);
-      p = p.left;
-    }
-    const current = stack.pop()!;
-    if (!current.right || prevRight === current.right) {
-      res.push(current.val);
-      prevRight = current;
-    } else {
-      stack.push(current);
-      p = current.right;
-    }
-  }
-  return res;
-}
-
-// leetcode math 6-10
-
-export function powerOfTwo(n: number): boolean {
-  if (n === 0) return false;
-  if (n === 1) return true;
-  if (n % 2 !== 0) return false;
-  return powerOfTwo(n / 2);
-}
-
-export function isUgly(n: number) {
-  if (n <= 0) return false;
-  const arr = [2, 3, 5];
-  for (let item of arr) {
-    while (n % item === 0) {
-      n /= item;
-    }
-  }
-  return n === 1;
-}
-
-export function powerOfThree(n: number): boolean {
-  if (n === 0) return false;
-  if (n === 1) return true;
-  if (n % 3 !== 0) return false;
-  return powerOfThree(n / 3);
-}
-
-export function integerBreak(n: number) {
-  const arr = [1, 2, 4, 6, 9];
-  if (n <= 6) {
-    return arr[n - 2];
-  }
-  let res = 1;
-  while (n > 6) {
-    res *= 3;
-    n -= 3;
-  }
-  return res * arr[n - 2];
-}
-
-export function countNumbers(n: number) {
-  if (n === 0) return 1;
-  if (n === 1) return 10;
-  let res = 10,
-    current = 9;
-  for (let i = 0; i < n - 1; i++) {
-    current *= 9 - i;
-    res += current;
-  }
-  return res;
-}
+// leetcode sliding-window 6-10
