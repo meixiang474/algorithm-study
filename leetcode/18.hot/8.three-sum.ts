1390d571684fcb55431f023ede828fb7eef75272
meixiang474 <1436098850@qq.com>
2022-06-01 14:02:37 +0800

    leetcode 15

---
+++ leetcode/18.hot/8.three-sum.ts
@@ -0,0 +1,40 @@
+// leetcode 15
+
+export default function threeSum(nums: number[]) {
+  nums.sort((a, b) => a - b);
+  const res: number[][] = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    const current = nums[i];
+    if (i > 0 && current === nums[i - 1]) continue;
+    if (current > 0) break;
+    let l = i + 1,
+      r = nums.length - 1;
+    while (l < r) {
+      const currentl = nums[l];
+      const currentr = nums[r];
+      const sum = currentl + currentr + current;
+      if (sum === 0) {
+        res.push([current, currentl, currentr]);
+        while (l < r) {
+          l++;
+          if (nums[l] !== currentl) break;
+        }
+        while (l < r) {
+          r--;
+          if (nums[r] !== currentr) break;
+        }
+      } else if (sum > 0) {
+        while (l < r) {
+          r--;
+          if (nums[r] !== currentr) break;
+        }
+      } else {
+        while (l < r) {
+          l++;
+          if (nums[l] !== currentl) break;
+        }
+      }
+    }
+  }
+  return res;
+}
