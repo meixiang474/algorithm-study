75e5bc12f8df8e7681183f2ec5944f3618018c3d
meixiang474 <1436098850@qq.com>
2022-06-14 14:23:55 +0800

    leetcode 32

---
+++ leetcode/18.hot/16.longest-valid-parentheses.ts
@@ -0,0 +1,20 @@
+// leetcode 32
+
+export default function longestValidParentheses(s: string[]) {
+  if (s.length === 0) return 0;
+  const dp: number[] = new Array(s.length).fill(0);
+  for (let i = 1; i < s.length; i++) {
+    const current = s[i];
+    const prev = s[i - 1];
+    if (current === ")") {
+      if (prev === "(") {
+        dp[i] = (dp[i - 2] || 0) + 2;
+      } else {
+        if (s[i - dp[i - 1] - 1] === "(") {
+          dp[i] = dp[i - 1] + 2 + (dp[i - dp[i - 1] - 2] || 0);
+        }
+      }
+    }
+  }
+  return Math.max(...dp);
+}
