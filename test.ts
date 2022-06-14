024df94343fa16a032800da45c352601d38782b3
meixiang474 <1436098850@qq.com>
2022-06-14 08:12:17 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -1,224 +1,182 @@
-// offer 34
-export class TreeNode {
+// offer 35
+export class RandomListNode {
   val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
+  next: RandomListNode | null;
+  random: RandomListNode | null;
   constructor(val: number) {
     this.val = val;
-    this.left = null;
-    this.right = null;
+    this.next = null;
+    this.random = null;
   }
 }
 
-export function pathSum(root: TreeNode | null, target: number) {
-  if (!root) return [];
-  const res: number[][] = [];
-  const dfs = (node: TreeNode, path: number[], sum: number) => {
-    if (sum === target && !node.left && !node.right) {
-      res.push(path);
-      return;
+export function copyRandomList(head: RandomListNode | null) {
+  if (!head) return null;
+  const map = new Map<RandomListNode, RandomListNode>();
+  const dfs = (node: RandomListNode) => {
+    const newNode = new RandomListNode(node.val);
+    if (node.next && !map.has(node.next)) {
+      dfs(node.next);
     }
-    if (node.left)
-      dfs(node.left, path.concat(node.left.val), sum + node.left.val);
-    if (node.right)
-      dfs(node.right, path.concat(node.right.val), sum + node.right.val);
-  };
-  dfs(root, [root.val], root.val);
-  return res;
-}
-
-// graph
-export function dfs(
-  graph: Record<number, number[]>,
-  node: number,
-  visited: Set<number>
-) {
-  console.log(node);
-  visited.add(node);
-  graph[node].forEach((item) => {
-    if (!visited.has(item)) {
-      dfs(graph, item, visited);
+    newNode.next = node.next ? map.get(node.next)! : null;
+    if (node.random && !map.has(node.random)) {
+      dfs(node.random);
     }
-  });
-}
-
-export function bfs(graph: Record<number, number[]>, node: number) {
-  const visited: Set<number> = new Set();
-  const queue: number[] = [node];
-  visited.add(node);
-  while (queue.length) {
-    const current = queue.shift()!;
-    console.log(current);
-    graph[current].forEach((item) => {
-      if (!visited.has(item)) {
-        visited.add(item);
-        queue.push(item);
-      }
-    });
-  }
-}
-
-export function fn(matrix: number[][]) {
-  if (matrix.length === 0 || matrix[0].length === 0) return [];
-  const res: [number, number][] = [];
-  const m = matrix.length;
-  const n = matrix[0].length;
-  const flow1: boolean[][] = Array.from({ length: m }, () =>
-    new Array(n).fill(false)
-  );
-  const flow2: boolean[][] = Array.from({ length: m }, () =>
-    new Array(n).fill(false)
-  );
-  const dfs = (r: number, c: number, flow: boolean[][]) => {
-    flow[r][c] = true;
-    [
-      [r + 1, c],
-      [r - 1, c],
-      [r, c + 1],
-      [r, c - 1],
-    ].forEach(([nextR, nextC]) => {
-      if (
-        nextR >= 0 &&
-        nextR < m &&
-        nextC >= 0 &&
-        nextC < n &&
-        !flow[nextR][nextC] &&
-        matrix[nextR][nextC] >= matrix[r][c]
-      ) {
-        dfs(nextR, nextC, flow);
-      }
-    });
+    newNode.random = node.random ? map.get(node.random)! : null;
   };
-  for (let r = 0; r < m; r++) {
-    dfs(r, 0, flow1);
-    dfs(r, n - 1, flow2);
-  }
-  for (let c = 0; c < n; c++) {
-    dfs(0, c, flow1);
-    dfs(m - 1, c, flow2);
-  }
-  for (let r = 0; r < m; r++) {
-    for (let c = 0; c < n; c++) {
-      if (flow1[r][c] && flow2[r][c]) res.push([r, c]);
-    }
-  }
-  return res;
+  dfs(head);
+  return map.get(head);
 }
 
-export class GraphNode {
-  val: number;
-  neighbours: GraphNode[];
-  constructor(val: number) {
-    this.val = val;
-    this.neighbours = [];
-  }
-}
+// heap
 
-export function cloneGraph(node: GraphNode | null) {
-  if (!node) return null;
-  const visited = new Map<GraphNode, GraphNode>();
-  const dfs = (node: GraphNode) => {
-    const newNode = new GraphNode(node.val);
-    visited.set(node, newNode);
-    node.neighbours.forEach((item) => {
-      if (!visited.has(item)) {
-        dfs(item);
-      }
-      newNode.neighbours.push(visited.get(item)!);
-    });
-  };
-  dfs(node);
-  return visited.get(node);
-}
-
-export function cloneGraph1(node: GraphNode | null) {
-  if (!node) return null;
-  const visited = new Map<GraphNode, GraphNode>();
-  const queue: GraphNode[] = [node];
-  visited.set(node, new GraphNode(node.val));
-  while (queue.length) {
-    const current = queue.shift()!;
-    current.neighbours.forEach((item) => {
-      if (!visited.has(item)) {
-        visited.set(item, new GraphNode(item.val));
-        queue.push(item);
-      }
-      visited.get(current)!.neighbours.push(visited.get(item)!);
-    });
-  }
-  return visited.get(node);
-}
 
 // hot 1 2
-
-
-
-// leetcode sort 6-10
-
-export function largestNumber(nums: number[]) {
-  const res = nums
-    .map((item) => item.toString())
-    .sort((a, b) => parseInt(b + a) - parseInt(a + b))
-    .join("");
-  return res[0] === "0" ? "0" : res;
-}
-
-export function containsDuplicate(nums: number[], k: number, t: number) {
-  const getId = (num: number) => {
-    return num < 0
-      ? Math.floor((num + 1) / (t + 1)) - 1
-      : Math.floor(num / (t + 1));
-  };
+export function twoSum(nums: number[], target: number) {
   const map = new Map<number, number>();
   for (let i = 0; i < nums.length; i++) {
     const current = nums[i];
-    const id = getId(current);
-    if (map.has(id)) return true;
-    if (map.has(id + 1) && Math.abs(nums[i] - map.get(id + 1)!) <= t)
-      return true;
-    if (map.has(id - 1) && Math.abs(nums[i] - map.get(id - 1)!) <= t)
-      return true;
-    map.set(id, nums[i]);
-    if (i >= k) {
-      map.delete(getId(nums[i - k]));
+    const rest = target - current;
+    if (map.has(rest)) return [i, map.get(rest)];
+    map.set(current, i);
+  }
+}
+
+export class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val: number) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+export function addTwoNumbers(l1: ListNode | null, l2: ListNode | null) {
+  const l3 = new ListNode(-1);
+  let p1 = l1,
+    p2 = l2,
+    p3 = l3;
+  let carry = 0;
+  while (p1 || p2) {
+    const n1 = p1 ? p1.val : 0;
+    const n2 = p2 ? p2.val : 0;
+    const sum = n1 + n2 + carry;
+    carry = Math.floor(sum / 10);
+    p3.next = new ListNode(sum % 10);
+    if (p1) p1 = p1.next;
+    if (p2) p2 = p2.next;
+    p3 = p3.next;
+  }
+  if (carry) {
+    p3.next = new ListNode(carry);
+  }
+  return l3.next;
+}
+
+// leetcode stack 6-10
+export function postOrderTraversal(root: TreeNode | null) {
+  if (!root) return [];
+  const res: number[] = [];
+  const stack: TreeNode[] = [];
+  let p: TreeNode | null = root;
+  let prevRight: TreeNode | null = null;
+  while (stack.length || p) {
+    while (p) {
+      stack.push(p);
+      p = p.left;
+    }
+    const current = stack.pop()!;
+    if (!current.right || current.right === prevRight) {
+      res.push(current.val);
+      prevRight = current;
+    } else {
+      stack.push(current);
+      p = current.right;
     }
   }
-  return false;
+  return res;
 }
 
-export function isAnagram(s: string, t: string) {
-  const map = new Map<string, number>();
-  for (let item of s) {
-    map.set(item, map.has(item) ? map.get(item)! + 1 : 1);
+export function evalRPN(tokens: string[]) {
+  const stack: string[] = [];
+  for (let item of tokens) {
+    if (isNaN(parseFloat(item))) {
+      const n1 = stack.pop()!;
+      const n2 = stack.pop()!;
+      let res: number = eval(`${n2} ${item} ${n1}`);
+      res = res > 0 ? Math.floor(res) : Math.ceil(res);
+      stack.push(res + "");
+    } else {
+      stack.push(item);
+    }
   }
-  for (let item of t) {
-    if (!map.has(item)) return false;
-    map.set(item, map.get(item)! - 1);
-  }
-  for (let [key, value] of map) {
-    if (value !== 0) return false;
-  }
-  return true;
+  return parseFloat(stack[0]);
 }
 
-export function hIndex(nums: number[]) {
-  nums.sort((a, b) => a - b);
-  let h = 0;
-  let i = nums.length - 1;
-  while (i >= 0 && nums[i] > h) {
-    h++;
-    i--;
+export class MinStack {
+  stack: number[];
+  queue: number[];
+  constructor() {
+    this.stack = [];
+    this.queue = [];
   }
-  return h;
+  push(item: number) {
+    this.stack.push(item);
+    if (!this.queue.length || this.queue[0] >= item) {
+      this.queue.unshift(item);
+    }
+  }
+  pop() {
+    const res = this.stack.pop();
+    if (res === this.queue[0]) this.queue.shift();
+  }
+  top() {
+    return this.stack[this.stack.length - 1];
+  }
+  getMin() {
+    return this.queue[0];
+  }
 }
 
-export function wiggleSort(nums: number[]) {
-  let l = Math.floor((nums.length - 1) / 2);
-  let r = nums.length - 1;
-  nums
-    .slice()
-    .sort((a, b) => a - b)
-    .forEach((item, index, arr) => {
-      nums[index] = index % 2 === 0 ? arr[l--] : arr[r--];
-    });
+export class BSTIterator {
+  current: TreeNode | null;
+  stack: TreeNode[];
+  constructor(root: TreeNode | null) {
+    this.current = root;
+    this.stack = [];
+  }
+  next() {
+    while (this.current) {
+      this.stack.push(this.current);
+      this.current = this.current.left;
+    }
+    const current = this.stack.pop();
+    this.current = current ? current.right : null;
+    return current ? current.val : null;
+  }
+  hasNext() {
+    return this.current != null || this.stack.length !== 0;
+  }
+}
+
+export class MyStack {
+  items: number[];
+  constructor() {
+    this.items = [];
+  }
+  push(item: number) {
+    this.items.push(item);
+    for (let i = 0; i < this.items.length - 1; i++) {
+      this.items.push(this.items.shift()!);
+    }
+  }
+  pop() {
+    return this.items.shift();
+  }
+  top() {
+    return this.items[0];
+  }
+  empty() {
+    return this.items.length === 0;
+  }
 }
