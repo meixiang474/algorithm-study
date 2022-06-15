f41d5a651f8c95c338e3172814b3c18de69a74a2
meixiang474 <1436098850@qq.com>
2022-06-15 11:37:26 +0800

    test

--- imooc/1.greedy/3.island-escape.ts
+++ imooc/1.greedy/3.island-escape.ts
@@ -1,14 +1,32 @@
 // https://www.luogu.com.cn/problem/P1095
 
-export default function islandEscape(m: number, s: number, t: number) {}
+// is not right
 
-const m = 1;
-const t = 0;
-const i = 3;
-const dis = 4;
-const s = 10;
-const a =
-  (m < 2 && (t - i < 3 || s - dis <= 85)) ||
-  (m < 6 && (t - i < 2 || s - dis <= 34)) ||
-  t - i == 0 ||
-  s - dis <= 17;
+export default function islandEscape(m: number, s: number, t: number) {
+  let dis = 0;
+  for (let i = 1; i <= t; i++) {
+    if (m >= 10) {
+      dis += 60;
+      m -= 10;
+    } else if (
+      (m < 2 && (t - i < 3 || s - dis <= 85)) ||
+      (m < 6 && (t - i < 2 || s - dis <= 34)) ||
+      t - i == 0 ||
+      s - dis <= 17
+    )
+      dis += 17;
+    else {
+      m += 4;
+    }
+    if (dis >= s) {
+      return true;
+    }
+  }
+  if (dis < s) {
+    return dis;
+  }
+}
+
+console.log(islandEscape(0, 1000, 11))
+
+
