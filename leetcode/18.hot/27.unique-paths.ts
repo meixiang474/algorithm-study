8ee79ea0e10a6d01c201428fdc63439de944d217
meixiang474 <1436098850@qq.com>
2022-06-22 14:07:27 +0800

    leetcode 62

---
+++ leetcode/18.hot/27.unique-paths.ts
@@ -0,0 +1,17 @@
+// leetcode 62
+
+export default function uniquePaths(m: number, n: number) {
+  const dp: number[][] = Array.from({ length: m }, () => new Array(n).fill(0));
+  for (let r = 0; r < m; r++) {
+    dp[r][0] = 1;
+  }
+  for (let c = 0; c < n; c++) {
+    dp[0][c] = 1;
+  }
+  for (let r = 1; r < m; r++) {
+    for (let c = 1; c < n; c++) {
+      dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
+    }
+  }
+  return dp[m - 1][m - 1];
+}
