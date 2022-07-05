0e21107e95ffa0802cc8893341871538049e5efc
meixiang474 <1436098850@qq.com>
2022-07-05 11:46:04 +0800

    leetcode 141

---
+++ leetcode/18.hot/50.has-cycle.ts
@@ -0,0 +1,22 @@
+// leetcode 141
+
+export class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val: number) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+export default function hasCycle(head: ListNode | null) {
+  if (!head || !head.next) return false;
+  let slow: ListNode | null = head,
+    fast: ListNode | null = head;
+  while (slow && fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+  return false;
+}
