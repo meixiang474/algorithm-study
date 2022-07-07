c1551a4289c1e1ca2d6b872dc4db2f8f69cdcfb2
meixiang474 <1436098850@qq.com>
2022-07-07 10:55:55 +0800

    leetcode 148

---
+++ leetcode/18.hot/53.sort-list.ts
@@ -0,0 +1,41 @@
+// leetcode 148
+
+export class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val: number) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+export default function sortList(head: ListNode | null) {
+  if (!head || !head.next) return head;
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+  while (slow && fast && fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  const next = slow!.next;
+  slow!.next = null;
+  const l1 = sortList(head);
+  const l2 = sortList(next);
+  const dummyHead = new ListNode(-1);
+  let p1 = l1,
+    p2 = l2,
+    p3 = dummyHead;
+  while (p1 && p2) {
+    if (p1.val <= p2.val) {
+      p3.next = p1;
+      p1 = p1.next;
+    } else {
+      p3.next = p2;
+      p2 = p2.next;
+    }
+    p3 = p3.next;
+  }
+  if (p1) p3.next = p1;
+  if (p2) p3.next = p2;
+  return dummyHead.next;
+}
