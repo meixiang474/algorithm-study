fdad1eee757937738920e15214eb6acc4ec4f190
meixiang474 <1436098850@qq.com>
2022-07-12 13:15:11 +0800

    leetcode 200

---
+++ leetcode/18.hot/59.num-islands.ts
@@ -0,0 +1,36 @@
+// leetcode 200
+
+export default function numIslands(grid: string[][]) {
+  if (grid.length === 0 || grid[0].length === 0) return 0;
+  const m = grid.length;
+  const n = grid[0].length;
+  const dfs = (r: number, c: number) => {
+    grid[r][c] = "0";
+    [
+      [r + 1, c],
+      [r - 1, c],
+      [r, c + 1],
+      [r, c - 1],
+    ].forEach(([nextR, nextC]) => {
+      if (
+        nextR >= 0 &&
+        nextR < m &&
+        nextC >= 0 &&
+        nextC < n &&
+        grid[nextR][nextC] === "1"
+      ) {
+        dfs(nextR, nextC);
+      }
+    });
+  };
+  let res = 0;
+  for (let r = 0; r < m; r++) {
+    for (let c = 0; c < n; c++) {
+      if (grid[r][c] === "1") {
+        res++;
+        dfs(r, c);
+      }
+    }
+  }
+  return res;
+}
