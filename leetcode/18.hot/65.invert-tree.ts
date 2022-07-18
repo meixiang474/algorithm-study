feee5f333e74a34fd4cda3649cbce32a2580b58e
meixiang474 <1436098850@qq.com>
2022-07-18 11:43:28 +0800

    leetcode 226

---
+++ leetcode/18.hot/65.invert-tree.ts
@@ -0,0 +1,25 @@
+// leetcode 226
+
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val: number) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export default function invertTree(root: TreeNode | null) {
+  if (!root) return null;
+  const dfs = (node: TreeNode) => {
+    const temp = node.left;
+    node.left = node.right;
+    node.right = temp;
+    if (node.left) dfs(node.left);
+    if (node.right) dfs(node.right);
+  };
+  dfs(root);
+  return root;
+}
--- test.ts
+++ test.ts
@@ -406,5 +406,42 @@ export function lexicalOrder(n: number) {
 // practice week1
 
 export function removeDuplicates(head: ListNode | null) {
-  
+  const dummyHead = new ListNode(-1);
+  dummyHead.next = head;
+  let prev = dummyHead;
+  while (prev.next && prev.next.next) {
+    if (prev.next.val === prev.next.next.val) {
+      prev.next = prev.next.next;
+    } else {
+      prev = prev.next;
+    }
+  }
+  return dummyHead.next;
+}
+
+export function removeDuplicates1(head: ListNode | null) {
+  while (head && head.next && head.val === head.next.val) {
+    head = head.next;
+  }
+  if (!head) return head;
+  let prev = head;
+  while (prev.next && prev.next.next) {
+    if (prev.next.val === prev.next.next.val) {
+      prev.next = prev.next.next;
+    } else {
+      prev = prev.next;
+    }
+  }
+  return head;
+}
+
+export function removeDuplicates2(head: ListNode | null): ListNode | null {
+  if (!head || !head.next) return head;
+  const res = removeDuplicates2(head.next);
+  if (res && head.val === res.val) {
+    return res;
+  } else {
+    head.next = res;
+    return head;
+  }
 }
