23637286b0db30715be910b2d43c5a818a92635e
meixiang474 <1436098850@qq.com>
2022-07-27 15:31:12 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -18,203 +18,166 @@ export class TreeNode {
   }
 }
 
-// offer 47 48
-export function maxValue(grid: number[][]) {
-  if (grid.length === 0 || grid[0].length === 0) return 0;
-  const m = grid.length;
-  const n = grid[0].length;
-  const dp: number[][] = Array.from({ length: m }, () => new Array(n).fill(0));
-  for (let r = 0; r < m; r++) {
-    for (let c = 0; c < n; c++) {
-      if (r === 0 && c === 0) {
-        dp[r][c] = grid[r][c];
-      } else if (r === 0) {
-        dp[r][c] = grid[r][c] + dp[r][c - 1];
-      } else if (c === 0) {
-        dp[r][c] = grid[r][c] + dp[r - 1][c];
-      } else {
-        dp[r][c] = Math.max(dp[r - 1][c], dp[r][c - 1]) + grid[r][c];
+// offer 49 50
+export function nthUglyNumber(n: number) {
+  const dp: number[] = new Array(n).fill(0);
+  dp[0] = 1;
+  let a = 0;
+  let b = 0;
+  let c = 0;
+  for (let i = 1; i < n; i++) {
+    const n1 = dp[a] * 2;
+    const n2 = dp[b] * 3;
+    const n3 = dp[c] * 5;
+    dp[i] = Math.min(n1, n2, n3);
+    if (dp[i] === n1) a++;
+    if (dp[i] === n2) b++;
+    if (dp[i] === n3) c++;
+  }
+  return dp[n - 1];
+}
+
+export function firstUniqueChar(s: string) {
+  const map = new Map<string, number>();
+  for (let item of s) {
+    map.set(item, map.has(item) ? map.get(item)! + 1 : 1);
+  }
+  for (let item of s) {
+    if (map.get(item) === 1) return item;
+  }
+  return " ";
+}
+
+// stack
+export class Stack<T> {
+  items: T[];
+  constructor() {
+    this.items = [];
+  }
+  getSize() {
+    return this.items.length;
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  push(item: T) {
+    this.items.push(item);
+  }
+  pop() {
+    if (this.isEmpty()) throw new Error("error");
+    return this.items.pop()!;
+  }
+  peek() {
+    if (this.isEmpty()) throw new Error("error");
+    return this.items[this.items.length - 1];
+  }
+  toString() {
+    return this.items.toString();
+  }
+}
+
+export function isValid(s: string) {
+  if (s.length % 2 !== 0) return false;
+  const stack: string[] = [];
+  const map = new Map<string, string>();
+  map.set("(", ")");
+  map.set("{", "}");
+  map.set("[", "]");
+  for (let i = 0; i < s.length; i++) {
+    const item = s[i];
+    if (map.has(item)) {
+      stack.push(item);
+    } else {
+      const prev = stack.pop();
+      if (!prev || map.get(prev) !== item) return false;
+    }
+  }
+  return stack.length === 0;
+}
+
+export class MinStack {
+  items: number[];
+  queue: number[];
+  constructor() {
+    this.items = [];
+    this.queue = [];
+  }
+  getSize() {
+    return this.items.length;
+  }
+  isEmpty() {
+    return this.getSize() === 0;
+  }
+  push(item: number) {
+    this.items.push(item);
+    if (this.queue.length === 0 || item <= this.queue[0]) {
+      this.queue.unshift(item);
+    }
+  }
+  pop() {
+    if (this.isEmpty()) throw new Error("error");
+    const res = this.items.pop();
+    if (res === this.queue[0]) this.queue.shift();
+    return res;
+  }
+  peek() {
+    if (this.isEmpty()) throw new Error("error");
+    return this.items[this.items.length - 1];
+  }
+  getMin() {
+    return this.queue[0];
+  }
+}
+
+export class CustomStack {
+  items: number[];
+  maxSize: number;
+  constructor(maxSize: number) {
+    this.items = [];
+    this.maxSize = maxSize;
+  }
+  push(item: number) {
+    if (this.items.length >= this.maxSize) return;
+    this.items.push(item);
+  }
+  pop() {
+    if (this.items.length === 0) return -1;
+    return this.items.pop()!;
+  }
+  inc(k: number, val: number) {
+    if (this.items.length <= k) {
+      this.items = this.items.map((v) => v + k);
+    } else {
+      for (let i = 0; i < k; i++) {
+        this.items[i] += val;
       }
     }
   }
-  return dp[m - 1][n - 1];
 }
 
-export function lengthOfLongestSubstring(s: string) {
-  const map = new Map<string, number>();
-  let l = 0,
-    r = 0;
-  let res = 0;
-  while (r < s.length) {
-    const current = s[r];
-    if (map.has(current) && map.get(current)! >= l) {
-      l = map.get(current)! + 1;
-    }
-    res = Math.max(res, r - l + 1);
-    map.set(current, r);
-    r++;
+export function preOrderTraversal(root: TreeNode | null) {
+  if (!root) return [];
+  const stack: TreeNode[] = [root];
+  const res: number[] = [];
+  while (stack.length) {
+    const current = stack.pop()!;
+    res.push(current.val);
+    if (current.right) stack.push(current.right);
+    if (current.left) stack.push(current.left);
   }
   return res;
 }
 
-// array
-export class MyArray<T = number> {
-  data: (T | null)[];
-  size: number;
-  constructor(capacity = 10) {
-    this.data = new Array(capacity).fill(null);
-    this.size = 0;
+export function decToBi(num: number) {
+  const queue: number[] = [];
+  while (num) {
+    queue.unshift(num % 2);
+    num = Math.floor(num / 2);
   }
-  getCapacity() {
-    return this.data.length;
-  }
-  getSize() {
-    return this.size;
-  }
-  isEmpty() {
-    return this.size === 0;
-  }
-  resize(newCapacity: number) {
-    const newData: (T | null)[] = new Array(newCapacity).fill(null);
-    for (let i = 0; i < this.size; i++) {
-      newData[i] = this.data[i];
-    }
-    this.data = newData;
-  }
-  add(index: number, item: T) {
-    if (index < 0 || index > this.size) throw new Error("error");
-    if (this.size >= this.data.length) {
-      this.resize(2 * this.data.length);
-    }
-    for (let i = this.size; i > index; i--) {
-      this.data[i] = this.data[i - 1];
-    }
-    this.data[index] = item;
-    this.size++;
-  }
-  addFirst(item: T) {
-    this.add(0, item);
-  }
-  addLast(item: T) {
-    this.add(this.size, item);
-  }
-  get(index: number) {
-    if (index < 0 || index >= this.size) throw new Error("error");
-    return this.data[index];
-  }
-  getFirst() {
-    return this.get(0);
-  }
-  getLast() {
-    return this.get(this.size - 1);
-  }
-  contains(item: T) {
-    for (let i = 0; i < this.size; i++) {
-      if (this.data[i] === item) return true;
-    }
-    return false;
-  }
-  find(item: T) {
-    for (let i = 0; i < this.size; i++) {
-      if (this.data[i] === item) return i;
-    }
-    return -1;
-  }
-  remove(index: number) {
-    if (index < 0 || index >= this.size) throw new Error("error");
-    const res = this.data[index];
-    for (let i = index; i < this.size; i++) {
-      this.data[i] = this.data[i + 1];
-    }
-    this.size--;
-    if (
-      this.size <= Math.floor(this.data.length / 4) &&
-      Math.floor(this.data.length / 2) !== 0
-    ) {
-      this.resize(Math.floor(this.data.length / 2));
-    }
-    return res;
-  }
-  removeFirst() {
-    return this.remove(0);
-  }
-  removeLast() {
-    return this.remove(this.size - 1);
-  }
-  removeElement(item: T) {
-    const index = this.find(item);
-    if (index === -1) return false;
-    this.remove(index);
-    return true;
-  }
-  set(index: number, item: T) {
-    if (index < 0 || index >= this.size) throw new Error("error");
-    this.data[index] = item;
-  }
-  toString() {
-    let res = `MyArray: size=${this.size}, capacity=${this.getCapacity()}\r\n`;
-    res += "[";
-    for (let i = 0; i < this.size; i++) {
-      res += JSON.stringify(this.data[i]) + ",";
-    }
-    res = res.slice(0, -1) + "]";
-    return res;
-  }
+  return parseFloat(queue.join(""));
 }
 
-// hot 21-24
-export function permute(nums: number[]) {
-  const res: number[][] = [];
-  const dfs = (path: number[]) => {
-    if (path.length === nums.length) {
-      res.push(path);
-      return;
-    }
-    for (let i = 0; i < nums.length; i++) {
-      if (!path.includes(nums[i])) dfs(path.concat(nums[i]));
-    }
-  };
-  dfs([]);
-  return res;
-}
-
-export function rotate(matrix: number[][]) {
-  const n = matrix.length;
-  for (let i = 0; i < Math.floor(n / 2); i++) {
-    for (let j = 0; j < Math.floor((n + 1) / 2); j++) {
-      const temp = matrix[i][j];
-      matrix[i][j] = matrix[n - j - 1][i];
-      matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
-      matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
-      matrix[j][n - i - 1] = temp;
-    }
-  }
-}
-
-export function groupAnagrams(nums: string[]) {
-  const map = new Map<string, string[]>();
-  for (let item of nums) {
-    const key = item.split("").sort().join("");
-    if (map.has(key)) {
-      map.get(key)!.push(item);
-    } else {
-      map.set(key, [item]);
-    }
-  }
-  const res: string[][] = [];
-  for (let [key, value] of map) {
-    res.push(value);
-  }
-  return res;
-}
-
-export function maxSubarray(nums: number[]) {
-  const dp: number[] = [nums[0]];
-  for (let i = 1; i < nums.length; i++) {
-    dp[i] = dp[i - 1] >= 0 ? dp[i - 1] + nums[i] : nums[i];
-  }
-  return Math.max(...dp);
-}
+// hot 25-28
 
 // leetcode daily 11-15
 export class Solution {
