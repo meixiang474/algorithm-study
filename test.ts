0aa2aaff350d5e87152b5d19be470169e2a7c6ac
meixiang474 <1436098850@qq.com>
2022-08-10 15:01:17 +0800

    practice

--- test.ts
+++ test.ts
@@ -209,181 +209,42 @@ export function mergeField(intervals: number[][]) {
 }
 
 export function uniquePaths(m: number, n: number) {
-
-}
-
-// leetcode daily 11-15
-export class Solution {
-  nums: number[];
-  constructor(nums: number[]) {
-    this.nums = nums;
-  }
-  pick(target: number) {
-    let count = 0;
-    let res = -1;
-    for (let i = 0; i < this.nums.length; i++) {
-      if (this.nums[i] === target) {
-        count++;
-        if (Math.floor(Math.random() * count) === 0) res = i;
-      }
-    }
-    return res;
-  }
-}
-
-export function projectionArea(grid: number[][]) {
-  if (grid.length === 0) return 0;
-  const n = grid.length;
-  const rowMax: number[] = new Array(n).fill(0);
-  const colMax: number[] = new Array(n).fill(0);
-  let topArea = 0;
-  for (let r = 0; r < n; r++) {
-    for (let c = 0; c < n; c++) {
-      const current = grid[r][c];
-      if (current !== 0) topArea++;
-      rowMax[r] = Math.max(rowMax[r], current);
-      colMax[c] = Math.max(colMax[c], current);
-    }
-  }
-  return (
-    topArea + rowMax.reduce((a, b) => a + b) + colMax.reduce((a, b) => a + b)
-  );
-}
-
-export function percificAtlantic(grid: number[][]) {
-  if (grid.length === 0 || grid[0].length === 0) return [];
-  const m = grid.length;
-  const n = grid[0].length;
-  const flow1: boolean[][] = Array.from({ length: m }, () =>
-    new Array(n).fill(false)
-  );
-  const flow2: boolean[][] = Array.from({ length: m }, () =>
-    new Array(n).fill(false)
-  );
-  const dfs = (r: number, c: number, flow: boolean[][]) => {
-    flow[r][c] = true;
-    [
-      [r + 1, c],
-      [r - 1, c],
-      [r, c + 1],
-      [r, c - 1],
-    ].forEach(([nextR, nextC]) => {
-      if (
-        nextR >= 0 &&
-        nextR < m &&
-        nextC >= 0 &&
-        nextC < n &&
-        grid[r][c] <= grid[nextR][nextC] &&
-        !flow[nextR][nextC]
-      ) {
-        dfs(nextR, nextC, flow);
-      }
-    });
-  };
+  if (m === 0 || n === 0) return 0;
+  const dp: number[][] = Array.from({ length: m }, () => new Array(n).fill(0));
   for (let r = 0; r < m; r++) {
-    dfs(r, 0, flow1);
-    dfs(r, n - 1, flow2);
+    dp[r][0] = 1;
   }
   for (let c = 0; c < n; c++) {
-    dfs(0, c, flow1);
-    dfs(m - 1, c, flow2);
+    dp[0][c] = 1;
   }
-  const res: number[][] = [];
-  for (let r = 0; r < m; r++) {
-    for (let c = 0; c < n; c++) {
-      if (flow1[r][c] && flow2[r][c]) res.push([r, c]);
+  for (let r = 1; r < m; r++) {
+    for (let c = 1; c < n; c++) {
+      dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
     }
   }
-  return res;
+  return dp[m - 1][n - 1];
 }
 
-export function sortArrayByParity(nums: number[]) {
-  return nums.sort((a, b) => {
-    if (a % 2 === b % 2) return 0;
-    if (a % 2 === 0) return -1;
-    return 0;
-  });
-}
-
-export class TreeNode1 {
-  val: boolean;
-  isLeaf: boolean;
-  topLeft: TreeNode1 | null;
-  topRight: TreeNode1 | null;
-  bottomLeft: TreeNode1 | null;
-  bottomRight: TreeNode1 | null;
-  constructor(
-    val: boolean,
-    isLeaf: boolean,
-    topLeft: TreeNode1 | null = null,
-    topRight: TreeNode1 | null = null,
-    bottomLeft: TreeNode1 | null = null,
-    bottomRight: TreeNode1 | null = null
-  ) {
-    this.val = val;
-    this.isLeaf = isLeaf;
-    this.topLeft = topLeft;
-    this.topRight = topRight;
-    this.bottomLeft = bottomLeft;
-    this.bottomRight = bottomRight;
+export function minPathSum(grid: number[][]) {
+  if(grid.length === 0 || grid[0].length === 0) return 0
+  const m = grid.length
+  const n = grid[0].length
+  const dp: number[][] = Array.from({length: m}, () => new Array(n).fill(0))
+  for(let r = 1; r < m; r++) {
+    dp[r][0] = dp[r - 1][0] + grid[r][0]
   }
+  for(let c = 1; c < n; c++) {
+    dp[0][c] = dp[0][c - 1] + grid[0][c]
+  }
+  for(let r = 1; r < m; r++) {
+    for(let c = 1; c < n; c++) {
+      dp[r][c] = Math.min(dp[r - 1][c], dp[r][c - 1]) + grid[r][c]
+    }
+  }
+  return dp[m - 1][n - 1]
 }
 
-export function fourTree(grid: number[][]) {
-  const dfs = (
-    grid: number[][],
-    r0: number,
-    c0: number,
-    r1: number,
-    c1: number
-  ): TreeNode1 => {
-    let same = true;
-    for (let r = r0; r <= r1; r++) {
-      for (let c = c0; c <= c1; c++) {
-        if (grid[r][c] !== grid[r0][c0]) {
-          same = false;
-          break;
-        }
-      }
-    }
-    if (same) {
-      return new TreeNode1(grid[r0][c0] === 1, true);
-    }
-    return new TreeNode1(
-      true,
-      false,
-      dfs(
-        grid,
-        r0,
-        c0,
-        Math.floor(r0 + (r1 - r0) / 2),
-        Math.floor(c0 + (c1 - c0) / 2)
-      ),
-      dfs(
-        grid,
-        r0,
-        Math.floor(c0 + (c1 - c0) / 2) + 1,
-        Math.floor(r0 + (r1 - r0) / 2),
-        c1
-      ),
-      dfs(
-        grid,
-        Math.floor(r0 + (r1 - r0) / 2) + 1,
-        c0,
-        r1,
-        Math.floor(c0 + (c1 - c0) / 2)
-      ),
-      dfs(
-        grid,
-        Math.floor(r0 + (r1 - r0) / 2) + 1,
-        Math.floor(c0 + (c1 - c0) / 2) + 1,
-        r1,
-        c1
-      )
-    );
-  };
-  return dfs(grid, 0, 0, grid.length - 1, grid.length - 1);
-}
+// leetcode daily 16-20
 
 // practice week3
 export class QueueBasedOnStack {
