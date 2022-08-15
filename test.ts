deceeeb8466924776e35afc92fa5a62c3c0caf95
meixiang474 <1436098850@qq.com>
2022-08-15 20:09:15 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -107,8 +107,153 @@ export class LoopQueue<T> {
   front: number;
   tail: number;
   constructor(capacity = 10) {
-    this.data = new Array(capacity + 1).fill(null)
-    this.front = this.tail = 0
+    this.data = new Array(capacity + 1).fill(null);
+    this.front = this.tail = 0;
+  }
+  getCapacity() {
+    return this.data.length - 1;
+  }
+  getSize() {
+    return this.tail >= this.front
+      ? this.tail - this.front
+      : this.tail - this.front + this.data.length;
+  }
+  isEmpty() {
+    return this.getSize() === 0;
+  }
+  resize(newCapacity: number) {
+    const newData: (T | null)[] = new Array(newCapacity + 1).fill(null);
+    for (let i = 0; i < this.getSize(); i++) {
+      newData[i] = this.data[(this.front + i) % this.data.length];
+    }
+    this.tail = this.getSize();
+    this.front = 0;
+    this.data = newData;
+  }
+  enqueue(item: T) {
+    if (this.getSize() >= this.getCapacity()) {
+      this.resize(this.getCapacity() * 2);
+    }
+    this.data[this.tail] = item;
+    this.tail = (this.tail + 1) % this.data.length;
+  }
+  dequeue() {
+    if (this.isEmpty()) throw new Error("error");
+    const res = this.data[this.front]!;
+    this.data[this.front] = null;
+    this.front = (this.front + 1) % this.data.length;
+    if (
+      this.getSize() <= Math.floor(this.getCapacity() / 4) &&
+      Math.floor(this.getCapacity() / 2) !== 0
+    ) {
+      this.resize(Math.floor(this.getCapacity() / 2));
+    }
+    return res;
+  }
+  getFront() {
+    if (this.isEmpty()) throw new Error("error");
+    return this.data[this.front]!;
+  }
+  toString() {
+    let res = `LoopQueue: size=${this.getSize()}, capacity=${this.getCapacity()}\r\n`;
+    res += "front [";
+    for (let i = 0; i < this.getSize(); i++) {
+      res +=
+        JSON.stringify(this.data[(this.front + i) % this.data.length]) + ",";
+    }
+    res = res.slice(0, -1) + "] tail";
+    return res;
+  }
+}
+
+export class Deque<T> {
+  data: (T | null)[];
+  front: number;
+  tail: number;
+  size: number;
+  constructor(capacity = 10) {
+    this.data = new Array(capacity).fill(null);
+    this.front = this.tail = this.size = 0;
+  }
+  getCapacity() {
+    return this.data.length;
+  }
+  getSize() {
+    return this.size;
+  }
+  isEmpty() {
+    return this.getSize() === 0;
+  }
+  resize(newCapacity: number) {
+    const newData: (T | null)[] = new Array(newCapacity).fill(null);
+    for (let i = 0; i < this.getSize(); i++) {
+      newData[i] = this.data[(this.front + i) % this.data.length];
+    }
+    this.front = 0;
+    this.tail = this.getSize();
+    this.data = newData;
+  }
+  addLast(item: T) {
+    if (this.getSize() >= this.getCapacity())
+      this.resize(this.getCapacity() * 2);
+    this.data[this.tail] = item;
+    this.tail = (this.tail + 1) % this.data.length;
+    this.size++;
+  }
+  addFront(item: T) {
+    if (this.getSize() >= this.getCapacity()) {
+      this.resize(this.getCapacity() * 2);
+    }
+    this.front = this.front > 0 ? this.front - 1 : this.data.length - 1;
+    this.data[this.front] = item;
+    this.size++;
+  }
+  removeFront() {
+    if (this.isEmpty()) throw new Error("error");
+    const res = this.data[this.front];
+    this.data[this.front] = null;
+    this.size--;
+    this.front = (this.front + 1) % this.data.length;
+    if (
+      this.getSize() <= Math.floor(this.getCapacity() / 4) &&
+      Math.floor(this.getCapacity() / 2) !== 0
+    ) {
+      this.resize(Math.floor(this.getCapacity() / 2));
+    }
+    return res;
+  }
+  removeLast() {
+    if (this.isEmpty()) throw new Error("error");
+    this.tail = this.tail > 0 ? this.tail - 1 : this.data.length - 1;
+    const res = this.data[this.tail];
+    this.data[this.tail] = null;
+    this.size--;
+    if (
+      this.getSize() <= Math.floor(this.getCapacity() / 4) &&
+      Math.floor(this.getCapacity() / 2) !== 0
+    ) {
+      this.resize(Math.floor(this.getCapacity() / 2));
+    }
+    return res;
+  }
+  getFront() {
+    if (this.isEmpty()) throw new Error("error");
+    return this.data[this.front]!;
+  }
+  getLast() {
+    if (this.isEmpty()) throw new Error("error");
+    const index = this.tail > 0 ? this.tail - 1 : this.data.length - 1;
+    return this.data[index]!;
+  }
+  toString() {
+    let res = `Deque: size=${this.getSize()}, capacity=${this.getCapacity()}\r\n`;
+    res += "front [";
+    for (let i = 0; i < this.getSize(); i++) {
+      res +=
+        JSON.stringify(this.data[(i + this.front) % this.data.length]) + ",";
+    }
+    res = res.slice(0, -1) + "] tail";
+    return res;
   }
 }
 
