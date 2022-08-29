dab2319f241a92ea2b4763c9a5155883ae216b76
meixiang474 <1436098850@qq.com>
2022-08-29 20:15:31 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -18,314 +18,58 @@ export class TreeNode {
   }
 }
 
-// offer 51 52
-export function reversePairs(nums: number[]) {
-  const sortArr = (arr: number[], l: number, r: number, temp: number[]) => {
-    if (l >= r) return;
-    const mid = Math.floor(l + (r - l) / 2);
-    sortArr(arr, l, mid, temp);
-    sortArr(arr, mid + 1, r, temp);
-    if (arr[mid] > arr[mid + 1]) {
-      merge(arr, l, mid, r, temp);
-    }
-  };
-  const merge = (
-    arr: number[],
-    l: number,
-    mid: number,
-    r: number,
-    temp: number[]
-  ) => {
-    for (let i = l; i <= r; i++) {
-      temp[i] = arr[i];
-    }
-    let i = l,
-      j = mid + 1;
-    for (let k = l; k <= r; k++) {
-      if (i > mid) {
-        arr[k] = temp[j];
-        j++;
-      } else if (j > r) {
-        arr[k] = temp[i];
-        i++;
-      } else if (temp[i] > temp[j]) {
-        res += mid - i + 1;
-        arr[k] = temp[j];
-        j++;
+// offer 53-I 53-II
+export function search(nums: number[], target: number) {
+  const floor = (nums: number[], target: number) => {
+    let l = -1,
+      r = nums.length - 1;
+    while (l < r) {
+      const mid = Math.floor(l + (r - l + 1) / 2);
+      if (nums[mid] < target) {
+        l = mid;
       } else {
-        arr[k] = temp[i];
-        i++;
+        r = mid - 1;
       }
     }
+    return l;
   };
-  let res = 0;
-  sortArr([...nums], 0, nums.length - 1, [...nums]);
-  return res;
-}
-
-export function getIntersectionNode(l1: ListNode | null, l2: ListNode | null) {
-  if (!l1 || !l2) return null;
-  let p1: ListNode | null = l1;
-  let p2: ListNode | null = l2;
-  while (p1 !== p2) {
-    p1 = p1 ? p1.next : l2;
-    p2 = p2 ? p2.next : l1;
-  }
-  return p1;
-}
-
-// queue
-export class Queue<T> {
-  items: T[];
-  constructor() {
-    this.items = [];
-  }
-  enqueue(item: T) {
-    this.items.push(item);
-  }
-  dequeue() {
-    if (this.items.length === 0) throw new Error("error");
-    return this.items.shift()!;
-  }
-  getFront() {
-    if (this.items.length === 0) throw new Error("error");
-    return this.items[0];
-  }
-  getSize() {
-    return this.items.length;
-  }
-  isEmpty() {
-    return this.getSize() === 0;
-  }
-  toString() {
-    return this.items.toString();
+  const ceil = (nums: number[], target: number) => {
+    let l = 0,
+      r = nums.length;
+    while (l < r) {
+      const mid = Math.floor(l + (r - l) / 2);
+      if (nums[mid] > target) {
+        r = mid;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return l;
+  };
+  const left = floor(nums, target);
+  const right = ceil(nums, target);
+  if (nums[target + 1] === target) {
+    return right - left - 1;
+  } else {
+    return 0;
   }
 }
 
-export class LoopQueue<T> {
-  data: (T | null)[];
-  front: number;
-  tail: number;
-  constructor(capacity = 10) {
-    this.data = new Array(capacity + 1).fill(null);
-    this.front = this.tail = 0;
-  }
-  getCapacity() {
-    return this.data.length - 1;
-  }
-  getSize() {
-    return this.tail >= this.front
-      ? this.tail - this.front
-      : this.tail - this.front + this.data.length;
-  }
-  isEmpty() {
-    return this.getSize() === 0;
-  }
-  resize(newCapacity: number) {
-    const newData: (T | null)[] = new Array(newCapacity + 1).fill(null);
-    for (let i = 0; i < this.getSize(); i++) {
-      newData[i] = this.data[(this.front + i) % this.data.length];
+export function missingNumber(nums: number[]) {
+  let l = 0,
+    r = nums.length;
+  while (l < r) {
+    const mid = Math.floor(l + (r - l) / 2);
+    if (nums[mid] === mid) {
+      l = mid + 1;
+    } else {
+      r = mid;
     }
-    this.tail = this.getSize();
-    this.front = 0;
-    this.data = newData;
   }
-  enqueue(item: T) {
-    if (this.getSize() >= this.getCapacity()) {
-      this.resize(this.getCapacity() * 2);
-    }
-    this.data[this.tail] = item;
-    this.tail = (this.tail + 1) % this.data.length;
-  }
-  dequeue() {
-    if (this.isEmpty()) throw new Error("error");
-    const res = this.data[this.front]!;
-    this.data[this.front] = null;
-    this.front = (this.front + 1) % this.data.length;
-    if (
-      this.getSize() <= Math.floor(this.getCapacity() / 4) &&
-      Math.floor(this.getCapacity() / 2) !== 0
-    ) {
-      this.resize(Math.floor(this.getCapacity() / 2));
-    }
-    return res;
-  }
-  getFront() {
-    if (this.isEmpty()) throw new Error("error");
-    return this.data[this.front]!;
-  }
-  toString() {
-    let res = `LoopQueue: size=${this.getSize()}, capacity=${this.getCapacity()}\r\n`;
-    res += "front [";
-    for (let i = 0; i < this.getSize(); i++) {
-      res +=
-        JSON.stringify(this.data[(this.front + i) % this.data.length]) + ",";
-    }
-    res = res.slice(0, -1) + "] tail";
-    return res;
-  }
+  return l;
 }
 
-export class Deque<T> {
-  data: (T | null)[];
-  front: number;
-  tail: number;
-  size: number;
-  constructor(capacity = 10) {
-    this.data = new Array(capacity).fill(null);
-    this.front = this.tail = this.size = 0;
-  }
-  getCapacity() {
-    return this.data.length;
-  }
-  getSize() {
-    return this.size;
-  }
-  isEmpty() {
-    return this.getSize() === 0;
-  }
-  resize(newCapacity: number) {
-    const newData: (T | null)[] = new Array(newCapacity).fill(null);
-    for (let i = 0; i < this.getSize(); i++) {
-      newData[i] = this.data[(this.front + i) % this.data.length];
-    }
-    this.front = 0;
-    this.tail = this.getSize();
-    this.data = newData;
-  }
-  addLast(item: T) {
-    if (this.getSize() >= this.getCapacity())
-      this.resize(this.getCapacity() * 2);
-    this.data[this.tail] = item;
-    this.tail = (this.tail + 1) % this.data.length;
-    this.size++;
-  }
-  addFront(item: T) {
-    if (this.getSize() >= this.getCapacity()) {
-      this.resize(this.getCapacity() * 2);
-    }
-    this.front = this.front > 0 ? this.front - 1 : this.data.length - 1;
-    this.data[this.front] = item;
-    this.size++;
-  }
-  removeFront() {
-    if (this.isEmpty()) throw new Error("error");
-    const res = this.data[this.front];
-    this.data[this.front] = null;
-    this.size--;
-    this.front = (this.front + 1) % this.data.length;
-    if (
-      this.getSize() <= Math.floor(this.getCapacity() / 4) &&
-      Math.floor(this.getCapacity() / 2) !== 0
-    ) {
-      this.resize(Math.floor(this.getCapacity() / 2));
-    }
-    return res;
-  }
-  removeLast() {
-    if (this.isEmpty()) throw new Error("error");
-    this.tail = this.tail > 0 ? this.tail - 1 : this.data.length - 1;
-    const res = this.data[this.tail];
-    this.data[this.tail] = null;
-    this.size--;
-    if (
-      this.getSize() <= Math.floor(this.getCapacity() / 4) &&
-      Math.floor(this.getCapacity() / 2) !== 0
-    ) {
-      this.resize(Math.floor(this.getCapacity() / 2));
-    }
-    return res;
-  }
-  getFront() {
-    if (this.isEmpty()) throw new Error("error");
-    return this.data[this.front]!;
-  }
-  getLast() {
-    if (this.isEmpty()) throw new Error("error");
-    const index = this.tail > 0 ? this.tail - 1 : this.data.length - 1;
-    return this.data[index]!;
-  }
-  toString() {
-    let res = `Deque: size=${this.getSize()}, capacity=${this.getCapacity()}\r\n`;
-    res += "front [";
-    for (let i = 0; i < this.getSize(); i++) {
-      res +=
-        JSON.stringify(this.data[(i + this.front) % this.data.length]) + ",";
-    }
-    res = res.slice(0, -1) + "] tail";
-    return res;
-  }
-}
-
-export class StackBasedOnQueue<T = number> {
-  queue: T[];
-  constructor() {
-    this.queue = [];
-  }
-  getSize() {
-    return this.queue.length;
-  }
-  isEmpty() {
-    return this.getSize() === 0;
-  }
-  push(item: T) {
-    this.queue.push(item);
-  }
-  pop() {
-    if (this.isEmpty()) throw new Error("error");
-    for (let i = 0; i < this.getSize() - 1; i++) {
-      this.queue.push(this.queue.shift()!);
-    }
-    return this.queue.shift()!;
-  }
-  peek() {
-    if (this.isEmpty()) throw new Error("error");
-    const res = this.pop();
-    this.push(res);
-    return res;
-  }
-}
-
-export class QueueBasedOnStack<T = number> {
-  stack1: T[];
-  stack2: T[];
-  constructor() {
-    this.stack1 = [];
-    this.stack2 = [];
-  }
-  getSize() {
-    return this.stack1.length;
-  }
-  isEmpty() {
-    return this.stack1.length === 0;
-  }
-  enqueue(item: T) {
-    this.stack1.push(item);
-  }
-  dequeue() {
-    if (this.isEmpty()) throw new Error("error");
-    while (this.stack1.length) {
-      this.stack2.push(this.stack1.pop()!);
-    }
-    const res = this.stack2.pop()!;
-    while (this.stack2.length) {
-      this.stack1.push(this.stack2.pop()!);
-    }
-    return res;
-  }
-  getFront() {
-    if (this.isEmpty()) throw new Error("error");
-    const res = this.dequeue();
-    while (this.stack1.length) {
-      this.stack2.push(this.stack1.pop()!);
-    }
-    this.stack1.push(res);
-    while (this.stack2.length) {
-      this.stack1.push(this.stack2.pop()!);
-    }
-    return res;
-  }
-}
+// linked list
 
 // hot 29-32
 export function climbStairs(n: number) {
@@ -416,7 +160,7 @@ export function minWindow(s: string, t: string) {
   return res;
 }
 
-// practice week5
+// practice week5 1 - 6
 export type HeapType = "min" | "max";
 
 export class Heap<T = number> {
@@ -516,5 +260,51 @@ export function lastStoneWeight(stones: number[]) {
     }
   }
   if (heap.size() === 0) return 0;
+  return heap.peek();
+}
+
+export class KthLargest {
+  k: number;
+  heap: Heap;
+  constructor(nums: number[], k: number) {
+    this.k = k;
+    this.heap = new Heap();
+    for (let item of nums) {
+      this.heap.insert(item);
+      if (this.heap.size() > k) this.heap.pop();
+    }
+  }
+  add(val: number) {
+    this.heap.insert(val);
+    if (this.heap.size() > this.k) this.heap.pop();
+    return this.heap.peek();
+  }
+}
+
+export function kSmallestPairs(nums1: number[], nums2: number[], k: number) {
+  const heap = new Heap<[number, number]>(
+    "min",
+    (a, b) => nums1[a[0]] + nums2[a[1]] < nums1[b[0]] + nums2[b[1]]
+  );
+  const res: number[][] = [];
+  const set = new Set<string>();
+  heap.insert([0, 0]);
+  while (heap.size() > 0 && res.length < k) {
+    const arr = heap.pop();
+    if (set.has(arr.join("."))) continue;
+    res.push([nums1[arr[0]], nums2[arr[1]]]);
+    set.add(arr.join("."));
+    if (arr[0] + 1 < nums1.length) heap.insert([arr[0] + 1, arr[1]]);
+    if (arr[1] + 1 < nums2.length) heap.insert([arr[0], arr[1] + 1]);
+  }
+  return res;
+}
+
+export function findKthLargest(nums: number[], k: number) {
+  const heap = new Heap();
+  for (let item of nums) {
+    heap.insert(item);
+    if (heap.size() > k) heap.pop();
+  }
   return heap.peek();
 }
