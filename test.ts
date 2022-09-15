6e9a19c2d3b29f944ee43ee56eb84c3a8908fa69
meixiang474 <1436098850@qq.com>
2022-09-15 16:14:05 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -19,296 +19,205 @@ export class ListNode {
 }
 
 // offer 55-II 56-I
-// todo
-
-// merge sort
-// todo
-export function mergeSort(nums: number[]) {
-  const sortArr = (nums: number[], l: number, r: number, temp: number[]) => {
-    if (l >= r) return;
-    const mid = Math.floor(l + (r - l) / 2);
-    sortArr(nums, l, mid, temp);
-    sortArr(nums, mid + 1, r, temp);
-    if (nums[mid] > nums[mid + 1]) {
-      merge(nums, l, mid, r, temp);
-    }
+export function isBalanced(root: TreeNode | null): boolean {
+  if (!root) return true;
+  const getDepth = (node: TreeNode | null): number => {
+    if (!node) return 0;
+    return Math.max(getDepth(node.left), getDepth(node.right)) + 1;
   };
-  const merge = (
-    nums: number[],
-    l: number,
-    mid: number,
-    r: number,
-    temp: number[]
-  ) => {
-    for (let i = l; i <= r; i++) {
-      temp[i] = nums[i];
-    }
-    let i = l,
-      j = mid + 1;
-    for (let k = l; k <= r; k++) {
-      if (i > mid) {
-        nums[k] = temp[j];
-        j++;
-      } else if (j > r) {
-        nums[k] = temp[i];
+  return (
+    Math.abs(getDepth(root.left) - getDepth(root.right)) <= 1 &&
+    isBalanced(root.left) &&
+    isBalanced(root.right)
+  );
+}
+
+export function singleNumbers(nums: number[]) {
+  const map = new Map<number, number>();
+  for (let item of nums) {
+    map.set(item, map.has(item) ? map.get(item)! + 1 : 1);
+  }
+  const res: number[] = [];
+  map.forEach((val, key) => {
+    if (val === 1) res.push(key);
+  });
+  return res;
+}
+
+// quick sort
+export function quickSort(nums: number[]) {
+  const sortArr = (nums: number[], l: number, r: number) => {
+    if (l >= r) return;
+    const p = partition(nums, l, r);
+    sortArr(nums, l, p - 1);
+    sortArr(nums, p + 1, r);
+  };
+  const swap = (arr: number[], i: number, j: number) =>
+    ([arr[i], arr[j]] = [arr[j], arr[i]]);
+  const getRandom = (l: number, r: number) =>
+    Math.floor(Math.random() * (r - l + 1) + l);
+  const partition = (nums: number[], l: number, r: number) => {
+    const p = getRandom(l, r);
+    swap(nums, l, p);
+    let i = l + 1,
+      j = r;
+    while (true) {
+      while (i <= j && nums[i] < nums[l]) {
         i++;
-      } else if (temp[i] <= temp[j]) {
-        nums[k] = temp[i];
-        i++;
-      } else {
-        nums[k] = temp[j];
-        j++;
       }
+      while (j >= i && nums[j] > nums[l]) {
+        j--;
+      }
+      if (i >= j) break;
+      swap(nums, i, j);
+      i++;
+      j--;
     }
+    swap(nums, l, j);
+    return j;
   };
   const res = [...nums];
-  sortArr(res, 0, res.length - 1, [...res]);
+  sortArr(res, 0, res.length - 1);
   return res;
 }
 
-export function reversePairs(nums: number[][]) {
-  let res = 0;
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] > nums[j]) res++;
-    }
-  }
-  return res;
-}
-
-export function reversePairs1(nums: number[]) {
-  let res = 0;
-  const sortArr = (nums: number[], l: number, r: number, temp: number[]) => {
+export function quickSort1(nums: number[]) {
+  const sortArr = (nums: number[], l: number, r: number) => {
     if (l >= r) return;
-    const mid = Math.floor(l + (r - l) / 2);
-    sortArr(nums, l, mid, temp);
-    sortArr(nums, mid + 1, r, temp);
-    if (nums[mid] > nums[mid + 1]) {
-      merge(nums, l, mid, r, temp);
-    }
+    const { left, right } = partition(nums, l, r);
+    sortArr(nums, l, left);
+    sortArr(nums, right, r);
   };
-  const merge = (
-    nums: number[],
-    l: number,
-    mid: number,
-    r: number,
-    temp: number[]
-  ) => {
-    for (let i = l; i <= r; i++) {
-      temp[i] = nums[i];
-    }
-    let i = l,
-      j = mid + 1;
-    for (let k = l; k <= r; k++) {
-      if (i > mid) {
-        nums[k] = temp[j];
-        j++;
-      } else if (j > r) {
-        nums[k] = temp[i];
+  const getRandom = (l: number, r: number) =>
+    Math.floor(Math.random() * (r - l + 1) + l);
+  const swap = (arr: number[], i: number, j: number) =>
+    ([arr[i], arr[j]] = [arr[j], arr[i]]);
+  const partition = (nums: number[], l: number, r: number) => {
+    const p = getRandom(l, r);
+    swap(nums, l, p);
+    let left = l,
+      right = r + 1,
+      i = l + 1;
+    while (i < right) {
+      if (nums[i] < nums[l]) {
+        left++;
+        swap(nums, left, i);
         i++;
-      } else if (temp[i] <= temp[j]) {
-        nums[k] = temp[i];
-        i++;
+      } else if (nums[i] > nums[l]) {
+        right--;
+        swap(nums, right, i);
       } else {
-        res += mid - i + 1;
-        nums[k] = temp[j];
-        j++;
+        i++;
       }
     }
+    swap(nums, l, left);
+    return {
+      left: left - 1,
+      right,
+    };
   };
-  sortArr([...nums], 0, nums.length, [...nums]);
+  const res = [...nums];
+  sortArr(res, 0, res.length - 1);
   return res;
 }
 
-export function mergeLists(l1: ListNode | null, l2: ListNode | null) {
-  const res = new ListNode(-1);
-  let p1 = l1,
-    p2 = l2,
-    p3 = res;
-  while (p1 && p2) {
-    if (p1.val <= p2.val) {
-      p3.next = p1;
-      p1 = p1.next;
+export function sortColors(nums: number[]) {
+  const swap = (arr: number[], i: number, j: number) =>
+    ([arr[i], arr[j]] = [arr[j], arr[i]]);
+  let l = -1,
+    r = nums.length,
+    i = 0;
+  while (i < r) {
+    if (nums[i] === 0) {
+      l++;
+      swap(nums, l, i);
+      i++;
+    } else if (nums[i] === 2) {
+      r--;
+      swap(nums, r, i);
     } else {
-      p3.next = p2;
-      p2 = p2.next;
+      i++;
     }
-    p3 = p3.next;
   }
-  if (p1) p3.next = p1;
-  if (p2) p3.next = p2;
-  return res.next;
+  return nums;
+}
+
+export function findKMax(nums: number[], k: number) {
+  k = nums.length - k;
+  const sortArr = (nums: number[], l: number, r: number): number => {
+    if (l >= r) return nums[k];
+    const p = partition(nums, l, r);
+    if (p === k) return nums[p];
+    if (p > k) return sortArr(nums, l, p - 1);
+    if (p < k) return sortArr(nums, p + 1, r);
+    return 0;
+  };
+  const getRandom = (l: number, r: number) =>
+    Math.floor(Math.random() * (r - l + 1) + l);
+  const swap = (arr: number[], i: number, j: number) =>
+    ([arr[i], arr[j]] = [arr[j], arr[i]]);
+  const partition = (nums: number[], l: number, r: number) => {
+    const p = getRandom(l, r);
+    swap(nums, l, p);
+    let i = l + 1,
+      j = r;
+    while (true) {
+      while (i <= j && nums[i] < nums[l]) {
+        i++;
+      }
+      while (j >= i && nums[j] > nums[l]) {
+        j--;
+      }
+      if (i >= j) break;
+      swap(nums, i, j);
+      i++;
+      j--;
+    }
+    swap(nums, l, j);
+    return j;
+  };
+  return sortArr([...nums], 0, nums.length - 1);
+}
+
+export function findKMin(nums: number[], k: number) {
+  if (nums.length <= k) return nums.slice(0, k);
+  const sortArr = (nums: number[], l: number, r: number): number[] => {
+    if (l >= r) return nums.slice(0, k);
+    const p = partition(nums, l, r);
+    if (p === k) {
+      return nums.slice(0, k);
+    } else if (p > k) {
+      return sortArr(nums, l, p - 1);
+    } else {
+      return sortArr(nums, p + 1, r);
+    }
+  };
+  const getRandom = (l: number, r: number) =>
+    Math.floor(Math.random() * (r - l + 1) + l);
+  const swap = (arr: number[], i: number, j: number) =>
+    ([arr[i], arr[j]] = [arr[j], arr[i]]);
+  const partition = (nums: number[], l: number, r: number) => {
+    const p = getRandom(l, r);
+    swap(nums, l, p);
+    let i = l + 1,
+      j = r;
+    while (true) {
+      while (i <= j && nums[i] < nums[l]) {
+        i++;
+      }
+      while (j >= i && nums[j] > nums[l]) {
+        j--;
+      }
+      if (i >= j) break;
+      swap(nums, i, j);
+      i++;
+      j--;
+    }
+    swap(nums, l, j);
+    return j;
+  };
+  return sortArr([...nums], 0, nums.length - 1);
 }
 
 // hot 37 - 40
-export function inorderTraversal(root: TreeNode | null) {
-  if (!root) return [];
-  const res: number[] = [];
-  const stack: TreeNode[] = [];
-  let p: TreeNode | null = root;
-  while (p || stack.length) {
-    while (p) {
-      stack.push(p);
-      p = p.left;
-    }
-    const current = stack.pop()!;
-    res.push(current.val);
-    p = current.right;
-  }
-  return res;
-}
-
-export function numsTree(n: number) {
-  const dp = [1, 1];
-  for (let i = 2; i <= n; i++) {
-    if (dp[i] == null) dp[i] = 0;
-    for (let j = 1; j <= i; j++) {
-      dp[i] += dp[j - 1] * dp[i - j];
-    }
-  }
-  return dp[n];
-}
-
-export function isValidBST(root: TreeNode | null) {
-  if (!root) return true;
-  const dfs = (node: TreeNode, floor: number, ceil: number): boolean => {
-    if (node.val <= floor || node.val >= ceil) return false;
-    return (
-      (!node.left || dfs(node.left, floor, node.val)) &&
-      (!node.right || dfs(node.right, node.val, ceil))
-    );
-  };
-  return dfs(root, -Infinity, Infinity);
-}
-
-export function isSymmetric(root: TreeNode | null) {
-  if (!root) return true;
-  const dfs = (p: TreeNode | null, q: TreeNode | null) => {
-    if (!p && !q) return true;
-    if (
-      p &&
-      q &&
-      p.val === q.val &&
-      dfs(p.left, q.right) &&
-      dfs(p.right, q.left)
-    )
-      return true;
-    return false;
-  };
-  return dfs(root.left, root.right);
-}
-
-// practice array 57 - 64
-export function insertIntervals(intervals: number[][], newInterval: number[]) {
-  intervals.push(newInterval);
-  intervals.sort((a, b) => a[0] - b[0]);
-  const res: number[][] = [];
-  let prevEnd = -Infinity;
-  for (let i = 0; i < intervals.length; i++) {
-    const [start, end] = intervals[i];
-    if (i > 0 && prevEnd >= start) {
-      res.splice(res.length - 1, 1, [
-        res[res.length - 1][0],
-        Math.max(prevEnd, end),
-      ]);
-    } else {
-      res.push(intervals[i]);
-    }
-    prevEnd = Math.max(prevEnd, end);
-  }
-  return res;
-}
-
-export function generateMatrix(n: number) {
-  const res: number[][] = Array.from({ length: n }, () => new Array(n).fill(0));
-  const map: boolean[][] = Array.from({ length: n }, () =>
-    new Array(n).fill(false)
-  );
-  const directions = [
-    [0, 1],
-    [1, 0],
-    [0, -1],
-    [-1, 0],
-  ];
-  let dIndex = 0,
-    r = 0,
-    c = 0;
-  for (let i = 1; i <= n ** 2; i++) {
-    res[r][c] = i;
-    map[r][c] = true;
-    const nextR = r + directions[dIndex][0];
-    const nextC = c + directions[dIndex][1];
-    if (
-      !(
-        nextR >= 0 &&
-        nextR < n &&
-        nextC >= 0 &&
-        nextC < n &&
-        !map[nextR][nextC]
-      )
-    ) {
-      dIndex++;
-    }
-    r += directions[dIndex][0];
-    c += directions[dIndex][1];
-  }
-  return res;
-}
-
-export function uniquePaths(m: number, n: number) {
-  const dp: number[][] = Array.from({ length: m }, () => new Array(n).fill(0));
-  for (let r = 0; r < m; r++) {
-    dp[r][0] = 1;
-  }
-  for (let c = 0; c < n; c++) {
-    dp[0][c] = 1;
-  }
-  for (let r = 1; r < m; r++) {
-    for (let c = 1; c < n; c++) {
-      dp[r][c] = dp[r][c - 1] + dp[r - 1][c];
-    }
-  }
-  return dp[m - 1][n - 1];
-}
-
-export function uniquePaths1(obstacleGrid: number[][]) {
-  if (obstacleGrid.length === 0 || obstacleGrid[0].length === 0) return 0;
-  const m = obstacleGrid.length;
-  const n = obstacleGrid[0].length;
-  const dp: number[][] = Array.from({ length: m }, () => new Array(n).fill(0));
-  for (let r = 0; r < m; r++) {
-    if (obstacleGrid[r][0] === 1) break;
-    dp[r][0] = 1;
-  }
-  for (let c = 0; c < n; c++) {
-    if (obstacleGrid[0][c] === 1) break;
-    dp[0][c] = 1;
-  }
-  for (let r = 1; r < m; r++) {
-    for (let c = 1; c < n; c++) {
-      if (obstacleGrid[r][c] === 1) continue;
-      dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
-    }
-  }
-  return dp[m - 1][n - 1];
-}
-
-export function minPathSum(grid: number[][]) {
-  if (grid.length === 0 || grid[0].length === 0) return 0;
-  const m = grid.length;
-  const n = grid[0].length;
-  const dp: number[][] = Array.from({ length: m }, () => new Array(n).fill(0));
-  dp[0][0] = grid[0][0];
-  for (let r = 1; r < m; r++) {
-    dp[r][0] = dp[r - 1][0] + grid[r][0];
-  }
-  for (let c = 1; c < n; c++) {
-    dp[0][c] = dp[0][c - 1] + grid[0][c];
-  }
-  for (let r = 1; r < m; r++) {
-    for (let c = 1; c < n; c++) {
-      dp[r][c] = grid[r][c] + Math.min(dp[r - 1][c], dp[r][c - 1]);
-    }
-  }
-  return dp[m - 1][n - 1];
-}
+// todo
