efbe0f34a8eba065ba0ce8321781ac6088a16cf9
meixiang474 <1436098850@qq.com>
2022-09-18 15:20:14 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -18,207 +18,41 @@ export class ListNode {
   }
 }
 
-// offer 55-II 56-I
-export function isBalanced(root: TreeNode | null): boolean {
-  if (!root) return true;
-  const getDepth = (node: TreeNode | null): number => {
-    if (!node) return 0;
-    return Math.max(getDepth(node.left), getDepth(node.right)) + 1;
-  };
-  return (
-    Math.abs(getDepth(root.left) - getDepth(root.right)) <= 1 &&
-    isBalanced(root.left) &&
-    isBalanced(root.right)
-  );
-}
-
-export function singleNumbers(nums: number[]) {
+// offer 56-II 57-II
+export function singleNumber(nums: number[]) {
   const map = new Map<number, number>();
   for (let item of nums) {
     map.set(item, map.has(item) ? map.get(item)! + 1 : 1);
   }
-  const res: number[] = [];
-  map.forEach((val, key) => {
-    if (val === 1) res.push(key);
-  });
-  return res;
+  for (let [key, value] of map) {
+    if (value === 1) return key;
+  }
 }
 
-// quick sort
-export function quickSort(nums: number[]) {
-  const sortArr = (nums: number[], l: number, r: number) => {
-    if (l >= r) return;
-    const p = partition(nums, l, r);
-    sortArr(nums, l, p - 1);
-    sortArr(nums, p + 1, r);
-  };
-  const swap = (arr: number[], i: number, j: number) =>
-    ([arr[i], arr[j]] = [arr[j], arr[i]]);
-  const getRandom = (l: number, r: number) =>
-    Math.floor(Math.random() * (r - l + 1) + l);
-  const partition = (nums: number[], l: number, r: number) => {
-    const p = getRandom(l, r);
-    swap(nums, l, p);
-    let i = l + 1,
-      j = r;
-    while (true) {
-      while (i <= j && nums[i] < nums[l]) {
-        i++;
+export function findSequence(target: number) {
+  const res: number[][] = [];
+  let l = 1,
+    r = 2;
+  while (l < r) {
+    const sum = ((l + r) * (r - l + 1)) / 2;
+    if (sum === target) {
+      const arr = [];
+      for (let i = l; i <= r; i++) {
+        arr.push(i);
       }
-      while (j >= i && nums[j] > nums[l]) {
-        j--;
-      }
-      if (i >= j) break;
-      swap(nums, i, j);
-      i++;
-      j--;
-    }
-    swap(nums, l, j);
-    return j;
-  };
-  const res = [...nums];
-  sortArr(res, 0, res.length - 1);
-  return res;
-}
-
-export function quickSort1(nums: number[]) {
-  const sortArr = (nums: number[], l: number, r: number) => {
-    if (l >= r) return;
-    const { left, right } = partition(nums, l, r);
-    sortArr(nums, l, left);
-    sortArr(nums, right, r);
-  };
-  const getRandom = (l: number, r: number) =>
-    Math.floor(Math.random() * (r - l + 1) + l);
-  const swap = (arr: number[], i: number, j: number) =>
-    ([arr[i], arr[j]] = [arr[j], arr[i]]);
-  const partition = (nums: number[], l: number, r: number) => {
-    const p = getRandom(l, r);
-    swap(nums, l, p);
-    let left = l,
-      right = r + 1,
-      i = l + 1;
-    while (i < right) {
-      if (nums[i] < nums[l]) {
-        left++;
-        swap(nums, left, i);
-        i++;
-      } else if (nums[i] > nums[l]) {
-        right--;
-        swap(nums, right, i);
-      } else {
-        i++;
-      }
-    }
-    swap(nums, l, left);
-    return {
-      left: left - 1,
-      right,
-    };
-  };
-  const res = [...nums];
-  sortArr(res, 0, res.length - 1);
-  return res;
-}
-
-export function sortColors(nums: number[]) {
-  const swap = (arr: number[], i: number, j: number) =>
-    ([arr[i], arr[j]] = [arr[j], arr[i]]);
-  let l = -1,
-    r = nums.length,
-    i = 0;
-  while (i < r) {
-    if (nums[i] === 0) {
+      res.push(arr);
       l++;
-      swap(nums, l, i);
-      i++;
-    } else if (nums[i] === 2) {
-      r--;
-      swap(nums, r, i);
+    } else if (sum < target) {
+      r++;
     } else {
-      i++;
+      l++;
     }
   }
-  return nums;
+  return res;
 }
 
-export function findKMax(nums: number[], k: number) {
-  k = nums.length - k;
-  const sortArr = (nums: number[], l: number, r: number): number => {
-    if (l >= r) return nums[k];
-    const p = partition(nums, l, r);
-    if (p === k) return nums[p];
-    if (p > k) return sortArr(nums, l, p - 1);
-    if (p < k) return sortArr(nums, p + 1, r);
-    return 0;
-  };
-  const getRandom = (l: number, r: number) =>
-    Math.floor(Math.random() * (r - l + 1) + l);
-  const swap = (arr: number[], i: number, j: number) =>
-    ([arr[i], arr[j]] = [arr[j], arr[i]]);
-  const partition = (nums: number[], l: number, r: number) => {
-    const p = getRandom(l, r);
-    swap(nums, l, p);
-    let i = l + 1,
-      j = r;
-    while (true) {
-      while (i <= j && nums[i] < nums[l]) {
-        i++;
-      }
-      while (j >= i && nums[j] > nums[l]) {
-        j--;
-      }
-      if (i >= j) break;
-      swap(nums, i, j);
-      i++;
-      j--;
-    }
-    swap(nums, l, j);
-    return j;
-  };
-  return sortArr([...nums], 0, nums.length - 1);
-}
-
-export function findKMin(nums: number[], k: number) {
-  if (nums.length <= k) return nums.slice(0, k);
-  const sortArr = (nums: number[], l: number, r: number): number[] => {
-    if (l >= r) return nums.slice(0, k);
-    const p = partition(nums, l, r);
-    if (p === k) {
-      return nums.slice(0, k);
-    } else if (p > k) {
-      return sortArr(nums, l, p - 1);
-    } else {
-      return sortArr(nums, p + 1, r);
-    }
-  };
-  const getRandom = (l: number, r: number) =>
-    Math.floor(Math.random() * (r - l + 1) + l);
-  const swap = (arr: number[], i: number, j: number) =>
-    ([arr[i], arr[j]] = [arr[j], arr[i]]);
-  const partition = (nums: number[], l: number, r: number) => {
-    const p = getRandom(l, r);
-    swap(nums, l, p);
-    let i = l + 1,
-      j = r;
-    while (true) {
-      while (i <= j && nums[i] < nums[l]) {
-        i++;
-      }
-      while (j >= i && nums[j] > nums[l]) {
-        j--;
-      }
-      if (i >= j) break;
-      swap(nums, i, j);
-      i++;
-      j--;
-    }
-    swap(nums, l, j);
-    return j;
-  };
-  return sortArr([...nums], 0, nums.length - 1);
-}
-
+// binary search
+// todo
 // hot 41 - 44
 export function levelOrder(root: TreeNode | null) {
   if (!root) return [];
@@ -299,4 +133,93 @@ export function subsets(nums: number[]) {
   }
   return res;
 }
-// todo
+
+export function exist(board: string[][], word: string) {
+  if (board.length === 0 || board[0].length === 0) return false;
+  const m = board.length;
+  const n = board[0].length;
+  const dfs = (r: number, c: number, index: number) => {
+    if (index >= word.length) return true;
+    const temp = board[r][c];
+    board[r][c] = "";
+    const res = [
+      [r + 1, c],
+      [r - 1, c],
+      [r, c + 1],
+      [r, c - 1],
+    ].some(([nextR, nextC]) => {
+      if (
+        nextR >= 0 &&
+        nextR < m &&
+        nextC >= 0 &&
+        nextC < n &&
+        board[nextR][nextC] === word[index]
+      ) {
+        return dfs(nextR, nextC, index + 1);
+      }
+      return false;
+    });
+    board[r][c] = temp;
+    return res;
+  };
+  for (let r = 0; r < m; r++) {
+    for (let c = 0; c < n; c++) {
+      if (board[r][c] === word[0]) {
+        const res = dfs(r, c, 1);
+        if (res) return true;
+      }
+    }
+  }
+  return false;
+}
+
+export function subsetsWithDup(nums: number[]) {
+  nums.sort((a, b) => a - b);
+  const res: number[][] = [];
+  const dfs = (path: number[], index: number, length: number) => {
+    if (path.length === length) {
+      res.push(path);
+      return;
+    }
+    if (path.length + nums.length - index < length) return;
+    for (let i = index; i < nums.length; i++) {
+      if (i > index && nums[i - 1] === nums[i]) continue;
+      dfs(path.concat(nums[i]), i + 1, length);
+    }
+  };
+  for (let i = 0; i <= nums.length; i++) {
+    dfs([], 0, i);
+  }
+  return res;
+}
+
+export function restoreIpAddresses(s: string) {
+  const ips: string[] = [];
+  const res: string[] = [];
+  const dfs = (ipIndex: number, index: number) => {
+    if (ipIndex === 4) {
+      if (index === s.length) {
+        res.push(ips.join("."));
+      }
+      return;
+    }
+    if (index === s.length) return;
+    if (s[index] === "0") {
+      ips[ipIndex] = "0";
+      dfs(ipIndex + 1, index + 1);
+      return;
+    }
+    let item = 0;
+    for (let i = index; i < s.length; i++) {
+      const current = item * 10 + parseInt(s[i]);
+      if (current > 0 && current <= 255) {
+        ips[ipIndex] = current.toString();
+        dfs(ipIndex + 1, i + 1);
+      } else {
+        break;
+      }
+    }
+  };
+  dfs(0, 0);
+  return res;
+}
