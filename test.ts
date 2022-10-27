af9835155d51649d8d3e12e8adb7ee99444142f8
meixiang474 <1436098850@qq.com>
2022-10-27 20:00:22 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -22,366 +22,119 @@ export class ListNode {
 
 // offer 60 61
 export function dicesProbability(n: number) {
-  // todo
-}
-
-
-// set map
-export class BSTSet<T = number> {
-  bst: BST<T>;
-  constructor() {
-    this.bst = new BST();
-  }
-  getSize() {
-    return this.bst.getSize();
-  }
-  isEmpty() {
-    return this.bst.isEmpty();
-  }
-  add(item: T) {
-    this.bst.add(item);
-  }
-  contains(item: T) {
-    return this.bst.contains(item);
-  }
-  remove(item: T) {
-    this.bst.remove(item);
-  }
-}
-
-export class LinkListSet<T = number> {
-  list: LinkedList<T>;
-  constructor() {
-    this.list = new LinkedList(-1 as any);
-  }
-  getSize() {
-    return this.list.getSize();
-  }
-  isEmpty() {
-    return this.list.isEmpty();
-  }
-  contains(item: T) {
-    return this.list.contains(item);
-  }
-  add(item: T) {
-    if (!this.list.contains(item)) {
-      this.list.addFirst(item);
-    }
-  }
-  remove(item: T) {
-    this.list.removeElement(item);
-  }
-}
-
-export function fn(nums1: number[], nums2: number[]) {
-  return [...new Set(nums1)].filter((item) => nums2.includes(item));
-}
-
-export function uniqueMorse(words: string[]) {
-  const arr = [
-    ".-",
-    "-...",
-    "-.-.",
-    "-..",
-    ".",
-    "..-.",
-    "--.",
-    "....",
-    "..",
-    ".---",
-    "-.-",
-    ".-..",
-    "--",
-    "-.",
-    "---",
-    ".--.",
-    "--.-",
-    ".-.",
-    "...",
-    "-",
-    "..-",
-    "...-",
-    ".--",
-    "-..-",
-    "-.--",
-    "--..",
-  ];
-  const set = new Set<string>();
-  for (let item of words) {
-    const code = item.split("").reduce((memo, current) => {
-      return memo + arr[current.charCodeAt(0) - "a".charCodeAt(0)];
-    }, "");
-    set.add(code);
-  }
-  return set.size;
-}
-
-export class LinkedListMapNode<K = string, V = any> {
-  key: K | null;
-  value: V | null;
-  next: LinkedListMapNode<K, V> | null;
-  constructor(
-    key: K | null = null,
-    value: V | null = null,
-    next: LinkedListMapNode<K, V> | null = null
-  ) {
-    this.key = key;
-    this.value = value;
-    this.next = next;
-  }
-  toString() {
-    return `${JSON.stringify(this.key)} : ${JSON.stringify(this.value)}`;
-  }
-}
-
-export class LinkedListMap<K = string, V = any> {
-  dummyHead: LinkedListMapNode<K, V>;
-  size: number;
-  constructor() {
-    this.dummyHead = new LinkedListMapNode();
-    this.size = 0;
-  }
-  getSize() {
-    return this.size;
-  }
-  isEmpty() {
-    return this.size === 0;
-  }
-  getNode(key: K) {
-    let current = this.dummyHead.next;
-    while (current) {
-      if (current.key === key) {
-        return current;
+  if (n === 0) return [];
+  const dp = new Array(6).fill(1 / 6);
+  for (let i = 2; i <= n; i++) {
+    const temp: number[] = [];
+    for (let j = 1; j <= 6; j++) {
+      for (let k = 0; k < dp.length; k++) {
+        const sum = k + j - 1;
+        temp[sum] = (temp[sum] || 0) + dp[k] * (1 / 6);
       }
-      current = current.next;
-    }
-    return null;
-  }
-  contains(key: K) {
-    return this.getNode(key) != null;
-  }
-  get(key: K) {
-    const node = this.getNode(key);
-    return node ? node.value : null;
-  }
-  add(key: K, value: V) {
-    const node = this.getNode(key);
-    if (node == null) {
-      this.dummyHead.next = new LinkedListMapNode(
-        key,
-        value,
-        this.dummyHead.next
-      );
-    } else {
-      node.value = value;
     }
   }
-  set(key: K, value: V) {
-    const node = this.getNode(key);
-    if (!node) throw new Error("error");
-    node.value = value;
-  }
-  remove(key: K) {
-    let prev = this.dummyHead;
-    while (prev.next) {
-      if (prev.next.key === key) {
-        break;
-      }
-      prev = prev.next;
-    }
-    if (prev.next) {
-      const node = prev.next;
-      prev.next = prev.next.next;
-      this.size--;
-      return node.value;
-    }
-    return null;
-  }
+  return dp;
 }
 
-export class BSTMapNode<K = number, V = any> {
-  key: K;
-  value: V;
-  left: BSTMapNode<K, V> | null;
-  right: BSTMapNode<K, V> | null;
-  constructor(key: K, value: V) {
-    this.key = key;
-    this.value = value;
-    this.left = null;
-    this.right = null;
+export function isStraight(nums: number[]) {
+  const set = new Set<number>();
+  let max = 0;
+  let min = 14;
+  for (let item of nums) {
+    if (item === 0) continue;
+    if (set.has(item)) return false;
+    max = Math.max(max, item);
+    min = Math.min(min, item);
+    set.add(item);
   }
+  return max - min < 5;
 }
 
-export class BSTMap<K = number, V = any> {
-  root: BSTMapNode<K, V> | null;
-  size: number;
-  constructor(compare: (a: K, b: K) => boolean) {
-    this.root = null;
-    this.size = 0;
-    this.compare = compare || this.compare;
-  }
-  compare(a: K, b: K) {
-    return a < b;
-  }
-  add(key: K, value: V) {
-    this.root = this.addNode(key, value, this.root);
-  }
-  addNode(key: K, value: V, node: BSTMapNode<K, V> | null) {
-    if (!node) {
-      this.size++;
-      return new BSTMapNode(key, value);
-    }
-    if (this.compare(node.key, key)) {
-      node.right = this.addNode(key, value, node.right);
-    } else if (this.compare(key, node.key)) {
-      node.left = this.addNode(key, value, node.left);
-    } else {
-      node.value = value;
-    }
-    return node;
-  }
-  getNode(node: BSTMapNode<K, V> | null, key: K): BSTMapNode<K, V> | null {
-    if (!node) return null;
-    if (this.compare(node.key, key)) {
-      return this.getNode(node.right, key);
-    } else if (this.compare(key, node.key)) {
-      return this.getNode(node.left, key);
-    } else {
-      return node;
-    }
-  }
-  contains(key: K) {
-    return this.getNode(this.root, key) != null;
-  }
-  get(key: K) {
-    const node = this.getNode(this.root, key);
-    return node ? node.value : null;
-  }
-  set(key: K, value: V) {
-    const node = this.getNode(this.root, key);
-    if (!node) throw new Error("error");
-    node.value = value;
-  }
-  minimumNode(node: BSTMapNode<K, V>): BSTMapNode<K, V> {
-    if (!node.left) return node;
-    return this.minimumNode(node.left);
-  }
-  removeMinNode(node: BSTMapNode<K, V>) {
-    if (!node.left) {
-      this.size--;
-      return node.right;
-    }
-    node.left = this.removeMinNode(node.left);
-    return node;
-  }
-  remove(key: K) {
-    const node = this.getNode(this.root, key);
-    if (!node) return null;
-    this.root = this.removeNode(this.root, key);
-    return node.value;
-  }
-  removeNode(node: BSTMapNode<K, V> | null, key: K) {
-    if (!node) return null;
-    if (this.compare(node.key, key)) {
-      node.right = this.removeNode(node.right, key);
-      return node;
-    }
-    if (this.compare(key, node.key)) {
-      node.left = this.removeNode(node.left, key);
-      return node;
-    }
-    if (!node.left) {
-      this.size--;
-      return node.right;
-    } else if (!node.right) {
-      this.size--;
-      return node.left;
-    } else {
-      const successor = this.minimumNode(node.right);
-      successor.left = node.left;
-      successor.right = this.removeMinNode(node.right);
-      return successor;
-    }
-  }
-}
-
-export function intersection(nums1: number[], nums2: number[]) {
-  const map = new Map<number, boolean>();
-  for (let item of nums1) {
-    map.set(item, true);
-  }
+// graph
+export function bfs(
+  graph: Record<number, number[]>,
+  node: number,
+  visited: Set<number>
+) {
   const res: number[] = [];
-  for (let item of nums2) {
-    if (map.get(item)) {
-      res.push(item);
-      map.delete(item);
-    }
-  }
-  return res;
-}
-
-export function twoSum(nums: number[], target: number) {
-  const map = new Map<number, number>();
-  for (let i = 0; i < nums.length; i++) {
-    const current = nums[i];
-    const rest = target - current;
-    if (map.has(rest)) {
-      return [i, map.get(rest)];
-    }
-    map.set(current, i);
-  }
-}
-
-export function longestSubstring(s: string) {
-  const map = new Map<string, number>();
-  let res = 0;
-  let l = 0,
-    r = 0;
-  while (r < s.length) {
-    const currentr = s[r];
-    if (map.has(currentr) && map.get(currentr)! >= l) {
-      l = map.get(currentr)! + 1;
-    }
-    res = Math.max(res, r - l + 1);
-    map.set(currentr, r);
-    r++;
-  }
-  return res;
-}
-
-export function shortestSubstring(s: string, t: string) {
-  if (s === t) return s;
-  const need = new Map<string, number>();
-  for (let item of t) {
-    need.set(item, need.has(item) ? need.get(item)! + 1 : 1);
-  }
-  let needType = need.size;
-  let l = 0,
-    r = 0;
-  let res = "";
-  while (r < s.length) {
-    const currentr = s[r];
-    if (need.has(currentr)) {
-      need.set(currentr, need.get(currentr)! - 1);
-      if (need.get(currentr) === 0) {
-        needType--;
+  const queue: number[] = [node];
+  visited.add(node);
+  while (queue.length) {
+    const current = queue.shift()!;
+    res.push(current);
+    graph[current].forEach((item) => {
+      if (!visited.has(item)) {
+        visited.add(item);
+        queue.push(item);
       }
-    }
-    while (needType === 0) {
-      const newRes = s.slice(l, r + 1);
-      if (!res || newRes.length < res.length) res = newRes;
-      const currentl = s[l];
-      if (need.has(currentl)) {
-        need.set(currentl, need.get(currentl)! + 1);
-        if (need.get(currentl) === 1) {
-          needType++;
-        }
-      }
-      l++;
-    }
-    r++;
+    });
   }
   return res;
+}
+
+export function dfs(
+  graph: Record<number, number[]>,
+  node: number,
+  visited: Set<number>
+) {
+  console.log(node);
+  visited.add(node);
+  graph[node].forEach((item) => {
+    if (!visited.has(item)) {
+      dfs(graph, item, visited);
+    }
+  });
+}
+
+export function waterFlow(matrix: number[][]) {
+  if (matrix.length === 0 || matrix[0].length === 0) return [];
+  const m = matrix.length;
+  const n = matrix[0].length;
+  const flow1: boolean[][] = Array.from({ length: m }, () =>
+    new Array(n).fill(false)
+  );
+  const flow2: boolean[][] = Array.from({ length: m }, () =>
+    new Array(n).fill(false)
+  );
+  const dfs = (r: number, c: number, flow: boolean[][]) => {
+    flow[r][c] = true;
+    [
+      [r + 1, c],
+      [r - 1, c],
+      [r, c + 1],
+      [r, c - 1],
+    ].forEach(([nextR, nextC]) => {
+      if (
+        nextR >= 0 &&
+        nextR < m &&
+        nextC >= 0 &&
+        nextC < n &&
+        !flow[nextR][nextC] &&
+        matrix[nextR][nextC] >= matrix[r][c]
+      ) {
+        dfs(nextR, nextC, flow);
+      }
+    });
+  };
+  for (let r = 0; r < m; r++) {
+    dfs(r, 0, flow1);
+    dfs(r, n - 1, flow2);
+  }
+  for (let c = 0; c < n; c++) {
+    dfs(0, c, flow1);
+    dfs(m - 1, c, flow2);
+  }
+  const res: [number, number][] = [];
+  for (let r = 0; r < m; r++) {
+    for (let c = 0; c < n; c++) {
+      if (flow1[r][c] && flow2[r][c]) res.push([r, c]);
+    }
+  }
+  return res;
+}
+
+export function cloneGraph() {
+  // todo
 }
 
 // hot 53 - 56
