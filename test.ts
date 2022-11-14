8caf801d520dda64eba367d337865c6405baca97
meixiang474 <1436098850@qq.com>
2022-11-14 14:00:59 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -1,5 +1,6 @@
 import { BST } from "./11.BST";
 import { LinkedList } from "./7.LinkedList";
+import { Heap } from "./practice/week5/1.heap";
 export class TreeNode {
   val: number;
   left: TreeNode | null;
@@ -46,219 +47,117 @@ export function lowestCommon(
   return res;
 }
 
+export function findRepeatNumber(nums: number[]) {
+  const set = new Set<number>();
+  for (let item of nums) {
+    if (set.has(item)) return item;
+    set.add(item);
+  }
+}
+
+// priorityqueue shellsort
+export class PriorityQueue<T = number> {
+  maxHeap: Heap<T>;
+  constructor(compare: (a: T, b: T) => boolean) {
+    this.maxHeap = new Heap("max", compare);
+  }
+  getSize() {
+    return this.maxHeap.size();
+  }
+  isEmpty() {
+    return this.maxHeap.size() === 0;
+  }
+  getFront() {
+    return this.maxHeap.peek();
+  }
+  enqueue(item: T) {
+    this.maxHeap.insert(item);
+  }
+  dequeue() {
+    if (this.isEmpty()) throw new Error("error");
+    return this.maxHeap.pop();
+  }
+}
+
+export function shellSort(nums: number[]) {
+  const res = [...nums];
+  let h = Math.floor(nums.length / 2);
+  while (h) {
+    for (let start = 0; start < h; start++) {
+      for (let i = start; i < res.length; i += h) {
+        let swapIndex = i;
+        let current = res[i];
+        for (let j = i - h; j >= start; j -= h) {
+          if (res[j] > current) {
+            swapIndex = j;
+            res[j + h] = res[j];
+          } else {
+            break;
+          }
+        }
+        if (swapIndex !== i) {
+          res[swapIndex] = current;
+        }
+      }
+    }
+    h = Math.floor(h / 2);
+  }
+  return h;
+}
+
+export function shellSort1(nums: number[]) {
+  const res = [...nums];
+  let h = Math.floor(res.length / 2);
+  while (h) {
+    for (let i = h; i < res.length; i++) {
+      let swapIndex = i;
+      const current = res[i];
+      for (let j = i - h; j >= 0; j -= h) {
+        if (res[j] > current) {
+          swapIndex = j;
+          res[j + h] = res[j];
+        } else {
+          break;
+        }
+      }
+      if (swapIndex !== i) {
+        res[swapIndex] = current;
+      }
+    }
+    h = Math.floor(h / 2);
+  }
+  return res;
+}
+
+export function shellSort2(nums: number[]) {
+  const res = [...nums];
+  let h = Math.floor(nums.length / 2);
+  while (h < res.length) {
+    h = h * 3 + 1;
+  }
+  while (h) {
+    for (let i = h; i < res.length; i++) {
+      let swapIndex = i;
+      const current = res[i];
+      for (let j = i - h; j >= 0; j -= h) {
+        if (res[j] > current) {
+          swapIndex = j;
+          res[j + h] = res[j];
+        } else {
+          break;
+        }
+      }
+      if (swapIndex !== i) {
+        res[swapIndex] = current;
+      }
+    }
+    h = Math.floor(h / 3);
+  }
+  return res;
+}
+
+// hot 69-72
 // todo
-
-// fenzhi donggui tanxin huisu
-export function invertTree(root: TreeNode | null) {
-  if (!root) return null;
-  const dfs = (node: TreeNode) => {
-    const temp = node.left;
-    node.left = node.right;
-    node.right = temp;
-    if (node.left) dfs(node.left);
-    if (node.right) dfs(node.right);
-  };
-  dfs(root);
-  return root;
-}
-
-export function isSameTree(p: TreeNode | null, q: TreeNode | null) {
-  if (!p && !q) return true;
-  if (
-    p &&
-    q &&
-    p.val === q.val &&
-    isSameTree(p.left, q.left) &&
-    isSameTree(p.right, q.right)
-  )
-    return true;
-  return false;
-}
-
-export function isSymmetic(root: TreeNode | null) {
-  if (!root) return true;
-  const compare = (p: TreeNode | null, q: TreeNode | null) => {
-    if (!p || !q) return true;
-    if (
-      p &&
-      q &&
-      p.val === q.val &&
-      compare(p.left, q.right) &&
-      compare(p.right, q.left)
-    )
-      return true;
-    return false;
-  };
-  return compare(root.left, root.right);
-}
-
-export function climbStairs(n: number) {
-  const dp = [1, 1];
-  for (let i = 2; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2];
-  }
-  return dp[n];
-}
-
-export function rob(nums: number[]) {
-  const dp = [0, nums[0]];
-  for (let i = 2; i <= nums.length; i++) {
-    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
-  }
-  return dp[nums.length];
-}
-
-export function rob1(nums: number[]) {
-  if (nums.length === 0) return 0;
-  if (nums.length === 1) return nums[0];
-  const compute = (nums: number[]) => {
-    const dp = [0, nums[0]];
-    for (let i = 2; i <= nums.length; i++) {
-      dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
-    }
-    return dp[nums.length];
-  };
-  return Math.max(compute(nums.slice(1)), compute(nums.slice(0, -1)));
-}
-
-export function findContentChildren(g: number[], s: number[]) {
-  g.sort((a, b) => a - b);
-  s.sort((a, b) => a - b);
-  let res = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] >= g[res]) {
-      res++;
-    }
-  }
-  return res;
-}
-
-export function maxProfit(prices: number[]) {
-  let profit = 0;
-  for (let i = 0; i < prices.length - 1; i++) {
-    if (prices[i + 1] > prices[i]) {
-      profit += prices[i + 1] - prices[i];
-    }
-  }
-  return profit;
-}
-
-export function permute(nums: number[]) {
-  const dfs = (path: number[]) => {
-    if (path.length === nums.length) {
-      res.push(path);
-      return;
-    }
-    for (let i = 0; i < nums.length; i++) {
-      if (path.includes(nums[i])) continue;
-      dfs(path.concat(nums[i]));
-    }
-  };
-  const res: number[][] = [];
-  dfs([]);
-  return res;
-}
-
-export function subsets(nums: number[]) {
-  const dfs = (path: number[], index: number, length: number) => {
-    if (path.length === length) {
-      res.push(path);
-      return;
-    }
-    if (path.length + nums.length - index < length) return;
-    for (let i = index; i < nums.length; i++) {
-      dfs(path.concat(nums[i]), i + 1, length);
-    }
-  };
-  const res: number[][] = [];
-  for (let i = 0; i <= nums.length; i++) {
-    dfs([], 0, i);
-  }
-  return res;
-}
-
-// hot 65-68
-export function invertTree1(root: TreeNode | null) {
-  if (!root) return root;
-  const dfs = (node: TreeNode) => {
-    const temp = node.left;
-    node.left = node.right;
-    node.right = temp;
-    if (node.left) dfs(node.left);
-    if (node.right) dfs(node.right);
-  };
-  dfs(root);
-  return root;
-}
-
-export function isPalindrome(head: ListNode | null) {
-  if (!head || !head.next) return true;
-  let slow: ListNode | null = head,
-    fast: ListNode | null = head;
-  while (slow && fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-  if (fast) {
-    slow = slow!.next;
-  }
-  let prev: ListNode | null = null;
-  let current = slow;
-  while (current) {
-    const temp = current.next;
-    current.next = prev;
-    prev = current;
-    current = temp;
-  }
-  let p1: ListNode | null = head;
-  let p2 = prev;
-  while (p1 && p2) {
-    if (p1.val !== p2.val) return false;
-    p1 = p1.next;
-    p2 = p2.next;
-  }
-  return true;
-}
-
-export function lowestCommonAncestor(
-  root: TreeNode | null,
-  p: TreeNode | null,
-  q: TreeNode | null
-) {
-  let res = null;
-  const dfs = (
-    node: TreeNode | null,
-    p: TreeNode | null,
-    q: TreeNode | null
-  ): boolean => {
-    if (!node || !p || !q) return false;
-    const left = dfs(node.left, p, q);
-    const right = dfs(node.right, p, q);
-    if (
-      (left && right) ||
-      ((node.val === p.val || node.val === q.val) && (left || right))
-    ) {
-      res = node;
-    }
-    return left || right || node.val === p.val || node.val === q.val;
-  };
-  dfs(root, p, q);
-  return res;
-}
-
-export function productExceptionSelf(nums: number[]) {
-  const res: number[] = [];
-  res[0] = 1;
-  for (let i = 1; i < nums.length; i++) {
-    res[i] = nums[i - 1] * res[i - 1];
-  }
-  let right = 1;
-  for (let i = nums.length - 1; i >= 0; i--) {
-    res[i] *= right;
-    right *= nums[i];
-  }
-  return res;
-}
 
 // linkedlist 6-10
 export function rotateRight(head: ListNode | null, k: number) {
