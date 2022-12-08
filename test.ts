6898fa2930d6ea71fa03c4678e14bfa7eb82ad16
meixiang474 <1436098850@qq.com>
2022-12-08 19:51:39 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -144,89 +144,206 @@ export class SegmentTree<T = number> {
   }
 }
 
+export class NumArray {
+  data: number[];
+  tree: (number | null)[];
+  constructor(arr: number[]) {
+    this.data = [...arr];
+    this.tree = new Array(4 * this.data.length).fill(null);
+    this.buildSegmentTree(0, 0, this.data.length - 1);
+  }
+  merge(a: number, b: number) {
+    return a + b;
+  }
+  getLeftIndex(index: number) {
+    return 2 * index + 1;
+  }
+  getRightIndex(index: number) {
+    return 2 * index + 2;
+  }
+  buildSegmentTree(treeIndex: number, l: number, r: number) {
+    if (l > r) return;
+    if (l === r) {
+      this.tree[treeIndex] = this.data[l];
+      return;
+    }
+    const mid = Math.floor(l + (r - l) / 2);
+    const leftIndex = this.getLeftIndex(treeIndex);
+    const rightIndex = this.getRightIndex(treeIndex);
+    this.buildSegmentTree(leftIndex, l, mid);
+    this.buildSegmentTree(rightIndex, mid + 1, r);
+    this.tree[treeIndex] = this.merge(
+      this.tree[leftIndex] as number,
+      this.tree[rightIndex] as number
+    );
+  }
+  sumRange(left: number, right: number) {
+    if (
+      left < 0 ||
+      right < 0 ||
+      left > right ||
+      left >= this.data.length ||
+      right >= this.data.length
+    )
+      throw new Error("error");
+    return this.query(0, 0, this.data.length - 1, left, right);
+  }
+  query(
+    treeIndex: number,
+    l: number,
+    r: number,
+    queryl: number,
+    queryr: number
+  ): number {
+    if (l === queryl && r === queryr) return this.tree[treeIndex] as number;
+    const mid = Math.floor(l + (r - l) / 2);
+    const leftIndex = this.getLeftIndex(treeIndex);
+    const rightIndex = this.getRightIndex(treeIndex);
+    if (queryl >= mid + 1) {
+      return this.query(rightIndex, mid + 1, r, queryl, queryr);
+    }
+    if (queryr <= mid) {
+      return this.query(leftIndex, l, mid, queryl, queryr);
+    }
+    return this.merge(
+      this.query(leftIndex, l, mid, queryl, mid),
+      this.query(rightIndex, mid + 1, r, mid + 1, queryr)
+    );
+  }
+}
+
+export class NumArray1 {
+  sums: number[];
+  data: number[];
+  constructor(arr: number[]) {
+    this.data = [...arr];
+    this.sums = new Array(this.data.length + 1).fill(0);
+    for (let i = 1; i < this.sums.length; i++) {
+      this.sums[i] = this.sums[i - 1] + this.data[i - 1];
+    }
+  }
+  sumRange(left: number, right: number) {
+    return this.sums[right + 1] - this.sums[left];
+  }
+}
+
+export class NumArray2 {
+  sums: number[];
+  data: number[];
+  constructor(arr: number[]) {
+    this.data = [...arr];
+    this.sums = new Array(this.data.length + 1).fill(0);
+    for (let i = 1; i < this.sums.length; i++) {
+      this.sums[i] = this.sums[i - 1] + this.data[i - 1];
+    }
+  }
+  sumRange(left: number, right: number) {
+    return this.sums[right + 1] - this.sums[left];
+  }
+  update(index: number, value: number) {
+    this.data[index] = value;
+    for (let i = index + 1; i < this.sums.length; i++) {
+      this.sums[i] = this.sums[i - 1] + this.data[i - 1];
+    }
+  }
+}
+
+export class NumArray3 {
+  data: number[];
+  tree: (number | null)[];
+  constructor(arr: number[]) {
+    this.data = [...arr];
+    this.tree = new Array(4 * this.data.length).fill(null);
+    this.buildSegmentTree(0, 0, this.data.length - 1);
+  }
+  merge(a: number, b: number) {
+    return a + b;
+  }
+  getLeftIndex(index: number) {
+    return 2 * index + 1;
+  }
+  getRightIndex(index: number) {
+    return 2 * index + 2;
+  }
+  buildSegmentTree(treeIndex: number, l: number, r: number) {
+    if (l > r) return;
+    if (l === r) {
+      this.tree[treeIndex] = this.data[l];
+      return;
+    }
+    const mid = Math.floor(l + (r - l) / 2);
+    const leftIndex = this.getLeftIndex(treeIndex);
+    const rightIndex = this.getRightIndex(treeIndex);
+    this.buildSegmentTree(leftIndex, l, mid);
+    this.buildSegmentTree(rightIndex, mid + 1, r);
+    this.tree[treeIndex] = this.merge(
+      this.tree[leftIndex]!,
+      this.tree[rightIndex]!
+    );
+  }
+  sumRange(left: number, right: number) {
+    if (
+      left < 0 ||
+      left >= this.data.length ||
+      right < 0 ||
+      right >= this.data.length ||
+      left > right
+    )
+      throw new Error("error");
+    return this.query(0, 0, this.data.length - 1, left, right);
+  }
+  query(
+    treeIndex: number,
+    l: number,
+    r: number,
+    queryl: number,
+    queryr: number
+  ): number {
+    if (l === queryl && r === queryr) {
+      return this.tree[treeIndex]!;
+    }
+    const mid = Math.floor(l + (r - l) / 2);
+    const leftIndex = this.getLeftIndex(treeIndex);
+    const rightIndex = this.getRightIndex(treeIndex);
+    if (queryl >= mid + 1) {
+      return this.query(rightIndex, mid + 1, r, queryl, queryr);
+    }
+    if (queryr <= mid) {
+      return this.query(leftIndex, l, mid, queryl, queryr);
+    }
+    return this.merge(
+      this.query(leftIndex, l, mid, queryl, mid),
+      this.query(rightIndex, mid + 1, r, mid + 1, queryr)
+    );
+  }
+  update(index: number, val: number) {
+    if (index < 0 || index >= this.data.length) throw new Error("error");
+    this.data[index] = val;
+    this.set(0, 0, this.data.length - 1, index, val);
+  }
+  set(treeIndex: number, l: number, r: number, index: number, val: number) {
+    if (l === r) {
+      this.tree[treeIndex] = val;
+      return;
+    }
+    const mid = Math.floor(l + (r - l) / 2);
+    const leftIndex = this.getLeftIndex(treeIndex);
+    const rightIndex = this.getRightIndex(treeIndex);
+    if (index >= mid + 1) {
+      this.set(rightIndex, mid + 1, r, index, val);
+    }
+    if (index <= mid) {
+      this.set(leftIndex, l, mid, index, val);
+    }
+    this.tree[treeIndex] = this.merge(
+      this.tree[leftIndex]!,
+      this.tree[rightIndex]!
+    );
+  }
+}
+
+// hot 73-76
 // todo
-
-// hot 69-72
-export function maxSlidingWindow(nums: number[], k: number) {
-  if (nums.length === 0 || k === 0) return [];
-  const stack: number[] = [];
-  const res: number[] = [];
-  for (let i = 0; i < k; i++) {
-    while (stack.length && nums[i] >= nums[stack[stack.length - 1]]) {
-      stack.pop();
-    }
-    stack.push(i);
-  }
-  res.push(nums[stack[0]]);
-  for (let i = k; i < nums.length; i++) {
-    while (stack.length && nums[i] >= nums[stack[stack.length - 1]]) {
-      stack.pop();
-    }
-    stack.push(i);
-    if (stack[0] <= i - k) stack.shift();
-    res.push(nums[stack[0]]);
-  }
-  return res;
-}
-
-export function searchMatrix(matrix: number[][], target: number) {
-  if (matrix.length === 0 || matrix[0].length === 0) return false;
-  const m = matrix.length;
-  const n = matrix[0].length;
-  const dfs = (r: number, c: number): boolean => {
-    if (matrix[r][c] === target) return true;
-    if (matrix[r][c] > target) {
-      return c - 1 >= 0 && dfs(r, c - 1);
-    } else {
-      return r + 1 < m && dfs(r + 1, c);
-    }
-  };
-  return dfs(0, n - 1);
-}
-
-export function minMeetingRooms(intervals: number[][]) {
-  const map = new Array(intervals.length).fill(false);
-  const dfs = (stop: number) => {
-    let l = 0,
-      r = intervals.length;
-    while (l < r) {
-      const mid = Math.floor(l + (r - l) / 2);
-      if (intervals[mid][0] >= stop) {
-        r = mid;
-      } else {
-        l = mid + 1;
-      }
-    }
-    while (map[l]) {
-      l++;
-    }
-    if (l >= intervals.length) return;
-    map[l] = true;
-    dfs(intervals[l][1]);
-  };
-  let res = 0;
-  for (let i = 0; i < intervals.length; i++) {
-    if (!map[i]) {
-      res++;
-      dfs(intervals[i][1]);
-    }
-  }
-  return res;
-}
-
-export function numSquares(n: number) {
-  if (n < 0) return 0;
-  if (n === 0) return 1;
-  const dp = [0];
-  for (let i = 1; i <= n; i++) {
-    let min = Infinity;
-    for (let j = 1; j ** 2 <= i; j++) {
-      min = Math.min(min, dp[i - j ** 2]);
-    }
-    dp[i] = min + 1;
-  }
-  return dp[n];
-}
 
 // math 6-10
 export function powerOfTwo(n: number): boolean {
