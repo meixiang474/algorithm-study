37e42aaf0d84ebb0e8808452f09a946f876c9d26
meixiang474 <1436098850@qq.com>
2023-02-01 13:32:48 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -21,218 +21,138 @@ export class ListNode1 {
   }
 }
 
-// offer 9 10
-export class QueueBasedOnStack {
-  stack1: number[];
-  stack2: number[];
-  constructor() {
-    this.stack1 = [];
-    this.stack2 = [];
-  }
-  enqueue(item: number) {
-    this.stack1.push(item);
-  }
-  dequeue() {
-    while (this.stack1.length) {
-      this.stack2.push(this.stack1.pop()!);
-    }
-    const res = this.stack2.pop();
-    while (this.stack2.length) {
-      this.stack1.push(this.stack2.pop()!);
-    }
-    return res;
-  }
-}
-
-export function fib(n: number) {
-  const dp = [0, 1];
+// offer 10-II 11
+export function numWays(n: number) {
+  const dp = [1, 1];
   for (let i = 2; i <= n; i++) {
-    dp[n] = dp[n - 1] + dp[n - 2];
+    dp[i] = dp[i - 1] + dp[i - 2];
   }
   return dp[n];
 }
 
-// search sort
-export function isObject<T>(obj: T) {
-  return typeof obj === "object" && obj != null;
-}
-
-export function isEqual<T>(a: T, b: T) {
-  if (!isObject(a) || !isObject(b)) return a === b;
-  const keysA = Object.keys(a as {});
-  const keysB = Object.keys(b as {});
-  if (keysA.length !== keysB.length) return false;
-  for (let key in a) {
-    if ((a as {}).hasOwnProperty(key)) {
-      const flag = isEqual(a[key], b[key]);
-      if (!flag) return false;
-    }
-  }
-  return true;
-}
-
-export function linearSearch<T>(data: T[], target: T) {
-  for (let i = 0; i < data.length; i++) {
-    if (isEqual(data[i], target)) return i;
-  }
-  return -1;
-}
-
-export function selectionSort(data: number[]) {
-  const swap = (arr: number[], i: number, j: number) =>
-    ([arr[i], arr[j]] = [arr[j], arr[i]]);
-  const res = [...data];
-  for (let i = 0; i < res.length; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < res.length; j++) {
-      minIndex = res[j] < res[minIndex] ? j : minIndex;
-    }
-    if (minIndex !== i) {
-      swap(res, minIndex, i);
-    }
-  }
-  return res;
-}
-
-export function insertionSort(data: number[]) {
-  const res = [...data];
-  for (let i = 0; i < res.length; i++) {
-    let current = data[i];
-    let swapIndex = i;
-    for (let j = i - 1; j >= 0; j--) {
-      if (res[j] > current) {
-        res[j + 1] = res[j];
-        swapIndex = j;
-      } else {
-        break;
-      }
-    }
-    if (swapIndex !== i) {
-      res[swapIndex] = current;
-    }
-  }
-  return res;
-}
-
-export function bubbleSort(data: number[]) {
-  const res = [...data];
-  for (let i = 0; i < res.length - 1; i++) {
-    let flag = false;
-    for (let j = 0; j < res.length - i - 1; j++) {
-      if (res[j] > res[j + 1]) {
-        const temp = res[j];
-        res[j] = res[j + 1];
-        res[j + 1] = temp;
-        flag = true;
-      }
-    }
-    if (!flag) break;
-  }
-  return res;
-}
-
-// hot 81-84
-export function rob(root: TreeNode | null) {
-  const dfs = (node: TreeNode | null): [number, number] => {
-    if (!node) return [0, 0];
-    const left = dfs(node.left);
-    const right = dfs(node.right);
-    return [
-      node.val + left[1] + right[1],
-      Math.max(left[0], left[1]) + Math.max(right[0], right[1]),
-    ];
-  };
-  const [select, notSelect] = dfs(root);
-  return Math.max(select, notSelect);
-}
-
-export function countBits(n: number) {
-  const res: number[] = new Array(n + 1).fill(0);
-  let hightBit = 0;
-  for (let i = 0; i <= n; i++) {
-    if ((i & (i - 1)) === 0) {
-      hightBit = i;
-    }
-    res[i] = res[i - hightBit] + 1;
-  }
-  return res;
-}
-
-export function topKFrequent(nums: number[], k: number) {
-  const map = new Map<number, number>();
-  for (let item of nums) {
-    map.set(item, map.has(item) ? map.get(item)! + 1 : 1);
-  }
-  const arr: [number, number][] = [];
-  for (let [key, value] of map) {
-    arr.push([key, value]);
-  }
-  if (k >= arr.length) return arr.map((item) => item[0]);
-  k = arr.length - k;
-  const sortArr = (arr: [number, number][], l: number, r: number): number[] => {
-    if (l >= r) return arr.slice(k).map((item) => item[0]);
-    const p = partition(arr, l, r);
-    if (p === k) {
-      return arr.slice(k).map((item) => item[0]);
-    } else if (p > k) {
-      return sortArr(arr, l, p - 1);
+export function minArray(nums: number[]) {
+  let l = 0,
+    r = nums.length - 1;
+  while (l < r) {
+    const mid = Math.floor(l + (r - l) / 2);
+    if (nums[mid] > nums[r]) {
+      l = mid + 1;
+    } else if (nums[mid] < nums[r]) {
+      r = mid;
     } else {
-      return sortArr(arr, p + 1, r);
+      r--;
     }
-  };
-  const swap = <T>(arr: T[], i: number, j: number) =>
-    ([arr[i], arr[j]] = [arr[j], arr[i]]);
-  const getRandom = (l: number, r: number) =>
-    Math.floor(Math.random() * (r - l + 1) + l);
-  const partition = (arr: [number, number][], l: number, r: number): number => {
-    const p = getRandom(l, r);
-    swap(arr, l, p);
-    let i = l + 1,
-      j = r;
-    while (true) {
-      while (i <= j && arr[i][1] < arr[l][1]) {
-        i++;
-      }
-      while (i <= j && arr[j][1] > arr[l][1]) {
-        j--;
-      }
-      if (i >= j) break;
-      swap(arr, i, j);
-      i++;
-      j--;
-    }
-    swap(arr, l, j);
-    return j;
-  };
-  return sortArr(arr, 0, arr.length - 1);
+  }
+  return nums[l];
 }
 
-export function decodeString(s: string) {
-  let index = 0;
-  const getString = (): string => {
-    if (index >= s.length || s[index] === "]") return "";
-    const current = s[index];
-    let res = "";
-    if (!isNaN(parseFloat(current))) {
-      const count = getCount();
-      index++;
-      const str = getString();
-      index++;
-      res += str.repeat(count);
-    } else {
-      res = current;
-      index++;
+// array
+export class MyArray<T> {
+  data: (T | null)[];
+  size: number;
+  constructor(capacity = 10) {
+    this.data = new Array(capacity).fill(null);
+    this.size = 0;
+  }
+  getCapacity() {
+    return this.data.length;
+  }
+  getSize() {
+    return this.size;
+  }
+  resize(newCapacity: number) {
+    const newData: (T | null)[] = new Array(newCapacity).fill(null);
+    for (let i = 0; i < this.size; i++) {
+      newData[i] = this.data[i];
     }
-    return res + getString();
-  };
-  const getCount = () => {
-    let res = 0;
-    while (index < s.length && s[index] !== "[") {
-      res = res * 10 + parseFloat(s[index]);
+    this.data = newData;
+  }
+  add(index: number, e: T) {
+    if (index < 0 || index > this.size) throw new Error("error");
+    if (this.size >= this.data.length) {
+      this.resize(this.data.length * 2);
+    }
+    for (let i = this.size; i > index; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[index] = e;
+    this.size++;
+  }
+  addFirst(e: T) {
+    this.add(0, e);
+  }
+  addLast(e: T) {
+    this.add(this.size, e);
+  }
+  get(index: number) {
+    if (index < 0 || index >= this.size) throw new Error("error");
+    return this.data[index];
+  }
+  getFirst() {
+    return this.get(0);
+  }
+  getLast() {
+    return this.get(this.size - 1);
+  }
+  contains(e: T) {
+    for (let i = 0; i < this.size; i++) {
+      if (this.data[i] === e) return true;
+    }
+    return false;
+  }
+  find(e: T) {
+    for (let i = 0; i < this.size; i++) {
+      if (this.data[i] === e) return i;
+    }
+    return -1;
+  }
+  remove(index: number) {
+    if (index < 0 || index >= this.size) throw new Error("error");
+    const res = this.data[index];
+    for (let i = index; i < this.size; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    this.size--;
+    if (
+      this.size <= Math.floor(this.getCapacity() / 4) &&
+      Math.floor(this.getCapacity() / 2) !== 0
+    ) {
+      this.resize(Math.floor(this.getCapacity() / 2));
     }
     return res;
-  };
+  }
+  removeFirst() {
+    return this.remove(0);
+  }
+  removeLast() {
+    return this.remove(this.size - 1);
+  }
+  removeElement(e: T) {
+    const index = this.find(e);
+    if (index !== -1) {
+      this.remove(index);
+      return true;
+    }
+    return false;
+  }
+  set(index: number, e: T) {
+    if (index < 0 || index >= this.size) throw new Error("error");
+    this.data[index] = e;
+  }
+  toString() {
+    let res = `MyArray: size=${this.getSize()}, capacity=${
+      this.getCapacity
+    }\r\n`;
+    res += "[";
+    for (let i = 0; i < this.size; i++) {
+      res += JSON.stringify(this.data[i]) + ",";
+    }
+    res = res.slice(0, -1) + "]";
+    return res;
+  }
 }
+
+// hot 85-88
 
 // string 6-10
 export function groupAnagrams(strs: string[]) {
@@ -309,4 +229,29 @@ export function restoreIpAddresses(s: string) {
   return res;
 }
 
-// todo 10
+export function isPalindrome(s: string) {
+  let l = 0,
+    r = s.length - 1;
+  while (l < r) {
+    const currentl = s[l];
+    const currentr = s[r];
+    if (
+      isNaN(parseFloat(currentl)) &&
+      currentl.toLowerCase() === currentl.toUpperCase()
+    ) {
+      l++;
+      continue;
+    }
+    if (
+      isNaN(parseFloat(currentr)) &&
+      currentr.toLowerCase() === currentr.toLowerCase()
+    ) {
+      r--;
+      continue;
+    }
+    if (currentl.toLowerCase() !== currentr.toLowerCase()) return false;
+    l++;
+    r--;
+  }
+  return true;
+}
