af2e4eb78f73e592490bb6cbf165547193176934
meixiang474 <1436098850@qq.com>
2023-03-06 13:58:09 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -21,290 +21,31 @@ export class ListNode1 {
   }
 }
 
-// offer 14-I 14-II
-export function cuttingRope(n: number) {
-  const compute = (m: number) => {
-    const floor = Math.floor(n / m);
-    const ceil = Math.ceil(n / m);
-    return Math.max(
-      ceil ** (m - 1) * (n - (m - 1) * ceil),
-      floor ** (m - 1) * (n - (m - 1) * floor)
-    );
+// offer 15 16
+export function harmmingWeight(num: number) {
+  return num
+    .toString(2)
+    .split("")
+    .reduce((memo, current) => {
+      if (current === "1") memo++;
+      return memo;
+    }, 0);
+}
+
+export function myPow(x: number, n: number) {
+  const isNagitve = n < 0;
+  n = isNagitve ? -n : n;
+  const absPow = (x: number, n: number): number => {
+    if (n === 0) return 1;
+    if (n === 1) return x;
+    const res = absPow(x, Math.floor(n / 2));
+    return n % 2 === 0 ? res * res : res * res * x;
   };
-  let res = 0;
-  for (let i = 2; i <= n; i++) {
-    res = Math.max(res, compute(i));
-  }
-  return res;
+  return isNagitve ? 1 / absPow(x, n) : absPow(x, n);
 }
 
-export function cuttingRope1(n: number) {
-  const arr = [0, 0, 1, 2, 4];
-  if (n < 5) {
-    return arr[n];
-  }
-  let res = 1;
-  while (n >= 5) {
-    res *= 3;
-    n -= 3;
-  }
-  return res * n;
-}
-
-// queue
-export class Queue<T> {
-  items: T[];
-  constructor() {
-    this.items = [];
-  }
-  getSize() {
-    return this.items.length;
-  }
-  isEmpty() {
-    return this.getSize() === 0;
-  }
-  enqueue(item: T) {
-    this.items.push(item);
-  }
-  dequeue() {
-    if (this.items.length === 0) throw new Error("error");
-    return this.items.shift()!;
-  }
-  getFront() {
-    if (this.isEmpty()) throw new Error("error");
-    return this.items[0];
-  }
-  toString() {
-    return this.items.toString();
-  }
-}
-
-export class LoopQueue<T = number> {
-  data: (T | null)[];
-  front: number;
-  tail: number;
-  constructor(capacity = 10) {
-    this.data = new Array(capacity + 1).fill(null);
-    this.front = this.tail = 0;
-  }
-  getCapacity() {
-    return this.data.length - 1;
-  }
-  getSize() {
-    return this.tail >= this.front
-      ? this.tail - this.front
-      : this.tail - this.front + this.data.length;
-  }
-  isEmpty() {
-    return this.getSize();
-  }
-  resize(newCapacity: number) {
-    const newData = new Array(newCapacity + 1).fill(null);
-    for (let i = 0; i < this.getSize(); i++) {
-      newData[i] = this.data[(this.front + i) % this.data.length];
-    }
-    this.tail = this.getSize();
-    this.front = 0;
-    this.data = newData;
-  }
-  enqueue(item: T) {
-    if (this.getSize() >= this.getCapacity()) {
-      this.resize(2 * this.getCapacity());
-    }
-    this.data[this.tail] = item;
-    this.tail = (this.tail + 1) % this.data.length;
-  }
-  dequeue() {
-    if (this.isEmpty()) throw new Error("error");
-    const res = this.data[this.front] as T;
-    this.data[this.front] = null;
-    this.front = (this.front + 1) % this.data.length;
-    if (
-      this.getSize() <= Math.floor(this.getCapacity() / 4) &&
-      Math.floor(this.getCapacity() / 2) !== 0
-    ) {
-      this.resize(Math.floor(this.getCapacity() / 2));
-    }
-    return res;
-  }
-  getFront() {
-    if (this.isEmpty()) throw new Error("error");
-    return this.data[this.front] as T;
-  }
-  toString() {
-    let res = `LoopQueue: size=${this.getSize()}, capacity=${this.getCapacity()}\r\n`;
-    res += "[";
-    for (let i = 0; i < this.getSize(); i++) {
-      res +=
-        JSON.stringify(this.data[(this.front + i) % this.data.length]) + ",";
-    }
-    res = res.slice(0, -1) + "]";
-    return res;
-  }
-}
-
+// linked list
 // todo
-export class Deque<T = number> {
-  data: (T | null)[];
-  size: number;
-  front: number;
-  tail: number;
-  constructor(capacity = 10) {
-    this.data = new Array(capacity).fill(null);
-    this.size = this.front = this.tail = 0;
-  }
-  getCapacity() {
-    return this.data.length;
-  }
-  getSize() {
-    return this.size;
-  }
-  isEmpty() {
-    return this.getSize() === 0;
-  }
-  resize(newCapacity: number) {
-    const newData = new Array(newCapacity).fill(null);
-    for (let i = 0; i < this.getSize(); i++) {
-      newData[i] = this.data[(this.front + i) % this.data.length];
-    }
-    this.front = 0;
-    this.tail = this.size;
-  }
-  addLast(e: T) {
-    if (this.getSize() === this.getCapacity()) {
-      this.resize(this.getCapacity() * 2);
-    }
-    this.data[this.tail] = e;
-    this.tail = (this.tail + 1) % this.data.length;
-    this.size++;
-  }
-  addFront(e: T) {
-    if (this.getSize() === this.getCapacity()) {
-      this.resize(this.getCapacity() * 2);
-    }
-    this.front = this.front === 0 ? this.data.length - 1 : this.front - 1;
-    this.data[this.front] = e;
-    this.size++;
-  }
-  removeFront() {
-    if (this.isEmpty()) throw new Error("error");
-    const res = this.data[this.front] as T;
-    this.data[this.front] = null;
-    this.front = (this.front + 1) % this.data.length;
-    this.size--;
-    if (
-      this.getSize() <= Math.floor(this.getCapacity() / 4) &&
-      Math.floor(this.getCapacity() / 2)
-    ) {
-      this.resize(Math.floor(this.getCapacity() / 2));
-    }
-    return res;
-  }
-  removeLast() {
-    if (this.isEmpty()) throw new Error("error");
-    this.tail = this.tail === 0 ? this.data.length - 1 : this.tail - 1;
-    const res = this.data[this.tail] as T;
-    this.data[this.tail] = null;
-    this.size--;
-    if (
-      this.getSize() <= Math.floor(this.getCapacity() / 4) &&
-      Math.floor(this.getSize() / 4)
-    ) {
-      this.resize(Math.floor(this.getCapacity() / 2));
-    }
-    return res;
-  }
-  getFront() {
-    if (this.isEmpty()) throw new Error("error");
-    return this.data[this.front] as T;
-  }
-  getLast() {
-    if (this.isEmpty()) throw new Error("error");
-    const index = this.tail === 0 ? this.data.length - 1 : this.tail - 1;
-    return this.data[index] as T;
-  }
-  toString() {
-    let res = `Deque: size=${this.getSize()}, capacity=${this.getCapacity()}\r\n`;
-    res += "front [";
-    for (let i = 0; i < this.getSize(); i++) {
-      res +=
-        JSON.stringify(this.data[(this.front + i) % this.data.length]) + ",";
-    }
-    res = res.slice(0, -1) + "] tail";
-    return res;
-  }
-}
-
-export class StackBasedOnQueue<T = number> {
-  queue: T[];
-  constructor() {
-    this.queue = [];
-  }
-  getSize() {
-    return this.queue.length;
-  }
-  isEmpty() {
-    return this.getSize() === 0;
-  }
-  push(item: T) {
-    this.queue.push(item);
-  }
-  pop() {
-    if (this.isEmpty()) throw new Error("error");
-    for (let i = 0; i < this.queue.length - 1; i++) {
-      this.queue.push(this.queue.shift()!);
-    }
-    return this.queue.shift() as T;
-  }
-  peek() {
-    if (this.isEmpty()) throw new Error("error");
-    const res = this.pop() as T;
-    this.push(res);
-    return res;
-  }
-}
-
-export class QueueBasedOnStack<T> {
-  stack1: T[];
-  stack2: T[];
-  constructor() {
-    this.stack1 = [];
-    this.stack2 = [];
-  }
-  getSize() {
-    return this.stack1.length;
-  }
-  isEmpty() {
-    return this.getSize() === 0;
-  }
-  enqueue(item: T) {
-    this.stack1.push(item);
-  }
-  dequeue() {
-    if (this.isEmpty()) throw new Error("error");
-    while (this.stack1.length) {
-      this.stack2.push(this.stack1.pop()!);
-    }
-    const res = this.stack2.pop()!;
-    while (this.stack2.length) {
-      this.stack1.push(this.stack2.pop()!);
-    }
-    return res;
-  }
-  getFront() {
-    if (this.isEmpty()) throw new Error("error");
-    const res = this.dequeue() as T;
-    while (this.stack1.length) {
-      this.stack2.push(this.stack1.pop()!);
-    }
-    this.stack1.push(res);
-    while (this.stack2.length) {
-      this.stack1.push(this.stack2.pop()!);
-    }
-    return res;
-  }
-}
-
 // hot 93-96
 export function convertBst(root: TreeNode | null) {
   if (!root) return root;
@@ -603,9 +344,32 @@ export function combineSum2(nums: number[], target: number) {
     }
     for (let i = index; i < nums.length; i++) {
       if (i > index && nums[i - 1] === nums[i]) continue;
-      dfs(path.concat(nums[i]), sum + nums[i], i + 1);
+      if (sum > target) return;
+      if (index >= nums.length) return;
+      for (let i = index; i < nums.length; i++) {
+        if (i > index && nums[i] === nums[i - 1]) continue;
+        dfs(path.concat(nums[i]), sum + nums[i], i + 1);
+      }
     }
+    dfs([], 0, 0);
+    return res;
   };
-  dfs([], 0, 0);
-  return res;
+}
+
+export function firstMissingPositive(nums: number[]) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] <= 0) {
+      nums[i] = nums.length + 1;
+    }
+  }
+  for (let i = 0; i < nums.length; i++) {
+    const current = Math.abs(nums[i]);
+    if (current <= nums.length) {
+      nums[current - 1] = -Math.abs(nums[current - 1]);
+    }
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) return i + 1;
+  }
+  return nums.length + 1;
 }
