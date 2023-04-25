9e4273123380ff4975a48cdce3d0d55e1890702a
meixiang474 <1436098850@qq.com>
2023-04-25 17:02:20 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -217,6 +217,151 @@ export class LinkedListMap<K = string, V = any> {
   }
   set(key: K, value: V) {
     const node = this.getNode(key);
+    if (node == null) throw new Error("error");
+    node.value = value;
+  }
+  remove(key: K) {
+    let prev = this.dummyHead;
+    while (prev.next) {
+      if (prev.next.key === key) {
+        break;
+      }
+      prev = prev.next;
+    }
+    if (prev.next) {
+      const node = prev.next;
+      this.size--;
+      prev.next = node.next;
+      return node.value;
+    }
+    return null;
+  }
+}
+
+export class BSTMapNode<K = number, V = any> {
+  key: K;
+  value: V;
+  left: BSTMapNode<K, V> | null;
+  right: BSTMapNode<K, V> | null;
+  constructor(key: K, value: V) {
+    this.key = key;
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export class BSTMap<K = number, V = any> {
+  root: BSTMapNode<K, V> | null;
+  size: number;
+  constructor(compare: (a: K, b: K) => boolean) {
+    this.root = null;
+    this.size = 0;
+    this.compare = compare || this.compare;
+  }
+  compare(a: K, b: K) {
+    return a < b;
+  }
+  add(key: K, value: V) {
+    this.root = this.addNode(this.root, key, value);
+  }
+  addNode(node: BSTMapNode<K, V> | null, key: K, value: V): BSTMapNode<K, V> {
+    if (!node) {
+      this.size++;
+      return new BSTMapNode(key, value);
+    }
+    if (this.compare(node.key, key)) {
+      node.right = this.addNode(node.right, key, value);
+    } else if (this.compare(key, node.key)) {
+      node.left = this.addNode(node.left, key, value);
+    } else {
+      node.value = value;
+    }
+    return node;
+  }
+  getNode(node: BSTMapNode<K, V> | null, key: K): BSTMapNode<K, V> | null {
+    if (!node) return null;
+    if (this.compare(node.key, key)) {
+      return this.getNode(node.right, key);
+    } else if (this.compare(key, node.key)) {
+      return this.getNode(node.left, key);
+    }
+    return node;
+  }
+  contains(key: K) {
+    return this.getNode(this.root, key) != null;
+  }
+  get(key: K) {
+    const node = this.getNode(this.root, key);
+    return node == null ? null : node.value;
+  }
+  set(key: K, value: V) {
+    const node = this.getNode(this.root, key);
+    if (node == null) throw new Error("error");
+    node.value = value;
+  }
+  minimumNode(node: BSTMapNode<K, V>): BSTMapNode<K, V> {
+    if (!node.left) {
+      return node;
+    }
+    return this.minimumNode(node.left);
+  }
+  removeMinNode(node: BSTMapNode<K, V>) {
+    if (!node.left) {
+      this.size--;
+      return node.right;
+    }
+    node.left = this.removeMinNode(node.left);
+    return node;
+  }
+  remove(key: K) {
+    const node = this.getNode(this.root, key);
+    if (node == null) return null;
+    this.root = this.removeNode(this.root, key);
+    return node.value;
+  }
+  removeNode(node: BSTMapNode<K, V> | null, key: K) {
+    if (!node) return null;
+    if (this.compare(node.key, key)) {
+      node.right = this.removeNode(node.right, key);
+      return node;
+    } else if (this.compare(key, node.key)) {
+      node.left = this.removeNode(node.left, key);
+      return node;
+    }
+    if (!node.left) {
+      this.size--;
+      return node.right;
+    } else if (!node.right) {
+      this.size--;
+      return node.left;
+    } else {
+      const successor = this.minimumNode(node.right);
+      successor.left = node.left;
+      successor.right = this.removeMinNode(node.right);
+      return successor;
+    }
+  }
+}
+
+export function intersection1(nums1: number[], nums2: number[]) {
+  const map = new Map<number, boolean>();
+  for (let item of nums1) {
+    map.set(item, true);
+  }
+  const res: number[] = [];
+  for (let item of nums2) {
+    if (map.has(item)) {
+      res.push(item);
+      map.delete(item);
+    }
+  }
+  return res;
+}
+
+export function twoSum(nums: number[], target: number) {
+  const map = new Map<number, number>();
+  for(let i = 0; i < nums.length; i++) {
     // todo
   }
 }
