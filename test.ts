64b017cf26b76b954cfbd746be442618f9b2a6f5
meixiang474 <1436098850@qq.com>
2023-05-31 17:41:23 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -384,5 +384,30 @@ export function longestOnes(nums: number[], k: number) {
 }
 
 export function moveStones(nums: number[]) {
+  nums.sort((a, b) => a - b);
+  const n = nums.length;
+  const max =
+    nums[n - 1] -
+    nums[0] +
+    1 -
+    n -
+    Math.min(nums[n - 1] - nums[n - 2] - 1, nums[1] - nums[0] - 1);
+  let min = Infinity;
+  let l = 0,
+    r = 0;
+  for (let l = 0; l < n; l++) {
+    while (r + 1 < n && nums[r + 1] - nums[l] + 1 <= n) {
+      r++;
+    }
+    let res = n - r + l - 1;
+    if (r - l + 1 === n - 1 && nums[r] - nums[l] + 1 === n - 1) {
+      res = 2;
+    }
+    min = Math.min(res, min);
+  }
+  return [min, max];
+}
+
+export function maxSatisfied(customers: number[], grumpy: number, minutes: number) {
   // todo
 }
