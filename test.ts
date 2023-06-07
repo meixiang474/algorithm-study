97ff4238f5a2b4c7cf60075d6773845ecaedfa74
meixiang474 <1436098850@qq.com>
2023-06-07 17:01:54 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -285,4 +285,23 @@ export function maximalRectangle(matrix: string[][]) {
 }
 
 // sort 1-5
+export function mergeField(intervals: number[][]) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const res: number[][] = [];
+  let prevEnd = -Infinity;
+  for (let i = 0; i < intervals.length; i++) {
+    const [start, end] = intervals[i];
+    if (i > 0 && prevEnd >= start) {
+      res.splice(res.length - 1, 1, [
+        res[res.length - 1][0],
+        Math.max(prevEnd, end),
+      ]);
+    } else {
+      res.push([start, end]);
+    }
+    prevEnd = Math.max(prevEnd, end);
+  }
+  return res;
+}
+
 // todo
