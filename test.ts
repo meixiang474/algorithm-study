5b7ad941f8d625e0ecf10bec859d64d5100ba9b5
meixiang474 <1436098850@qq.com>
2023-06-15 16:55:17 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -146,8 +146,143 @@ export class SegmentTree<T = any> {
       this.queryNode(rightIndex, mid + 1, r, mid + 1, queryr)
     );
   }
-  // todo
+  set(index: number, val: T) {
+    if (index < 0 || index >= this.data.length) throw new Error("error");
+    this.data[index] = val;
+    this.setNode(0, 0, this.data.length - 1, index, val);
+  }
+  setNode(treeIndex: number, l: number, r: number, index: number, val: T) {
+    if (l === r) {
+      this.tree[treeIndex] = val;
+      return;
+    }
+    const leftIndex = this.leftChild(treeIndex);
+    const rightIndex = this.rightChild(treeIndex);
+    const mid = Math.floor(l + (r - l) / 2);
+    if (index >= mid + 1) {
+      this.setNode(rightIndex, mid + 1, r, index, val);
+    }
+    if (index <= mid) {
+      this.setNode(leftIndex, l, mid, index, val);
+    }
+    this.tree[treeIndex] = this.merge(
+      this.tree[leftIndex]!,
+      this.tree[rightIndex]!
+    );
+  }
+  toString() {
+    let res = "[";
+    for (let i = 0; i < this.tree.length; i++) {
+      res += JSON.stringify(this.tree[i]) + ",";
+    }
+    return res.slice(0, -1) + "]";
+  }
 }
+
+export class NumArray {
+  data: number[];
+  tree: (number | null)[];
+  constructor(nums: number[]) {
+    this.data = [...nums];
+    this.tree = new Array(4 * this.data.length).fill(null);
+    this.buildSegmentTree(0, 0, this.data.length - 1);
+  }
+  buildSegmentTree(treeIndex: number, l: number, r: number) {
+    if (l > r) return;
+    if (l === r) {
+      this.tree[treeIndex] = this.data[l];
+      return;
+    }
+    const leftIndex = this.getLeftIndex(treeIndex);
+    const rightIndex = this.getRightIndex(treeIndex);
+    const mid = Math.floor(l + (r - l) / 2);
+    this.buildSegmentTree(leftIndex, l, mid);
+    this.buildSegmentTree(rightIndex, mid + 1, r);
+    this.tree[treeIndex] = this.merge(
+      this.tree[leftIndex]!,
+      this.tree[rightIndex]!
+    );
+  }
+  merge(a: number, b: number) {
+    return a + b;
+  }
+  getLeftIndex(index: number) {
+    return 2 * index + 1;
+  }
+  getRightIndex(index: number) {
+    return 2 * index + 2;
+  }
+  sumRange(left: number, right: number) {
+    if (
+      left < 0 ||
+      left >= this.data.length ||
+      right < 0 ||
+      right >= this.data.length
+    )
+      throw new Error("error");
+    return this.query(0, 0, this.data.length - 1, left, right);
+  }
+  query(
+    treeIndex: number,
+    l: number,
+    r: number,
+    queryl: number,
+    queryr: number
+  ): number {
+    if (l === queryl && r === queryr) {
+      return this.tree[treeIndex]!;
+    }
+    const leftIndex = this.getLeftIndex(treeIndex);
+    const rightIndex = this.getRightIndex(treeIndex);
+    const mid = Math.floor(l + (r - l) / 2);
+    if (queryl >= mid + 1) {
+      return this.query(rightIndex, mid + 1, r, queryl, queryr);
+    }
+    if (queryr <= mid) {
+      return this.query(leftIndex, l, mid, queryl, queryr);
+    }
+    return this.merge(
+      this.query(leftIndex, l, mid, queryl, mid),
+      this.query(rightIndex, mid + 1, r, mid + 1, queryr)
+    );
+  }
+}
+
+export class NumArray1 {
+  sums: number[];
+  constructor(nums: number[]) {
+    this.sums = new Array(nums.length + 1).fill(0);
+    for (let i = 1; i < this.sums.length; i++) {
+      this.sums[i] = this.sums[i - 1] + nums[i - 1];
+    }
+  }
+  sumRange(left: number, right: number) {
+    return this.sums[right + 1] - this.sums[left];
+  }
+}
+
+export class NumArray2 {
+  data: number[];
+  sums: number[];
+  constructor(nums: number[]) {
+    this.data = [...nums];
+    this.sums = new Array(this.data.length + 1).fill(0);
+    for (let i = 1; i < this.sums.length; i++) {
+      this.sums[i] = this.sums[i - 1] + this.data[i - 1];
+    }
+  }
+  sumRange(left: number, right: number) {
+    return this.sums[right + 1] - this.sums[left];
+  }
+  update(index: number, val: number) {
+    this.data[index] = val;
+    for (let i = index + 1; i < this.sums.length; i++) {
+      this.sums[i] = this.sums[i - 1] + this.data[i - 1];
+    }
+  }
+}
+
+// todo
 
 // hot 33 - 36
 export function subsets(nums: number[]) {
