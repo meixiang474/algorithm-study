014e3ae57109d52ad65251108df626d4f7b2fbc9
meixiang474 <1436098850@qq.com>
2023-07-12 16:40:26 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -21,244 +21,127 @@ export class ListNode<T = number> {
   }
 }
 
-// offer 39 40
-export function majorityElement(nums: number[]) {
-  const map = new Map<number, number>();
-  for (let item of nums) {
-    map.set(item, map.has(item) ? map.get(item)! + 1 : 1);
-    if (map.get(item)! > nums.length / 2) return item;
+// offer 42 45
+export function maxSubArray(nums: number[]) {
+  if (nums.length === 0) return -Infinity;
+  const dp = [nums[0]];
+  for (let i = 1; i < nums.length; i++) {
+    const current = nums[i];
+    if (dp[i - 1] >= 0) {
+      dp[i] = dp[i - 1] + current;
+    } else {
+      dp[i] = current;
+    }
   }
+  return Math.max(...dp);
 }
 
-export function getLeastNumbers(nums: number[], k: number) {
-  const arr = [...nums];
-  if (k >= arr.length) return arr;
-  const sortArr = (arr: number[], l: number, r: number): number[] => {
-    if (l >= r) return arr.slice(0, k);
-    const p = partition(arr, l, r);
-    if (p === k) {
-      return arr.slice(0, k);
-    } else if (p > k) {
-      return sortArr(arr, l, p - 1);
-    } else {
-      return sortArr(arr, p + 1, r);
+export function fn(nums: number[]) {
+  return nums
+    .map((item) => item + "")
+    .sort((a, b) => parseFloat(a + b) - parseFloat(b + a))
+    .join("");
+}
+
+// search sort
+export function isObject<T>(obj: T) {
+  return obj && typeof obj === "object";
+}
+
+export function isEqual<T>(a: T, b: T) {
+  if (!isObject(a) || !isObject(b)) return a === b;
+  const keys1 = Object.keys(a as any);
+  const keys2 = Object.keys(b as any);
+  if (keys1.length !== keys2.length) return false;
+  for (let key in keys1) {
+    if ((a as any).hasOwnProperty(key)) {
+      const res = isEqual((a as any)[key], (b as any)[key]);
+      if (!res) return false;
     }
-  };
-  const getRandom = (l: number, r: number) =>
-    Math.floor(Math.random() * (r - l + 1) + l);
+  }
+  return true;
+}
+
+export function linearSearch<T>(data: T[], target: T) {
+  for (let i = 0; i < data.length; i++) {
+    if (isEqual(data[i], target)) return i;
+  }
+  return -1;
+}
+
+export function selectionSort(nums: number[]) {
+  const res = [...nums];
   const swap = (arr: number[], i: number, j: number) =>
     ([arr[i], arr[j]] = [arr[j], arr[i]]);
-  const partition = (arr: number[], l: number, r: number) => {
-    const p = getRandom(l, r);
-    swap(arr, l, p);
-    let i = l + 1,
-      j = r;
-    while (true) {
-      while (i <= j && arr[i] < arr[l]) {
-        i++;
-      }
-      while (i <= j && arr[j] > arr[l]) {
-        j--;
-      }
-      if (i >= j) break;
-      swap(arr, i, j);
-      i++;
-      j--;
+  for (let i = 0; i < res.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < res.length; j++) {
+      minIndex = res[j] < res[minIndex] ? j : minIndex;
     }
-    swap(arr, l, j);
-    return j;
-  };
-  return sortArr(arr, 0, arr.length - 1);
-}
-
-export class TrieNode {
-  isWord: boolean;
-  next: Map<string, TrieNode>;
-  constructor(isWord: boolean = false) {
-    this.isWord = isWord;
-    this.next = new Map();
-  }
-}
-
-// trie
-export class Trie {
-  root: TrieNode;
-  size: number;
-  constructor() {
-    this.root = new TrieNode();
-    this.size = 0;
-  }
-  add(word: string) {
-    let current = this.root;
-    for (let item of word) {
-      if (!current.next.has(item)) {
-        current.next.set(item, new TrieNode());
-      }
-      current = current.next.get(item)!;
-    }
-    if (!current.isWord) {
-      current.isWord = true;
-      this.size++;
-    }
-  }
-  contains(word: string) {
-    let current = this.root;
-    for (let item of word) {
-      if (!current.next.has(item)) return false;
-      current = current.next.get(item)!;
-    }
-    return current.isWord;
-  }
-  isPrefix(prefix: string) {
-    let current = this.root;
-    for (let item of prefix) {
-      if (!current.next.has(item)) return false;
-      current = current.next.get(item)!;
-    }
-    return true;
-  }
-}
-
-export class WordDictionary {
-  root: TrieNode;
-  constructor() {
-    this.root = new TrieNode();
-  }
-  addWord(word: string) {
-    let current = this.root;
-    for (let item of word) {
-      if (!current.next.has(item)) {
-        current.next.set(item, new TrieNode());
-      }
-      current = current.next.get(item)!;
-    }
-    current.isWord = true;
-  }
-  search(word: string) {
-    return this.match(this.root, word, 0);
-  }
-  match(node: TrieNode, word: string, index: number): boolean {
-    if (index === word.length) return node.isWord;
-    const current = word[index];
-    if (current !== ".") {
-      if (!node.next.has(current)) return false;
-      return this.match(node.next.get(current)!, word, index + 1);
-    } else {
-      for (let [, item] of node.next) {
-        const res = this.match(item, word, index + 1);
-        if (res) return res;
-      }
-      return false;
-    }
-  }
-}
-
-export class TrieNode1 {
-  value: number;
-  next: Map<string, TrieNode1>;
-  constructor(value: number = 0) {
-    this.value = value;
-    this.next = new Map();
-  }
-}
-
-export class MapSum {
-  root: TrieNode1;
-  constructor() {
-    this.root = new TrieNode1();
-  }
-  insert(key: string, value: number) {
-    let current = this.root;
-    for (let item of key) {
-      if (!current.next.has(item)) {
-        current.next.set(item, new TrieNode1());
-      }
-      current = current.next.get(item)!;
-    }
-    current.value = value;
-  }
-  sum(prefix: string) {
-    let current = this.root;
-    for (let item of prefix) {
-      if (!current.next.has(item)) return 0;
-      current = current.next.get(item)!;
-    }
-    this.sumNode(current);
-  }
-  sumNode(node: TrieNode1) {
-    let res = node.value;
-    for (let [, item] of node.next) {
-      res += this.sumNode(item);
-    }
-    return res;
-  }
-}
-
-// hot 41 - 44
-export function levelOrder(root: TreeNode | null) {
-  if (!root) return [];
-  const res: number[][] = [];
-  const queue: [TreeNode, number][] = [[root, 0]];
-  while (queue.length) {
-    const [current, level] = queue.shift()!;
-    const arr = res[level] || (res[level] = []);
-    arr.push(current.val);
-    if (current.left) queue.push([current.left, level + 1]);
-    if (current.right) queue.push([current.right, level + 1]);
+    if (minIndex !== i) swap(res, i, minIndex);
   }
   return res;
 }
 
-export function maxDepth(root: TreeNode | null) {
+export function insertionSort(nums: number[]) {
+  const res = [...nums];
+  for (let i = 0; i < nums.length; i++) {
+    let current = res[i];
+    let swapIndex = i;
+    for (let j = i - 1; j >= 0; j--) {
+      if (res[j] > current) {
+        res[j + 1] = res[j];
+        swapIndex = j;
+      } else {
+        break;
+      }
+    }
+    if (swapIndex !== i) res[swapIndex] = current;
+  }
+  return res;
+}
+
+export function bubbleSort(nums: number[]) {
+  const res = [...nums];
+  const swap = (arr: number[], i: number, j: number) =>
+    ([arr[i], arr[j]] = [arr[j], arr[i]]);
+  for (let i = 0; i < nums.length - 1; i++) {
+    let flag = false;
+    for (let j = 0; j < nums.length - i - 1; j++) {
+      if (res[i] > res[j]) {
+        swap(res, i, j);
+        flag = true;
+      }
+    }
+    if (!flag) break;
+  }
+  return res;
+}
+
+// hot 45 - 48
+export function maxProfit(prices: number[]) {
+  let profit = 0;
+  for (let i = 0; i < prices.length - 1; i++) {
+    if (prices[i + 1] > prices[i]) profit += prices[i + 1] - prices[i];
+  }
+  return profit;
+}
+
+export function maxPathSum(root: TreeNode | null) {
   if (!root) return 0;
-  let res = 0;
-  const dfs = (node: TreeNode, level: number) => {
-    if (!node.left && !node.right) res = Math.max(res, level);
-    if (node.left) dfs(node.left, level + 1);
-    if (node.right) dfs(node.right, level + 1);
-  };
-  dfs(root, 1);
-  return res;
-}
-
-export function buildTree(
-  preorder: number[],
-  inorder: number[]
-): TreeNode | null {
-  if (preorder.length === 0 || inorder.length === 0) return null;
-  const rootValue = preorder[0];
-  const rootIndex = inorder.indexOf(rootValue);
-  const rootNode = new TreeNode(rootValue);
-  rootNode.left = buildTree(
-    preorder.slice(1, rootIndex + 1),
-    inorder.slice(0, rootIndex)
-  );
-  rootNode.right = buildTree(
-    preorder.slice(rootIndex + 1),
-    inorder.slice(rootIndex + 1)
-  );
-  return rootNode;
-}
-
-export function flatten(root: TreeNode | null) {
-  if (!root) return null;
-  const dummyHead = new TreeNode(-1);
-  let p = dummyHead;
-  const dfs = (node: TreeNode) => {
-    const left = node.left;
-    const right = node.right;
-    node.left = null;
-    node.right = null;
-    p.right = node;
-    p = p.right;
-    if (left) {
-      dfs(left);
-    }
-    if (right) {
-      dfs(right);
-    }
+  let res = -Infinity;
+  const dfs = (node: TreeNode): number => {
+    const left = Math.max(node.left ? dfs(node.left) : 0, 0);
+    const right = Math.max(node.right ? dfs(node.right) : 0, 0);
+    res = Math.max(res, left + right + node.val);
+    return Math.max(left, right, 0) + node.val;
   };
   dfs(root);
+  return res;
+}
+
+export function longestConsecutive(nums: number[]) {
+  // todo
 }
 
 // string 1 - 5
@@ -325,4 +208,46 @@ export function isValid(s: string) {
   return stack.length === 0;
 }
 
-// todo
+export function generateParenthesis(n: number) {
+  if (n === 0) return [];
+  const res: string[] = [];
+  const dfs = (path: string, open: number, close: number) => {
+    if (path.length >= n * 2) {
+      res.push(path);
+      return;
+    }
+    if (open < n) {
+      dfs(path + "(", open + 1, close);
+    }
+    if (close < open) {
+      dfs(path + ")", open, close + 1);
+    }
+  };
+  dfs("", 0, 0);
+  return res;
+}
+
+export function strStr(haystack: string, needle: string) {
+  if (haystack.length < needle.length) return -1;
+  let res = -1;
+  let index = 0;
+  while (index < haystack.length) {
+    if (haystack.length - index < needle.length) break;
+    const current = haystack[index];
+    if (current === needle[0]) {
+      let flag = true;
+      for (let i = index + 1; i < index + needle.length; i++) {
+        if (haystack[index] !== needle[i - index]) {
+          flag = false;
+          break;
+        }
+      }
+      if (flag) {
+        res = index;
+        break;
+      }
+    }
+    index++;
+  }
+  return res;
+}
