c313958119ace54cab2528cd393f3fe9b535bf50
meixiang474 <1436098850@qq.com>
2023-07-13 17:37:37 +0800

    chore: practice

--- test.ts
+++ test.ts
@@ -21,101 +21,99 @@ export class ListNode<T = number> {
   }
 }
 
-// offer 42 45
-export function maxSubArray(nums: number[]) {
-  if (nums.length === 0) return -Infinity;
-  const dp = [nums[0]];
-  for (let i = 1; i < nums.length; i++) {
-    const current = nums[i];
-    if (dp[i - 1] >= 0) {
-      dp[i] = dp[i - 1] + current;
+// offer 46 47
+export function translateNum(num: number) {
+  const numStr = num.toString();
+  const dp = [1, 1];
+  for (let i = 2; i <= numStr.length; i++) {
+    if (
+      parseFloat(numStr.slice(i - 2, i)) > 25 ||
+      parseFloat(numStr.slice(i - 2, i)) < 10
+    ) {
+      dp[i] = dp[i - 1];
     } else {
-      dp[i] = current;
+      dp[i] = dp[i - 1] + dp[i - 2];
     }
   }
-  return Math.max(...dp);
+  return dp[numStr.length];
 }
 
-export function fn(nums: number[]) {
-  return nums
-    .map((item) => item + "")
-    .sort((a, b) => parseFloat(a + b) - parseFloat(b + a))
-    .join("");
-}
-
-// search sort
-export function isObject<T>(obj: T) {
-  return obj && typeof obj === "object";
-}
-
-export function isEqual<T>(a: T, b: T) {
-  if (!isObject(a) || !isObject(b)) return a === b;
-  const keys1 = Object.keys(a as any);
-  const keys2 = Object.keys(b as any);
-  if (keys1.length !== keys2.length) return false;
-  for (let key in keys1) {
-    if ((a as any).hasOwnProperty(key)) {
-      const res = isEqual((a as any)[key], (b as any)[key]);
-      if (!res) return false;
-    }
-  }
-  return true;
-}
-
-export function linearSearch<T>(data: T[], target: T) {
-  for (let i = 0; i < data.length; i++) {
-    if (isEqual(data[i], target)) return i;
-  }
-  return -1;
-}
-
-export function selectionSort(nums: number[]) {
-  const res = [...nums];
-  const swap = (arr: number[], i: number, j: number) =>
-    ([arr[i], arr[j]] = [arr[j], arr[i]]);
-  for (let i = 0; i < res.length; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < res.length; j++) {
-      minIndex = res[j] < res[minIndex] ? j : minIndex;
-    }
-    if (minIndex !== i) swap(res, i, minIndex);
-  }
-  return res;
-}
-
-export function insertionSort(nums: number[]) {
-  const res = [...nums];
-  for (let i = 0; i < nums.length; i++) {
-    let current = res[i];
-    let swapIndex = i;
-    for (let j = i - 1; j >= 0; j--) {
-      if (res[j] > current) {
-        res[j + 1] = res[j];
-        swapIndex = j;
+export function maxValue(grid: number[][]) {
+  if (grid.length === 0 || grid[0].length === 0) return 0;
+  const m = grid.length;
+  const n = grid[0].length;
+  const dp: number[][] = Array.from({ length: m }, () => new Array(n).fill(0));
+  for (let r = 0; r < m; r++) {
+    for (let c = 0; c < n; c++) {
+      if (r === 0 && c === 0) {
+        dp[r][c] = grid[r][c];
+      } else if (r === 0) {
+        dp[r][c] = dp[r][c - 1] + grid[r][c];
+      } else if (c === 0) {
+        dp[r][c] = dp[r - 1][c] + grid[r][c];
       } else {
-        break;
+        dp[r][c] = Math.max(dp[r - 1][c], dp[r][c - 1]) + grid[r][c];
       }
     }
-    if (swapIndex !== i) res[swapIndex] = current;
   }
-  return res;
+  return dp[m - 1][n - 1];
 }
 
-export function bubbleSort(nums: number[]) {
-  const res = [...nums];
-  const swap = (arr: number[], i: number, j: number) =>
-    ([arr[i], arr[j]] = [arr[j], arr[i]]);
-  for (let i = 0; i < nums.length - 1; i++) {
-    let flag = false;
-    for (let j = 0; j < nums.length - i - 1; j++) {
-      if (res[i] > res[j]) {
-        swap(res, i, j);
-        flag = true;
-      }
-    }
-    if (!flag) break;
+// array
+class MyArray<T> {
+  data: (T | null)[];
+  size: number;
+  constructor(capacity = 10) {
+    this.data = new Array(capacity).fill(null);
+    this.size = 0;
   }
-  return res;
+  getCapacity() {
+    return this.data.length;
+  }
+  getSize() {
+    return this.size;
+  }
+  resize(newCapacity: number) {
+    const newData: (T | null)[] = new Array(newCapacity).fill(null);
+    for (let i = 0; i < this.size; i++) {
+      newData[i] = this.data[i];
+    }
+    this.data = newData;
+  }
+  add(index: number, val: T) {
+    if (index < 0 || index > this.size) throw new Error("error");
+    if (this.getCapacity() >= this.size) this.resize(2 * this.getCapacity());
+    for (let i = this.size; i > index; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[index] = val;
+    this.size++;
+  }
+  addFirst(val: T) {
+    this.add(0, val);
+  }
+  addLast(val: T) {
+    this.add(this.size, val);
+  }
+  get(index: number) {
+    if (index < 0 || index >= this.size) throw new Error("error");
+    return this.data[index];
+  }
+  getFirst() {
+    return this.get(0);
+  }
+  getLast() {
+    return this.get(this.size - 1);
+  }
+  contains(val: T) {
+    for (let i = 0; i < this.size; i++) {
+      if (this.data[i] === val) return true;
+    }
+    return false;
+  }
+  find(val: T) {
+    // todo
+  }
 }
 
 // hot 45 - 48
@@ -141,113 +139,108 @@ export function maxPathSum(root: TreeNode | null) {
 }
 
 export function longestConsecutive(nums: number[]) {
-  // todo
-}
-
-// string 1 - 5
-export function longestSubstring(s: string) {
-  let l = 0,
-    r = 0;
-  const map = new Map<string, number>();
+  const set = new Set<number>();
+  for (let item of nums) {
+    set.add(item);
+  }
   let res = 0;
-  while (r < s.length) {
-    const current = s[r];
-    if (map.has(current) && map.get(current)! >= l) {
-      l = map.get(current)! + 1;
-    }
-    res = Math.max(res, r - l + 1);
-    map.set(current, r);
-    r++;
-  }
-  return res;
-}
-
-export function letterCombinations(digits: string) {
-  if (digits === "") return "";
-  const map: Record<string, string[]> = {
-    "2": ["a", "b", "c"],
-    "3": ["d", "e", "f"],
-    "4": ["g", "h", "i"],
-    "5": ["j", "k", "l"],
-    "6": ["m", "n", "o"],
-    "7": ["p", "q", "r", "s"],
-    "8": ["t", "u", "v"],
-    "9": ["w", "x", "y", "z"],
-  };
-  const res: string[] = [];
-  const dfs = (path: string, index: number) => {
-    if (index >= digits.length) {
-      res.push(path);
-      return;
-    }
-    const current = digits[index];
-    const arr = map[current];
-    for (let item of arr) {
-      dfs(path + item, index + 1);
-    }
-  };
-  dfs("", 0);
-  return res;
-}
-
-export function isValid(s: string) {
-  if (s.length % 2 !== 0) return false;
-  const map = new Map<string, string>();
-  map.set("(", ")");
-  map.set("[", "]");
-  map.set("{", "}");
-  const stack: string[] = [];
-  for (let item of s) {
-    if (map.has(item)) {
-      stack.push(item);
-    } else {
-      const prev = stack.pop();
-      if (!prev || map.get(prev) !== item) return false;
-    }
-  }
-  return stack.length === 0;
-}
-
-export function generateParenthesis(n: number) {
-  if (n === 0) return [];
-  const res: string[] = [];
-  const dfs = (path: string, open: number, close: number) => {
-    if (path.length >= n * 2) {
-      res.push(path);
-      return;
-    }
-    if (open < n) {
-      dfs(path + "(", open + 1, close);
-    }
-    if (close < open) {
-      dfs(path + ")", open, close + 1);
-    }
-  };
-  dfs("", 0, 0);
-  return res;
-}
-
-export function strStr(haystack: string, needle: string) {
-  if (haystack.length < needle.length) return -1;
-  let res = -1;
-  let index = 0;
-  while (index < haystack.length) {
-    if (haystack.length - index < needle.length) break;
-    const current = haystack[index];
-    if (current === needle[0]) {
-      let flag = true;
-      for (let i = index + 1; i < index + needle.length; i++) {
-        if (haystack[index] !== needle[i - index]) {
-          flag = false;
-          break;
-        }
+  for (let item of nums) {
+    if (!set.has(item - 1)) {
+      let length = 1;
+      let current = item;
+      while (set.has(current + 1)) {
+        length++;
+        current++;
       }
-      if (flag) {
-        res = index;
-        break;
-      }
+      res = Math.max(res, length);
     }
-    index++;
   }
   return res;
+}
+
+export function singleNumber(nums: number[]) {
+  return nums.reduce((a, b) => (a ^= b));
+}
+
+// tree 1 - 5
+export function inorderTraversal(root: TreeNode | null) {
+  if (!root) return [];
+  const stack: TreeNode[] = [];
+  let p: TreeNode | null = root;
+  const res: number[] = [];
+  while (p || stack.length) {
+    while (p) {
+      stack.push(p);
+      p = p.left;
+    }
+    const current = stack.pop()!;
+    res.push(current.val);
+    p = current.right;
+  }
+  return res;
+}
+
+export function inorderTraversal1(root: TreeNode | null) {
+  if (!root) return [];
+  const res: number[] = [];
+  const dfs = (node: TreeNode) => {
+    if (node.left) dfs(node.left);
+    res.push(node.val);
+    if (node.right) dfs(node.right);
+  };
+  dfs(root);
+  return res;
+}
+
+export function numTrees(n: number) {
+  const dp = [1, 1];
+  for (let i = 2; i <= n; i++) {
+    if (dp[i] == null) dp[i] = 0;
+    for (let j = 1; j <= i; j++) {
+      dp[i] = dp[i] + dp[j - 1] * dp[i - j];
+    }
+  }
+  return dp[n];
+}
+
+export function isValidBST(root: TreeNode | null) {
+  if (!root) return true;
+  const dfs = (node: TreeNode, floor: number, ceil: number): boolean => {
+    if (node.val <= floor || node.val >= ceil) return false;
+    return (
+      (!node.left || dfs(node.left, floor, node.val)) &&
+      (!node.right || dfs(node.right, node.val, ceil))
+    );
+  };
+  return dfs(root, -Infinity, Infinity);
+}
+
+export function isSameTree(p: TreeNode | null, q: TreeNode | null) {
+  if (!p && !q) return true;
+  if (
+    p &&
+    q &&
+    p.val === q.val &&
+    isSameTree(p.left, q.left) &&
+    isSameTree(p.right, q.right)
+  )
+    return true;
+  return false;
+}
+
+export function isSymmetric(root: TreeNode | null) {
+  if (!root) return true;
+  const compare = (p: TreeNode | null, q: TreeNode | null) => {
+    if (!p && !q) return true;
+    if (
+      p &&
+      q &&
+      p.val === q.val &&
+      compare(p.left, q.right) &&
+      compare(p.right, q.left)
+    )
+      return true;
+    return false;
+  };
+  return compare(root.left, root.right);
 }
